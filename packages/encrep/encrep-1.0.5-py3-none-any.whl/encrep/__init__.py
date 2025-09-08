c2682e08fe747e60554b02612bb70a94911035d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:04:24 +0000

    Add package encrep/1.0.5/encrep-1.0.5-py3-none-any.whl

---
+++ packages/encrep/encrep-1.0.5-py3-none-any.whl/encrep/__init__.py
@@ -0,0 +1,10 @@
+# Encrep
+# MIT license
+# Copyright © 2025 Anatoly Petrov (petrov.projects@gmail.com)
+# All rights reserved
+
+"""Package content. Only for test environment."""
+
+from .main import app, ExclusionRule, Secrets
+
+__all__ = ["app", "ExclusionRule", "Secrets"]
