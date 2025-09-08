d99138a92a06d0b6e909fc4ec82564b3cd7ab5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 16:03:48 +0000

    Add package astro-p3/1.4.4/astro_p3-1.4.4.tar.gz

---
+++ packages/astro-p3/astro_p3-1.4.4.tar.gz/astro_p3-1.4.4/p3/aoSystem/__init__.py
@@ -0,0 +1,63 @@
+import numpy as np
+import os
+import warnings
+
+gpuEnabled = False
+cp = None
+fft = None
+spc = None
+interp = None
+scnd = None
+rotate = None
+nnp = np
+RectBivariateSpline = None
+
+systemDisable = os.environ.get('P3_DISABLE_GPU', 'FALSE')
+if systemDisable=='FALSE':
+    try:
+        import cupy as cp
+        print("Cupy import successfull. Installed version is:", cp.__version__)
+        gpuEnabled = True
+        import cupy.fft as fftI
+        import cupyx.scipy.special as spcI
+        from  scipy.interpolate import RectBivariateSpline as RectBivariateSplineI
+        import cupyx.scipy.ndimage as scndI
+        from cupyx.scipy.ndimage import rotate as rotateI
+        np = cp
+        trapz = cp.trapz
+    except:
+        print("Cupy import failed. P3 will fall back to CPU use.")
+        cp = np
+        import numpy.fft as fftI
+        import scipy.special as spcI
+        from scipy.interpolate import RectBivariateSpline as RectBivariateSplineI
+        import scipy.ndimage as scndI
+        from scipy.ndimage import rotate as rotateI
+        try:
+            trapz = np.trapezoid
+        except AttributeError:
+            trapz = np.trapz
+else:
+    print("env variable P3_DISABLE_GPU prevents using the GPU.")
+    cp = np
+    import numpy.fft as fftI
+    import scipy.special as spcI
+    from scipy.interpolate import RectBivariateSpline as RectBivariateSplineI
+    import scipy.ndimage as scndI
+    from scipy.ndimage import rotate as rotateI
+    try:
+        trapz = np.trapezoid
+    except AttributeError:
+        trapz = np.trapz
+
+fft = fftI
+spc = spcI
+RectBivariateSpline = RectBivariateSplineI
+scnd = scndI
+rotate = rotateI
+
+def cpuArray(v):
+    if isinstance(v,nnp.ndarray) or isinstance(v, list):
+        return v
+    else:
+        return v.get()
