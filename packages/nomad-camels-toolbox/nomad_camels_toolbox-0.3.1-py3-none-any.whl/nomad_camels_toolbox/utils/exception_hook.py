7a4f22a37a4c735353cd0ca4cbab932abe19243f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 15:03:57 +0000

    Add package nomad-camels-toolbox/0.3.1/nomad_camels_toolbox-0.3.1-py3-none-any.whl

---
+++ packages/nomad-camels-toolbox/nomad_camels_toolbox-0.3.1-py3-none-any.whl/nomad_camels_toolbox/utils/exception_hook.py
@@ -0,0 +1,73 @@
+from traceback import format_tb, print_tb
+from PySide6.QtWidgets import QMessageBox, QTextEdit, QPushButton
+
+
+class ErrorMessage(QMessageBox):
+    """A popUp-box describing an Error.
+
+    Parameters
+    ----------
+    msg : str
+        the error message
+    info_text : str
+        A longer text, explaining the error (usually traceback)
+    parent : QWidget
+        The parent widget of this Messagebox
+    """
+
+    def __init__(self, exc_info, parent=None):
+        tb = format_tb(exc_info[2])
+        print_tb(exc_info[2])
+        msg = str(exc_info[0].__name__)
+        info_text = str(exc_info[1])
+        print(msg)
+        print(info_text)
+        super().__init__(parent)
+        self.setWindowTitle("ERROR")
+        self.setIcon(QMessageBox.Warning)
+        self.setStandardButtons(QMessageBox.Ok)
+        self.setText(msg)
+        if info_text:
+            self.setInformativeText(info_text)
+
+        self.more_info_button = QPushButton("more information")
+        self.more_info_button.clicked.connect(self.show_more_info)
+
+        self.text_edit = QTextEdit()
+        self.text_edit.setReadOnly(True)
+        self.text_edit.setVisible(False)
+        self.text_edit.setPlainText("".join(tb))
+
+        self.layout().addWidget(self.more_info_button, 3, 0, 1, 5)
+        self.layout().addWidget(self.text_edit, 2, 0, 1, 5)
+        self.layout().addWidget(self.button(QMessageBox.Ok), 4, 0, 1, 5)
+        self.showing_more_info = False
+        self.adjustSize()
+
+    def show_more_info(self):
+        if self.showing_more_info:
+            self.text_edit.setVisible(False)
+            self.more_info_button.setText("more information")
+            self.adjustSize()
+            self.showing_more_info = False
+        else:
+            self.text_edit.setVisible(True)
+            self.more_info_button.setText("hide additional information")
+            self.text_edit.setMinimumHeight(500)
+            self.text_edit.setMinimumWidth(800)
+            self.showing_more_info = True
+        self.adjustSize()
+
+
+def exception_hook(*exc_info):
+    """Used to overwrite sys.excepthook, so that an exception does not
+    terminate the program, but simply shows a Message with the exception.
+    If the Exception is a KeyboardInterrupt, it does nothing, so that the
+    interrupt may actually stop the program execution.
+
+    Parameters
+    ----------
+    *exc_info : tuple(class, Exception, traceback)
+        The information for the exception.
+    """
+    ErrorMessage(exc_info).exec()
