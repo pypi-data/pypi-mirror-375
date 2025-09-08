7bfdafa885476f37a015acad102b210a71c74eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 16:04:46 +0000

    Add package mplhep/1.0.0rc1/mplhep-1.0.0rc1.tar.gz

---
+++ packages/mplhep/mplhep-1.0.0rc1.tar.gz/mplhep-1.0.0rc1/docs/source/api.rst
@@ -0,0 +1,76 @@
+=============
+API reference
+=============
+
+Plotting functions
+==================
+
+Primary functions.
+
+.. autofunction:: mplhep.histplot
+
+.. autofunction:: mplhep.hist2dplot
+
+.. # List all modules when appropriately privatized
+   .. automodule:: mplhep.plot
+      :members:
+
+Experiment label helpers
+========================
+
+Experiment specific helpers.
+
+For the effects of the ``label`` method, see also the gallery examples in :ref:`gallery-labels`.
+
+.. autofunction:: mplhep.cms.lumitext
+
+.. autofunction:: mplhep.cms.text
+
+.. autofunction:: mplhep.cms.label
+
+.. autofunction:: mplhep.atlas.text
+
+.. autofunction:: mplhep.atlas.label
+
+.. autofunction:: mplhep.lhcb.text
+
+.. autofunction:: mplhep.lhcb.label
+
+
+Axes helpers
+============
+
+Use all helpers together
+
+.. autofunction:: mplhep.mpl_magic
+
+or one by one.
+
+.. autofunction:: mplhep.plot.ylow
+
+.. autofunction:: mplhep.plot.yscale_legend
+
+.. autofunction:: mplhep.plot.yscale_anchored_text
+
+
+Figure helpers
+==============
+
+.. autofunction:: mplhep.append_axes
+
+.. autofunction:: mplhep.box_aspect
+
+.. autofunction:: mplhep.make_square_add_cbar
+
+.. autofunction:: mplhep.rescale_to_axessize
+
+Legend helpers
+==============
+
+.. autofunction:: mplhep.sort_legend()
+
+
+Styles
+==================
+
+See the :ref:`gallery-styles` section for an overview of the available styles.
---
+++ packages/mplhep/mplhep-1.0.0rc1.tar.gz/mplhep-1.0.0rc1/tests/test_styles.py
@@ -0,0 +1,258 @@
+from __future__ import annotations
+
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import pytest
+from matplotlib.testing.decorators import check_figures_equal
+
+os.environ["RUNNING_PYTEST"] = "true"
+
+import mplhep as hep
+
+"""
+To test run:
+pytest --mpl
+
+When adding new tests, run:
+pytest --mpl-generate-path=tests/baseline
+"""
+
+plt.switch_backend("Agg")
+
+
+# Compare styles
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
+@pytest.mark.mpl_image_compare(style="default", remove_text=False)
+def test_style_atlas():
+    # Test suite does not have Helvetica
+    plt.style.use([hep.style.ATLAS, {"font.sans-serif": ["Tex Gyre Heros"]}])
+    fig, ax = plt.subplots()
+    hep.atlas.label(label="Preliminary")
+
+    return fig
+
+
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
+@pytest.mark.mpl_image_compare(style="default", remove_text=False)
+def test_style_cms():
+    plt.style.use(hep.style.CMS)
+    fig, ax = plt.subplots()
+    hep.cms.label("Preliminary")
+
+    return fig
+
+
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
+@pytest.mark.mpl_image_compare(style="default", remove_text=False)
+def test_style_alice():
+    plt.style.use(hep.style.ALICE)
+    fig, ax = plt.subplots()
+    hep.alice.label("Preliminary")
+
+    return fig
+
+
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
+@pytest.mark.mpl_image_compare(style="default", remove_text=False)
+def test_style_lhcb():
+    plt.style.use([hep.style.LHCb1, {"figure.autolayout": False}])
+    fig, ax = plt.subplots()
+    hep.lhcb.label("Preliminary")
+    return fig
+
+
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
+@pytest.mark.mpl_image_compare(style="default", remove_text=False)
+def test_style_lhcb2():
+    plt.style.use([hep.style.LHCb2, {"figure.autolayout": False}])
+    fig, ax = plt.subplots()
+    hep.lhcb.label("Preliminary")
+    return fig
+
+
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
+@pytest.mark.mpl_image_compare(style="default", remove_text=False)
+def test_style_dune():
+    plt.style.use(hep.style.DUNE)
+    fig, ax = plt.subplots()
+    hep.dune.label(label="Preliminary")
+
+    return fig
+
+
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
+@pytest.mark.mpl_image_compare(style="default", remove_text=False)
+def test_style_dune1():
+    plt.style.use(hep.style.DUNE1)
+    fig, ax = plt.subplots()
+    hep.dune.label(label="Preliminary")
+
+    return fig
+
+
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
+@pytest.mark.mpl_image_compare(style="default", remove_text=False)
+def test_style_plothist():
+    plt.style.use(hep.style.PLOTHIST)
+    fig, ax = plt.subplots()
+    return fig
+
+
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
+@check_figures_equal(extensions=["pdf"])
+@pytest.mark.parametrize(
+    "mplhep_style",
+    [
+        hep.style.ALICE,
+        hep.style.ATLAS,
+        hep.style.CMS,
+        hep.style.DUNE1,
+        hep.style.DUNE,
+        hep.style.LHCb1,
+        hep.style.LHCb2,
+        hep.style.ROOT,
+    ],
+    ids=["ALICE", "ATLAS", "CMS", "DUNE1", "DUNE", "LHCb1", "LHCb2", "ROOT"],
+)
+def test_use_style(fig_test, fig_ref, mplhep_style):
+    hep.rcParams.clear()
+    plt.style.use(mplhep_style)
+    fig_ref.subplots()
+
+    hep.rcParams.clear()
+    hep.style.use(mplhep_style)
+    fig_test.subplots()
+
+
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
+@check_figures_equal(extensions=["pdf"])
+def test_use_style_LHCb_dep(fig_test, fig_ref):
+    hep.rcParams.clear()
+    with pytest.warns(FutureWarning):
+        plt.style.use(hep.style.LHCb)
+    fig_ref.subplots()
+
+    hep.rcParams.clear()
+    hep.style.use(hep.style.LHCb)
+    fig_test.subplots()
+
+
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
+@check_figures_equal(extensions=["pdf"])
+@pytest.mark.parametrize(
+    ("mplhep_style", "str_alias"),
+    [
+        (hep.style.ALICE, "ALICE"),
+        (hep.style.ATLAS, "ATLAS"),
+        (hep.style.CMS, "CMS"),
+        (hep.style.DUNE1, "DUNE1"),
+        (hep.style.DUNE, "DUNE"),
+        (hep.style.LHCb, "LHCb"),
+        (hep.style.LHCb1, "LHCb1"),
+        (hep.style.LHCb2, "LHCb2"),
+        (hep.style.ROOT, "ROOT"),
+    ],
+    ids=["ALICE", "ATLAS", "CMS", "DUNE1", "DUNE", "LHCb", "LHCb1", "LHCb2", "ROOT"],
+)
+def test_use_style_str_alias(fig_test, fig_ref, mplhep_style, str_alias):
+    hep.rcParams.clear()
+    plt.style.use(mplhep_style)
+    fig_ref.subplots()
+
+    hep.rcParams.clear()
+    hep.style.use(str_alias)
+    fig_test.subplots()
+
+
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
+@check_figures_equal(extensions=["pdf"])
+@pytest.mark.parametrize(
+    ("mplhep_style", "str_alias"),
+    [
+        (hep.style.ALICE, "ALICE"),
+        (hep.style.ATLAS, "ATLAS"),
+        (hep.style.CMS, "CMS"),
+        (hep.style.DUNE1, "DUNE1"),
+        (hep.style.DUNE, "DUNE"),
+        (hep.style.LHCb, "LHCb"),
+        (hep.style.LHCb1, "LHCb1"),
+        (hep.style.LHCb2, "LHCb2"),
+        (hep.style.ROOT, "ROOT"),
+    ],
+    ids=["ALICE", "ATLAS", "CMS", "DUNE1", "DUNE", "LHCb", "LHCb1", "LHCb2", "ROOT"],
+)
+def test_use_style_self_consistent(fig_test, fig_ref, mplhep_style, str_alias):
+    hep.rcParams.clear()
+    hep.style.use(mplhep_style)
+    fig_ref.subplots()
+
+    hep.rcParams.clear()
+    hep.style.use(str_alias)
+    fig_test.subplots()
+
+
+@pytest.mark.skipif(sys.platform != "linux", reason="Linux only")
+@check_figures_equal(extensions=["pdf"])
+@pytest.mark.parametrize(
+    ("mplhep_style", "str_alias"),
+    [
+        (hep.style.ALICE, "ALICE"),
+        (hep.style.ATLAS, "ATLAS"),
+        (hep.style.CMS, "CMS"),
+        (hep.style.DUNE1, "DUNE1"),
+        (hep.style.DUNE, "DUNE"),
+        (hep.style.LHCb, "LHCb"),
+        (hep.style.LHCb1, "LHCb1"),
+        (hep.style.LHCb2, "LHCb2"),
+        (hep.style.ROOT, "ROOT"),
+    ],
+    ids=["ALICE", "ATLAS", "CMS", "DUNE1", "DUNE", "LHCb", "LHCb1", "LHCb2", "ROOT"],
+)
+def test_use_style_style_list(fig_test, fig_ref, mplhep_style, str_alias):
+    hep.rcParams.clear()
+    plt.style.use([mplhep_style, {"font.sans-serif": "Comic Sans MS"}])
+    fig_ref.subplots()
+
+    hep.rcParams.clear()
+    hep.style.use([str_alias, {"font.sans-serif": "Comic Sans MS"}])
+    fig_test.subplots()
+
+
+@pytest.mark.mpl_image_compare(style="default")
+def test_labeltext_loc():
+    fig, axs = plt.subplots(1, 4, figsize=(16, 4))
+    for i, ax in enumerate(axs.flatten()):
+        hep.cms.text("Test", loc=i, ax=ax)
+    return fig
+
+
+@pytest.mark.mpl_image_compare(style="default")
+def test_label_loc():
+    fig, axs = plt.subplots(1, 5, figsize=(20, 4))
+    for i, ax in enumerate(axs.flatten()):
+        hep.cms.label("Preliminary", loc=i, ax=ax, lumi=50, data=True)
+    return fig
+
+
+@pytest.mark.mpl_image_compare(style="default")
+def test_pub_loc():
+    fig, axs = plt.subplots(2, 5, figsize=(20, 8))
+    for i, ax in enumerate(axs.flatten()):
+        hep.cms.label(loc=i % 5, ax=ax, lumi=50, pub="arXiv:aaaa.bbbbb", data=(i >= 5))
+    return fig
+
+
+@check_figures_equal(extensions=["pdf"])
+def test_label_config(fig_test, fig_ref):
+    hep.rcParams.label.data = True
+    hep.rcParams.label.lumi = 30
+    hep.rcParams.label.label = "Internal"
+
+    test_ax = fig_test.subplots()
+    hep.cms.label(data=False, ax=test_ax)
+
+    ref_ax = fig_ref.subplots()
+    hep.rcParams.clear()
+    hep.cms.label(data=False, lumi=30, label="Internal", ax=ref_ax)
