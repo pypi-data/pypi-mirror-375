bc82dd1bd569161ea6e1f4ae88a9fc2fc8ddf348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 16:03:56 +0000

    Add package hcloud/2.6.0/hcloud-2.6.0.tar.gz

---
+++ packages/hcloud/hcloud-2.6.0.tar.gz/hcloud-2.6.0/tests/unit/server_types/conftest.py
@@ -0,0 +1,154 @@
+from __future__ import annotations
+
+import pytest
+
+
+@pytest.fixture()
+def server_type_response():
+    return {
+        "server_type": {
+            "id": 1,
+            "name": "cx11",
+            "description": "CX11",
+            "category": "Shared vCPU",
+            "cores": 1,
+            "memory": 1,
+            "disk": 25,
+            "prices": [
+                {
+                    "location": "fsn1",
+                    "price_hourly": {
+                        "net": "1.0000000000",
+                        "gross": "1.1900000000000000",
+                    },
+                    "price_monthly": {
+                        "net": "1.0000000000",
+                        "gross": "1.1900000000000000",
+                    },
+                }
+            ],
+            "storage_type": "local",
+            "cpu_type": "shared",
+            "architecture": "x86",
+            "included_traffic": 21990232555520,
+            "deprecated": True,
+            "deprecation": {
+                "announced": "2023-06-01T00:00:00+00:00",
+                "unavailable_after": "2023-09-01T00:00:00+00:00",
+            },
+        }
+    }
+
+
+@pytest.fixture()
+def two_server_types_response():
+    return {
+        "server_types": [
+            {
+                "id": 1,
+                "name": "cx11",
+                "description": "CX11",
+                "cores": 1,
+                "memory": 1,
+                "disk": 25,
+                "prices": [
+                    {
+                        "location": "fsn1",
+                        "price_hourly": {
+                            "net": "1.0000000000",
+                            "gross": "1.1900000000000000",
+                        },
+                        "price_monthly": {
+                            "net": "1.0000000000",
+                            "gross": "1.1900000000000000",
+                        },
+                    }
+                ],
+                "storage_type": "local",
+                "cpu_type": "shared",
+                "architecture": "x86",
+                "included_traffic": 21990232555520,
+                "deprecated": True,
+                "deprecation": {
+                    "announced": "2023-06-01T00:00:00+00:00",
+                    "unavailable_after": "2023-09-01T00:00:00+00:00",
+                },
+            },
+            {
+                "id": 2,
+                "name": "cx21",
+                "description": "CX21",
+                "cores": 2,
+                "memory": 4.0,
+                "disk": 40,
+                "prices": [
+                    {
+                        "location": "fsn1",
+                        "price_hourly": {
+                            "net": "0.0080000000",
+                            "gross": "0.0095200000000000",
+                        },
+                        "price_monthly": {
+                            "net": "4.9000000000",
+                            "gross": "5.8310000000000000",
+                        },
+                    },
+                    {
+                        "location": "nbg1",
+                        "price_hourly": {
+                            "net": "0.0080000000",
+                            "gross": "0.0095200000000000",
+                        },
+                        "price_monthly": {
+                            "net": "4.9000000000",
+                            "gross": "5.8310000000000000",
+                        },
+                    },
+                ],
+                "storage_type": "local",
+                "cpu_type": "shared",
+                "architecture": "x86",
+                "included_traffic": 21990232555520,
+                "deprecated": False,
+                "deprecation": None,
+            },
+        ]
+    }
+
+
+@pytest.fixture()
+def one_server_types_response():
+    return {
+        "server_types": [
+            {
+                "id": 1,
+                "name": "cx11",
+                "description": "CX11",
+                "cores": 1,
+                "memory": 1,
+                "disk": 25,
+                "prices": [
+                    {
+                        "location": "fsn1",
+                        "price_hourly": {
+                            "net": "1.0000000000",
+                            "gross": "1.1900000000000000",
+                        },
+                        "price_monthly": {
+                            "net": "1.0000000000",
+                            "gross": "1.1900000000000000",
+                        },
+                    }
+                ],
+                "storage_type": "local",
+                "cpu_type": "shared",
+                "architecture": "x86",
+                "included_traffic": 21990232555520,
+                "deprecated": True,
+                "deprecation": {
+                    "announced": "2023-06-01T00:00:00+00:00",
+                    "unavailable_after": "2023-09-01T00:00:00+00:00",
+                },
+            }
+        ]
+    }
---
+++ packages/hcloud/hcloud-2.6.0.tar.gz/hcloud-2.6.0/tests/unit/server_types/test_client.py
@@ -0,0 +1,156 @@
+from __future__ import annotations
+
+from datetime import datetime, timezone
+from unittest import mock
+
+import pytest
+
+from hcloud import Client
+from hcloud.server_types import BoundServerType, ServerTypesClient
+
+
+class TestBoundServerType:
+    @pytest.fixture()
+    def bound_server_type(self, client: Client):
+        return BoundServerType(client.server_types, data=dict(id=14))
+
+    def test_bound_server_type_init(self, server_type_response):
+        bound_server_type = BoundServerType(
+            client=mock.MagicMock(), data=server_type_response["server_type"]
+        )
+
+        assert bound_server_type.id == 1
+        assert bound_server_type.name == "cx11"
+        assert bound_server_type.description == "CX11"
+        assert bound_server_type.category == "Shared vCPU"
+        assert bound_server_type.cores == 1
+        assert bound_server_type.memory == 1
+        assert bound_server_type.disk == 25
+        assert bound_server_type.storage_type == "local"
+        assert bound_server_type.cpu_type == "shared"
+        assert bound_server_type.architecture == "x86"
+        assert bound_server_type.deprecated is True
+        assert bound_server_type.deprecation is not None
+        assert bound_server_type.deprecation.announced == datetime(
+            2023, 6, 1, tzinfo=timezone.utc
+        )
+        assert bound_server_type.deprecation.unavailable_after == datetime(
+            2023, 9, 1, tzinfo=timezone.utc
+        )
+        with pytest.deprecated_call():
+            assert bound_server_type.included_traffic == 21990232555520
+
+
+class TestServerTypesClient:
+    @pytest.fixture()
+    def server_types_client(self, client: Client):
+        return ServerTypesClient(client)
+
+    def test_get_by_id(
+        self,
+        request_mock: mock.MagicMock,
+        server_types_client: ServerTypesClient,
+        server_type_response,
+    ):
+        request_mock.return_value = server_type_response
+
+        server_type = server_types_client.get_by_id(1)
+
+        request_mock.assert_called_with(
+            method="GET",
+            url="/server_types/1",
+        )
+        assert server_type._client is server_types_client
+        assert server_type.id == 1
+        assert server_type.name == "cx11"
+
+    @pytest.mark.parametrize(
+        "params", [{"name": "cx11", "page": 1, "per_page": 10}, {"name": ""}, {}]
+    )
+    def test_get_list(
+        self,
+        request_mock: mock.MagicMock,
+        server_types_client: ServerTypesClient,
+        two_server_types_response,
+        params,
+    ):
+        request_mock.return_value = two_server_types_response
+
+        result = server_types_client.get_list(**params)
+
+        request_mock.assert_called_with(
+            method="GET",
+            url="/server_types",
+            params=params,
+        )
+
+        server_types = result.server_types
+        assert result.meta is not None
+
+        assert len(server_types) == 2
+
+        server_types1 = server_types[0]
+        server_types2 = server_types[1]
+
+        assert server_types1._client is server_types_client
+        assert server_types1.id == 1
+        assert server_types1.name == "cx11"
+
+        assert server_types2._client is server_types_client
+        assert server_types2.id == 2
+        assert server_types2.name == "cx21"
+
+    @pytest.mark.parametrize("params", [{"name": "cx11"}])
+    def test_get_all(
+        self,
+        request_mock: mock.MagicMock,
+        server_types_client: ServerTypesClient,
+        two_server_types_response,
+        params,
+    ):
+        request_mock.return_value = two_server_types_response
+
+        server_types = server_types_client.get_all(**params)
+
+        params.update({"page": 1, "per_page": 50})
+
+        request_mock.assert_called_with(
+            method="GET",
+            url="/server_types",
+            params=params,
+        )
+
+        assert len(server_types) == 2
+
+        server_types1 = server_types[0]
+        server_types2 = server_types[1]
+
+        assert server_types1._client is server_types_client
+        assert server_types1.id == 1
+        assert server_types1.name == "cx11"
+
+        assert server_types2._client is server_types_client
+        assert server_types2.id == 2
+        assert server_types2.name == "cx21"
+
+    def test_get_by_name(
+        self,
+        request_mock: mock.MagicMock,
+        server_types_client: ServerTypesClient,
+        one_server_types_response,
+    ):
+        request_mock.return_value = one_server_types_response
+
+        server_type = server_types_client.get_by_name("cx11")
+
+        params = {"name": "cx11"}
+
+        request_mock.assert_called_with(
+            method="GET",
+            url="/server_types",
+            params=params,
+        )
+
+        assert server_type._client is server_types_client
+        assert server_type.id == 1
+        assert server_type.name == "cx11"
