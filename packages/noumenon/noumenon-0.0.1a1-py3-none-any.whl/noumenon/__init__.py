87dc22878f6663a83f4b1beb567d831a1147f6bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 15:03:59 +0000

    Add package noumenon/0.0.1a1/noumenon-0.0.1a1-py3-none-any.whl

---
+++ packages/noumenon/noumenon-0.0.1a1-py3-none-any.whl/noumenon/__init__.py
@@ -0,0 +1,9 @@
+"""
+Noumenon - A Python package for Noumenon
+"""
+
+__version__ = "0.0.1a1"
+
+def hello():
+    """Simple hello function for package testing."""
+    return "Hello from Noumenon!"
