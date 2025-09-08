d385f975096ab0a5c969abee8d1452607f574ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:04:08 +0000

    Add package lambda-universal-router/1.0.0/lambda_universal_router-1.0.0.tar.gz

---
+++ packages/lambda-universal-router/lambda_universal_router-1.0.0.tar.gz/lambda_universal_router-1.0.0/lambda_universal_router/events/__init__.py
@@ -0,0 +1,31 @@
+from .api_gateway import APIGatewayEvent, APIGatewayRequestContext
+from .sqs import SQSEvent, SQSMessage
+from .s3 import S3Event, S3Record, S3Bucket, S3Object
+from .dynamodb import DynamoDBStreamEvent, DynamoDBStreamRecord
+from .kinesis import KinesisStreamEvent, KinesisRecord
+from .sns import SNSEvent, SNSMessage
+from .eventbridge import EventBridgeEvent, EventBridgeDetail
+from .kafka import KafkaEvent, KafkaRecord
+from .custom import CustomEvent
+
+__all__ = [
+    "APIGatewayEvent",
+    "APIGatewayRequestContext",
+    "SQSEvent",
+    "SQSMessage",
+    "S3Event",
+    "S3Record",
+    "S3Bucket",
+    "S3Object",
+    "DynamoDBStreamEvent",
+    "DynamoDBStreamRecord",
+    "KinesisStreamEvent",
+    "KinesisRecord",
+    "SNSEvent",
+    "SNSMessage",
+    "EventBridgeEvent",
+    "EventBridgeDetail",
+    "KafkaEvent",
+    "KafkaRecord",
+    "CustomEvent"
+]
