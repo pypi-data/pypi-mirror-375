c3792248171ed69662d0fd31af83fc44a8ce46fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 15:04:14 +0000

    Add package rebrandly-otel/0.1.16/rebrandly_otel-0.1.16-py3-none-any.whl

---
+++ packages/rebrandly-otel/rebrandly_otel-0.1.16-py3-none-any.whl/rebrandly_otel/__init__.py
@@ -0,0 +1,16 @@
+# src/__init__.py
+from .rebrandly_otel import *
+
+# Explicitly define what's available
+__all__ = [
+    'lambda_handler',
+    'span',
+    'aws_message_span',
+    'traces',
+    'tracer',
+    'metrics',
+    'logger',
+    'force_flush',
+    'aws_message_handler',
+    'shutdown'
+]
