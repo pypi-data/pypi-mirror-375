b534fa44ffec6e6cfeb1b34341f477b5f9adc77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 16:03:54 +0000

    Add package twitwi/0.21.2/twitwi-0.21.2.tar.gz

---
+++ packages/twitwi/twitwi-0.21.2.tar.gz/twitwi-0.21.2/twitwi/bluesky/constants.py
@@ -0,0 +1,19 @@
+from typing import List, Optional
+
+from twitwi.bluesky.types import BlueskyProfile, BlueskyPost
+
+PROFILE_FIELDS = list(BlueskyProfile.__annotations__.keys())
+
+POST_FIELDS = list(BlueskyPost.__annotations__.keys())
+
+POST_PLURAL_FIELDS = [
+    k
+    for k, v in BlueskyPost.__annotations__.items()
+    if v == List[str] or v == Optional[List[str]]
+]
+
+POST_BOOLEAN_FIELDS = [
+    k
+    for k, v in BlueskyPost.__annotations__.items()
+    if v is bool or v == Optional[bool]
+]
