deab40e615d81544383f82b8ca987d962e826a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:04:00 +0000

    Add package media-downloader/1.0.11/media_downloader-1.0.11.tar.gz

---
+++ packages/media-downloader/media_downloader-1.0.11.tar.gz/media_downloader-1.0.11/media_downloader/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from media_downloader.media_downloader import (
+    media_downloader,
+    main,
+    setup_logging,
+    MediaDownloader,
+)
+
+"""
+media-downloader
+
+Download videos and audio from the internet!
+"""
+
+__all__ = ["media_downloader", "main", "setup_logging", "MediaDownloader"]
