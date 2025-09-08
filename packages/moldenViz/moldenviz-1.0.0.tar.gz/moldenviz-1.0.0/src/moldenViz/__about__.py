34ec2a833404fdadaf4719daa947e1af367363b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:04:02 +0000

    Add package moldenViz/1.0.0/moldenviz-1.0.0.tar.gz

---
+++ packages/moldenViz/moldenviz-1.0.0.tar.gz/moldenviz-1.0.0/src/moldenViz/__about__.py
@@ -0,0 +1,3 @@
+"""Package metadata for moldenViz."""
+
+__version__ = '1.0.0'
---
+++ packages/moldenViz/moldenviz-1.0.0.tar.gz/moldenviz-1.0.0/tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2025-present Faria22 <fariafelipe22@hotmail.com>
+#
+# SPDX-License-Identifier: MIT
