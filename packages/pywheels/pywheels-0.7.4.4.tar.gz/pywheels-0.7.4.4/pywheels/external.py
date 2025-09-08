4e7e23a51cdfc1494d8474398faab4b4882b40e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 16:03:49 +0000

    Add package pywheels/0.7.4.4/pywheels-0.7.4.4.tar.gz

---
+++ packages/pywheels/pywheels-0.7.4.4.tar.gz/pywheels-0.7.4.4/pywheels/external.py
@@ -0,0 +1,29 @@
+import os
+import re
+import ast
+import astor
+import json
+import random
+from time import sleep
+from threading import Lock
+from openai import OpenAI
+from threading import Lock
+from random import normalvariate
+from scipy.optimize import minimize
+from scipy.optimize import differential_evolution
+
+
+__all__ = [
+    "os",
+    "re",
+    "ast",
+    "astor",
+    "random",
+    "json",
+    "Lock",
+    "sleep",
+    "OpenAI",
+    "minimize",
+    "differential_evolution",
+    "normalvariate",
+]
