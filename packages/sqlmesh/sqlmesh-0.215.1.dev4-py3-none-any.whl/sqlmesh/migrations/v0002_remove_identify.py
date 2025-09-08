0ac4fa3865651f6dcc38b923f7b98a54090f6933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:04:07 +0000

    Add package sqlmesh/0.215.1.dev4/sqlmesh-0.215.1.dev4-py3-none-any.whl

---
+++ packages/sqlmesh/sqlmesh-0.215.1.dev4-py3-none-any.whl/sqlmesh/migrations/v0002_remove_identify.py
@@ -0,0 +1,9 @@
+"""Remove identify=True kwarg for rendering sql"""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
