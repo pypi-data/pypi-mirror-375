e961dff48e095ac759ca3cbb9845bcdb04004eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 16:04:45 +0000

    Add package gllm-agents-binary/0.4.2/gllm_agents_binary-0.4.2-cp313-cp313-win_amd64.whl

---
+++ packages/gllm-agents-binary/gllm_agents_binary-0.4.2-cp313-cp313-win_amd64.whl/gllm_agents/examples/hello_world_a2a_multi_agent_coordinator_client_streaming.pyi
@@ -0,0 +1,5 @@
+from gllm_agents.agent import LangGraphAgent as LangGraphAgent
+from gllm_agents.agent.types import A2AClientConfig as A2AClientConfig
+
+async def main() -> None:
+    """Main function demonstrating the Multi-Agent Coordinator streaming client."""
