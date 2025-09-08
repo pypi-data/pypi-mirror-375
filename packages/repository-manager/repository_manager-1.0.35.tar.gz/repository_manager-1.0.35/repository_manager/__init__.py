1f7d62fee4dd583328f78824b033c4f344116c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:03:59 +0000

    Add package repository-manager/1.0.35/repository_manager-1.0.35.tar.gz

---
+++ packages/repository-manager/repository_manager-1.0.35.tar.gz/repository_manager-1.0.35/repository_manager/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+from repository_manager.repository_manager import Git, main, setup_logging
+
+"""
+report-manager
+
+Manage your git projects
+"""
+
+
+__all__ = ["Git", "main", "setup_logging"]
