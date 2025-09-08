4f6cdc17c8936109b99d116f0a7e0c40f615722f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 16:03:49 +0000

    Add package mseep-eagle-mcp-server/0.1.1/mseep_eagle_mcp_server-0.1.1-py3-none-any.whl

---
+++ packages/mseep-eagle-mcp-server/mseep_eagle_mcp_server-0.1.1-py3-none-any.whl/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi_mcp import FastApiMCP
+from routes import (
+    mcp_router,
+    application_router,
+    folder_router,
+    item_router,
+    library_router,
+)
+
+app = FastAPI(
+    title="Eagle MCP API",
+)
+
+# Register routers
+app.include_router(mcp_router)
+app.include_router(application_router)
+app.include_router(folder_router)
+app.include_router(item_router)
+app.include_router(library_router)
+
+mcp = FastApiMCP(
+    app,
+    name="Eagle MCP Server",
+    description="An MCP server for Eagle",
+    exclude_tags=["Disabled"],
+)
+
+mcp.mount()
+
+def main():
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
