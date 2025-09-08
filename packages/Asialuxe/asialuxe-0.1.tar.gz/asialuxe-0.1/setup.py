33f55f66a2834050a0edd8b029ec97a7ad841405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 16:04:55 +0000

    Add package Asialuxe/0.1/asialuxe-0.1.tar.gz

---
+++ packages/Asialuxe/asialuxe-0.1.tar.gz/asialuxe-0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='Asialuxe',
+	version='0.1',
+	author='__token__',
+	author_email='rodnoc@list.ru',
+	description='',
+	packages=['Asialuxe'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.13',
+)
