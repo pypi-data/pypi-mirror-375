3e7ce054d41bbe9f8aac7f4a607ed4fe3b2f3b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 15:04:31 +0000

    Add package dashscope/1.24.4/dashscope-1.24.4-py3-none-any.whl

---
+++ packages/dashscope/dashscope-1.24.4-py3-none-any.whl/dashscope/aigc/__init__.py
@@ -0,0 +1,18 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+
+from .conversation import Conversation, History, HistoryItem
+from .generation import Generation
+from .image_synthesis import ImageSynthesis
+from .multimodal_conversation import MultiModalConversation, AioMultiModalConversation
+from .video_synthesis import VideoSynthesis
+
+__all__ = [
+    Generation,
+    Conversation,
+    HistoryItem,
+    History,
+    ImageSynthesis,
+    MultiModalConversation,
+    AioMultiModalConversation,
+    VideoSynthesis,
+]
