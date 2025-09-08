9ad24e6b7684e43606d300af585c29e6e87f5eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 15:04:31 +0000

    Add package perplexityai/0.6.0/perplexityai-0.6.0.tar.gz

---
+++ packages/perplexityai/perplexityai-0.6.0.tar.gz/perplexityai-0.6.0/api.md
@@ -0,0 +1,23 @@
+# Search
+
+Types:
+
+```python
+from perplexity.types import SearchCreateResponse
+```
+
+Methods:
+
+- <code title="post /search">client.search.<a href="./src/perplexity/resources/search.py">create</a>(\*\*<a href="src/perplexity/types/search_create_params.py">params</a>) -> <a href="./src/perplexity/types/search_create_response.py">SearchCreateResponse</a></code>
+
+# Content
+
+Types:
+
+```python
+from perplexity.types import ContentCreateResponse
+```
+
+Methods:
+
+- <code title="post /content">client.content.<a href="./src/perplexity/resources/content.py">create</a>(\*\*<a href="src/perplexity/types/content_create_params.py">params</a>) -> <a href="./src/perplexity/types/content_create_response.py">ContentCreateResponse</a></code>
---
+++ packages/perplexityai/perplexityai-0.6.0.tar.gz/perplexityai-0.6.0/src/perplexity/resources/__init__.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .search import (
+    SearchResource,
+    AsyncSearchResource,
+    SearchResourceWithRawResponse,
+    AsyncSearchResourceWithRawResponse,
+    SearchResourceWithStreamingResponse,
+    AsyncSearchResourceWithStreamingResponse,
+)
+from .content import (
+    ContentResource,
+    AsyncContentResource,
+    ContentResourceWithRawResponse,
+    AsyncContentResourceWithRawResponse,
+    ContentResourceWithStreamingResponse,
+    AsyncContentResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "SearchResource",
+    "AsyncSearchResource",
+    "SearchResourceWithRawResponse",
+    "AsyncSearchResourceWithRawResponse",
+    "SearchResourceWithStreamingResponse",
+    "AsyncSearchResourceWithStreamingResponse",
+    "ContentResource",
+    "AsyncContentResource",
+    "ContentResourceWithRawResponse",
+    "AsyncContentResourceWithRawResponse",
+    "ContentResourceWithStreamingResponse",
+    "AsyncContentResourceWithStreamingResponse",
+]
