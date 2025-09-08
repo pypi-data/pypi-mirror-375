1b13754291861fa1d922c7a1af90991d49ac076a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:04:42 +0000

    Add package itkwasm-elastix-wasi/1.0.2/itkwasm_elastix_wasi-1.0.2-py3-none-any.whl

---
+++ packages/itkwasm-elastix-wasi/itkwasm_elastix_wasi-1.0.2-py3-none-any.whl/itkwasm_elastix_wasi/__init__.py
@@ -0,0 +1,11 @@
+# Generated file. To retain edits, remove this comment.
+
+"""itkwasm-elastix-wasi: A toolbox for rigid and nonrigid registration of images. WASI implementation."""
+
+from .default_parameter_map import default_parameter_map
+from .elastix import elastix
+from .read_parameter_files import read_parameter_files
+from .transformix import transformix
+from .write_parameter_files import write_parameter_files
+
+from ._version import __version__
