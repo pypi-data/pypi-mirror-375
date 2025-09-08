539fb712ede4123ae25c7f09166c1ea2ba99e1b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 15:04:12 +0000

    Add package perplexityai/0.5.0/perplexityai-0.5.0.tar.gz

---
+++ packages/perplexityai/perplexityai-0.5.0.tar.gz/perplexityai-0.5.0/src/perplexity/types/__init__.py
@@ -0,0 +1,6 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .search_create_params import SearchCreateParams as SearchCreateParams
+from .search_create_response import SearchCreateResponse as SearchCreateResponse
