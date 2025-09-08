a5e33c98f2777ccb3c8df1e335b897f2ee5509a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 15:04:31 +0000

    Add package fake-bpy-module/20250908/fake_bpy_module-20250908-py3-none-any.whl

---
+++ packages/fake-bpy-module/fake_bpy_module-20250908-py3-none-any.whl/bl_operators/mesh/__init__.pyi
@@ -0,0 +1,121 @@
+import typing
+import collections.abc
+import typing_extensions
+import numpy.typing as npt
+import _bpy_types
+import bpy.types
+
+class MeshMirrorUV(_bpy_types.Operator):
+    """Copy mirror UV coordinates on the X axis based on a mirrored mesh"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def do_mesh_mirror_UV(self, mesh, DIR) -> None:
+        """
+
+        :param mesh:
+        :param DIR:
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+    @classmethod
+    def poll(cls, context) -> None:
+        """
+
+        :param context:
+        """
+
+class MeshSelectNext(_bpy_types.Operator):
+    """Select the next element (using selection order)"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+    @classmethod
+    def poll(cls, context) -> None:
+        """
+
+        :param context:
+        """
+
+class MeshSelectPrev(_bpy_types.Operator):
+    """Select the previous element (using selection order)"""
+
+    bl_idname: typing.Any
+    bl_label: typing.Any
+    bl_options: typing.Any
+    bl_rna: typing.Any
+    id_data: typing.Any
+
+    def bl_rna_get_subclass(self) -> bpy.types.Struct:
+        """
+
+        :return: The RNA type or default when not found.
+        :rtype: bpy.types.Struct
+        """
+
+    def bl_rna_get_subclass_py(self) -> typing.Any:
+        """
+
+        :return: The class or default when not found.
+        :rtype: typing.Any
+        """
+
+    def execute(self, context) -> None:
+        """
+
+        :param context:
+        """
+
+    @classmethod
+    def poll(cls, context) -> None:
+        """
+
+        :param context:
+        """
