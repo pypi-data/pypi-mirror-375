80b35ff29afa0bb5410e7fa28f67478265efcecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 16:04:44 +0000

    Add package mesures/3.3.0/mesures-3.3.0.tar.gz

---
+++ packages/mesures/mesures-3.3.0.tar.gz/mesures-3.3.0/mesures/autoconsumo.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+from mesures.dates import *
+from mesures.headers import AUTOCONSUMO_HEADER as columns
+from mesures.parsers.dummy_data import DummyKeys
+from mesures.utils import check_line_terminator_param
+import os
+import numpy as np
+import pandas as pd
+
+
+class AUTOCONSUMO(object):
+    def __init__(self, data, distributor=None, compression='bz2', version=0):
+        """
+        :param data: list of dicts or absolute file_path
+        :param distributor: str distributor REE code
+        :param compression: 'bz2', 'gz'... OR False otherwise
+        """
+        data = DummyKeys(data).data
+        self.file = self.reader(data)
+        self.generation_date = datetime.now()
+        self.prefix = 'AUTOCONSUMO'
+        self.version = version
+        self.default_compression = compression
+        self.distributor = distributor
+
+    def __repr__(self):
+        return "{}: {}".format(self.prefix, self.filename)
+
+    def __eq__(self, other):
+        return self.file.equals(other.file)
+
+    def __add__(self, other):
+        return self.file.append(other.file)
+
+    def __len__(self):
+        return len(self.file)
+
+    @property
+    def filename(self):
+        if self.default_compression:
+            return "{prefix}_{distributor}_{timestamp}.{version}.{compression}".format(
+                prefix=self.prefix, distributor=self.distributor,
+                timestamp=self.generation_date.strftime('%Y%m%d'), version=self.version,
+                compression=self.default_compression
+            )
+        else:
+            return "{prefix}_{distributor}_{timestamp}.{version}".format(
+                prefix=self.prefix, distributor=self.distributor,
+                timestamp=self.generation_date.strftime('%Y%m%d'), version=self.version
+            )
+
+    @property
+    def caus(self):
+        return list(set(self.file['cau']))
+
+    @property
+    def number_of_caus(self):
+        return len(list(set(self.file['cau'])))
+
+    def reader(self, file_path):
+        if isinstance(file_path, str):
+            df = pd.read_csv(
+                file_path, sep=';', names=columns
+            )
+        elif isinstance(file_path, list):
+            df = pd.DataFrame(data=file_path)
+        else:
+            raise Exception("Filepath must be an str or a list")
+
+        df['data_baixa'] = df['data_baixa'].apply(
+            lambda x: REE_END_DATE if not isinstance(x, pd.Timestamp) else x.strftime('%Y%m%d'))
+        df['data_alta'] = df['data_alta'].apply(lambda x: x.strftime('%Y%m%d'))
+        df['emmagatzematge'] = np.where(df['emmagatzematge'], 'S', 'N')
+        #df['potencia_nominal'] = np.where(df['cil'], '', df['potencia_nominal'])
+        df['subgrup'] = df['subgrup'].apply(lambda x: '{}.{}.{}'.format(x[0], x[1], x[2]) if x != '' else '')
+        df['tipus_antiabocament'] = (
+            df['tipus_antiabocament'].apply(lambda x: '' if (x == 0 or not x or x == '') else x))
+        df = df[columns]
+        return df
+
+    def writer(self):
+        """
+        :return: file path of generated AUTOCONSUMO File
+        """
+        file_path = os.path.join('/tmp', self.filename)
+        kwargs = {'sep': ';',
+                  'header': False,
+                  'columns': columns,
+                  'index': False,
+                  check_line_terminator_param(): ';\n'
+                  }
+        if self.default_compression:
+            kwargs.update({'compression': self.default_compression})
+
+        self.file.to_csv(file_path, **kwargs)
+        return file_path
---
+++ packages/mesures/mesures-3.3.0.tar.gz/mesures-3.3.0/mesures/cilcau.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from mesures.dates import *
+from mesures.cupscau import CUPSCAU
+from mesures.headers import CILCAU_HEADER as columns
+from mesures.utils import check_line_terminator_param
+import os
+import numpy as np
+import pandas as pd
+
+
+class CILCAU(CUPSCAU):
+    def __init__(self, data, distributor=None, compression='bz2', version=0):
+        """
+        :param data: list of dicts or absolute file_path
+        :param distributor: str distributor REE code
+        :param compression: 'bz2', 'gz'... OR False otherwise
+        """
+        super(CILCAU, self).__init__(data, distributor, compression, version=version)
+        self.prefix = 'CILCAU'
+
+    @property
+    def cils(self):
+        return list(set(self.file['cil']))
+
+    @property
+    def number_of_cils(self):
+        return len(list(set(self.file['cil'])))
+
+    def reader(self, file_path):
+        if isinstance(file_path, str):
+            df = pd.read_csv(
+                file_path, sep=';', names=columns
+            )
+        elif isinstance(file_path, list):
+            df = pd.DataFrame(data=file_path)
+        else:
+            raise Exception("Filepath must be an str or a list")
+
+        df['data_baixa'] = df['data_baixa'].apply(
+            lambda x: REE_END_DATE if not isinstance(x, pd.Timestamp) else x.strftime('%Y%m%d'))
+        df['data_alta'] = df['data_alta'].apply(lambda x: x.strftime('%Y%m%d'))
+        try:
+            df['comentari'] = np.where(df['comentari'], df['comentari'], '')
+        except KeyError:
+            df['comentari'] = ''
+        return df[columns]
+
+    def writer(self):
+        """
+        :return: file path of generated CUPSCAU File
+        """
+        file_path = os.path.join('/tmp', self.filename)
+        kwargs = {'sep': ';',
+                  'header': False,
+                  'columns': columns,
+                  'index': False,
+                  check_line_terminator_param(): ';\n'
+                  }
+        if self.default_compression:
+            kwargs.update({'compression': self.default_compression})
+
+        self.file.to_csv(file_path, **kwargs)
+        return file_path
