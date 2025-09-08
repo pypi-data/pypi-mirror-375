5ff175546bcf30c366c3d32e0486962668770aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:04:06 +0000

    Add package natlog/2.1.2/natlog-2.1.2.tar.gz

---
+++ packages/natlog/natlog-2.1.2.tar.gz/natlog-2.1.2/natlog/__main__.py
@@ -0,0 +1,26 @@
+import sys
+from natlog import get_version
+from natlog.natlog import natlog
+
+print("Natlog: ", get_version())
+print(
+    """Usage: python3 -m <file_name> <goal>
+
+    <file_name>, if present, must end with .nat, .pro or .pl
+
+    <goal> if present, is a string representing a Natlog goal to query <file_name>
+
+    if both absent, the REPL starts, with the only the library loaded.
+    """
+)
+k = len(sys.argv)
+file_name, goal = None, None
+if k > 1:
+    file_name = sys.argv[1]
+    if k > 2:
+        goal = sys.argv[2:]
+        print("CML GOAL:", len(sys.argv), goal)
+else:
+    file_name = None
+
+natlog(file_name=file_name, goal=goal)
