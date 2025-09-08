be0f81cbd000124554db4282ed7b5e647d888e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 16:04:49 +0000

    Add package social-auth-pipeline-app/0.2.0/social_auth_pipeline_app-0.2.0-py3-none-any.whl

---
+++ packages/social-auth-pipeline-app/social_auth_pipeline_app-0.2.0-py3-none-any.whl/fullname_plugin/apps.py
@@ -0,0 +1,19 @@
+from django.apps import AppConfig
+
+class SocialAuthPipelineConfig(AppConfig):
+    name = "fullname_plugin"
+    verbose_name = "social_auth_pipeline_config"
+
+    plugin_app = {
+        'settings_config': {
+            'cms.djangoapp': {
+                'common': {'relative_path': 'settings.common'},
+                'production': {'relative_path': 'settings.production'},
+            },
+            'lms.djangoapp': {
+                'common': {'relative_path': 'settings.common'},
+                'production': {'relative_path': 'settings.production'},
+            },
+        },
+    }
+
