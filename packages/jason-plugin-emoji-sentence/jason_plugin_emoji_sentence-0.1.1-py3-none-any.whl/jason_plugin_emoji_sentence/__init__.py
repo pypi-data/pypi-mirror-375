e293e26146599074c2095e2a99e68b781247cca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 15:04:19 +0000

    Add package jason-plugin-emoji-sentence/0.1.1/jason_plugin_emoji_sentence-0.1.1-py3-none-any.whl

---
+++ packages/jason-plugin-emoji-sentence/jason_plugin_emoji_sentence-0.1.1-py3-none-any.whl/jason_plugin_emoji_sentence/__init__.py
@@ -0,0 +1,28 @@
+"""
+This is a add emoji to sentence function.
+
+"""
+
+import asyncio
+from mcp.server.fastmcp import FastMCP
+
+from emoji_translator.core import translate_with_emoji
+
+# Create an MCP server
+mcp = FastMCP("JasonEmojiSentencePlugin")
+
+
+@mcp.tool()
+def emoji_translate(text: str) -> str:
+    """Translate text into emoji-rich version"""
+    return translate_with_emoji(text)
+
+
+
+async def enter_main_function():
+    await mcp.run_stdio_async()
+
+
+
+def main() -> None:
+    asyncio.run(enter_main_function())
