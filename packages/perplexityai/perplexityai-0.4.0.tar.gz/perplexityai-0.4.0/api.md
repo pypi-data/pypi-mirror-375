6a42ced0b0f1650e91cdebb40c472a6e4f32dffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 03:19:24 +0000

    Add package perplexityai/0.4.0/perplexityai-0.4.0.tar.gz

---
+++ packages/perplexityai/perplexityai-0.4.0.tar.gz/perplexityai-0.4.0/api.md
@@ -0,0 +1,11 @@
+# Search
+
+Types:
+
+```python
+from perplexity.types import SearchCreateResponse
+```
+
+Methods:
+
+- <code title="post /search">client.search.<a href="./src/perplexity/resources/search.py">create</a>(\*\*<a href="src/perplexity/types/search_create_params.py">params</a>) -> <a href="./src/perplexity/types/search_create_response.py">SearchCreateResponse</a></code>
---
+++ packages/perplexityai/perplexityai-0.4.0.tar.gz/perplexityai-0.4.0/src/perplexity/resources/__init__.py
@@ -0,0 +1,19 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .search import (
+    SearchResource,
+    AsyncSearchResource,
+    SearchResourceWithRawResponse,
+    AsyncSearchResourceWithRawResponse,
+    SearchResourceWithStreamingResponse,
+    AsyncSearchResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "SearchResource",
+    "AsyncSearchResource",
+    "SearchResourceWithRawResponse",
+    "AsyncSearchResourceWithRawResponse",
+    "SearchResourceWithStreamingResponse",
+    "AsyncSearchResourceWithStreamingResponse",
+]
