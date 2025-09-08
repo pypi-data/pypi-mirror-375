6951c522c7a207395b80b2aad3f0193094f1207e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 16:04:41 +0000

    Add package ruffle/0.0.1a3/ruffle-0.0.1a3-py3-none-any.whl

---
+++ packages/ruffle/ruffle-0.0.1a3-py3-none-any.whl/ruffle/trainer.py
@@ -0,0 +1,57 @@
+import torch
+from lightning.pytorch.callbacks import (
+    EarlyStopping,
+    LearningRateMonitor,
+    ModelCheckpoint,
+    RichProgressBar,
+)
+from lightning.pytorch.cli import ArgsType, LightningArgumentParser, LightningCLI
+
+from ruffle.dataloaders import JigsawDataModule
+from ruffle.models import Classifier
+
+# See https://pytorch.org/docs/stable/generated/torch.set_float32_matmul_precision.html
+torch.set_float32_matmul_precision("medium")
+
+
+class MyLightningCLI(LightningCLI):
+    def add_arguments_to_parser(self, parser: LightningArgumentParser) -> None:
+        parser.link_arguments("data.labels", "model.label_names")
+        parser.link_arguments(
+            "data.labels", "model.num_labels", compute_fn=lambda x: len(x)
+        )
+
+
+def cli_main(args: ArgsType = None) -> None:
+    MyLightningCLI(
+        model_class=Classifier,
+        datamodule_class=JigsawDataModule,
+        trainer_defaults={
+            "max_epochs": 20,
+            "deterministic": True,
+            "callbacks": [
+                EarlyStopping(
+                    monitor="val_loss",
+                    mode="min",
+                    patience=3,
+                    verbose=True,
+                ),
+                ModelCheckpoint(
+                    monitor="val_loss",
+                    mode="min",
+                    filename="{epoch:02d}-{val_loss:.4f}",
+                    save_top_k=1,
+                    verbose=True,
+                ),
+                LearningRateMonitor(logging_interval="epoch"),
+                RichProgressBar(),
+            ],
+            "logger": True,
+        },
+        seed_everything_default=1234,
+        args=args,
+    )
+
+
+if __name__ == "__main__":
+    cli_main()
