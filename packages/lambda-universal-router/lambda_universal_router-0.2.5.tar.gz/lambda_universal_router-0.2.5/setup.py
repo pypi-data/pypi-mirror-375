e19474cffb6a49d4ebd96e4325b67a183f432e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:04:00 +0000

    Add package lambda-universal-router/0.2.5/lambda_universal_router-0.2.5.tar.gz

---
+++ packages/lambda-universal-router/lambda_universal_router-0.2.5.tar.gz/lambda_universal_router-0.2.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lambda-universal-router",
+    version="0.2.5",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+    author="Raumil Dhandhukia",
+    author_email="raumild@gmail.com",
+    description="A flexible AWS Lambda event router with support for multiple event sources",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/raumildhandhukia/lambda-universal-router",
+    project_urls={
+        "Documentation": "https://raumildhandhukia.github.io/lambda-universal-router/",
+        "Source": "https://github.com/raumildhandhukia/lambda-universal-router",
+        "Changelog": "https://raumildhandhukia.github.io/lambda-universal-router/changelog",
+        "Contributing": "https://raumildhandhukia.github.io/lambda-universal-router/contributing",
+        "License": "https://github.com/raumildhandhukia/lambda-universal-router/blob/main/LICENSE",
+        "Issue Tracker": "https://github.com/raumildhandhukia/lambda-universal-router/issues",
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+)
