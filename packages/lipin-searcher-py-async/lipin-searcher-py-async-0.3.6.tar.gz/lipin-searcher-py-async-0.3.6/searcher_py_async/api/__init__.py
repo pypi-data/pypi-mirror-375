47f40d9a15e90b0700c1fba5542dc11b586cda2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 15:03:52 +0000

    Add package lipin-searcher-py-async/0.3.6/lipin-searcher-py-async-0.3.6.tar.gz

---
+++ packages/lipin-searcher-py-async/lipin-searcher-py-async-0.3.6.tar.gz/lipin-searcher-py-async-0.3.6/searcher_py_async/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from searcher_py_async.api.dict_api import DictApi
+from searcher_py_async.api.embedding_api import EmbeddingApi
+from searcher_py_async.api.policy_api import PolicyApi
+from searcher_py_async.api.search_api import SearchApi
+
