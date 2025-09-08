14b5bc57dd1e2c0e9ff6da9363718c75760eb4dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:04:14 +0000

    Add package imj/1.1.1/imj-1.1.1-py3-none-any.whl

---
+++ packages/imj/imj-1.1.1-py3-none-any.whl/imj/__init__.py
@@ -0,0 +1,43 @@
+import requests
+
+BASE = 'https://i.marcusj.org'
+
+class IMJ(object):
+    def __init__(self, img_id: str, key: str, base_url: str=BASE, disp_url: str=BASE):
+        self.id = img_id
+        self.base = base_url
+        self.disp = disp_url
+        self.key = key
+
+    @property
+    def viewer(self) -> str:
+        return f"{self.disp}/view/{self.id}"
+    
+    @property
+    def url(self) -> str:
+        return f"{self.disp}/image/{self.id}"
+    
+    def shorten(self):
+        return requests.get(f"{self.base}/shorten/{self.id}").url.replace(self.base, self.disp)
+    
+    def delete(self):
+        return requests.get(f"{self.base}/delete/{self.id}?key={self.key}").text
+
+def upload_file(fn: str) -> IMJ:
+    r = requests.post(f"{BASE}/upload", files={
+        'image': open(fn, 'rb')
+    })
+    return IMJ(*(r.url.split('/')[-1].split('&key=')))
+
+def upload(raw: bytes, fn: str='image.png', mimetype: str='image/png') -> IMJ:
+    r = requests.post(f"{BASE}/upload", files={
+        'image': (
+            fn,
+            raw,
+            mimetype,
+        )
+    })
+    return IMJ(r.url.split('/')[-1])
+
+def delete(img_id: str, key: str) -> str:
+    return requests.get(f"{BASE}/delete/{img_id}?key={key}").text
