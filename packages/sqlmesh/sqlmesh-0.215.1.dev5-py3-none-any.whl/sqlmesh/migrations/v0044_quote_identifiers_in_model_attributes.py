4c7898bc3f2ff808bc8d2aff803f0b67ef6d3f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:04:06 +0000

    Add package sqlmesh/0.215.1.dev5/sqlmesh-0.215.1.dev5-py3-none-any.whl

---
+++ packages/sqlmesh/sqlmesh-0.215.1.dev5-py3-none-any.whl/sqlmesh/migrations/v0044_quote_identifiers_in_model_attributes.py
@@ -0,0 +1,9 @@
+"""Quoted identifiers in model SQL attributes."""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
