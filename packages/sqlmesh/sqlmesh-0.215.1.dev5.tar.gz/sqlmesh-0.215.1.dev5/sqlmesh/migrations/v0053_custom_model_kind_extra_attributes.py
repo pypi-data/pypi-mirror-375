1d9d460f534df029abebddf6db00e8cd0e9574d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:04:07 +0000

    Add package sqlmesh/0.215.1.dev5/sqlmesh-0.215.1.dev5.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.215.1.dev5.tar.gz/sqlmesh-0.215.1.dev5/sqlmesh/migrations/v0053_custom_model_kind_extra_attributes.py
@@ -0,0 +1,9 @@
+"""Add batch_size, batch_concurrency, and batch_interval to the CUSTOM model kind."""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
