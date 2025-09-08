2402ab0238dffe7e7490a218d81af5acd94e55d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 15:04:17 +0000

    Add package fastapi-router-viz/0.1.9/fastapi_router_viz-0.1.9-py3-none-any.whl

---
+++ packages/fastapi-router-viz/fastapi_router_viz-0.1.9-py3-none-any.whl/fastapi_router_viz/type.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from typing import Literal
+
+
+@dataclass
+class FieldInfo:
+    name: str
+    type_name: str
+
+@dataclass
+class Tag:
+    id: str
+    name: str
+
+@dataclass
+class Route:
+    id: str
+    name: str
+
+@dataclass
+class SchemaNode:
+    id: str
+    name: str
+    fields: list[FieldInfo]
+    is_model: bool = False  # mapping to entities such as orm model
+
+@dataclass
+class Link:
+    source: str
+    source_origin: str
+    target: str
+    target_origin: str
+    # is_internal: bool = False
+    type: Literal['child', 'parent', 'entry', 'subset']
