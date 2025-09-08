eb50d34858dc765aa391318c4d23140bdd23646a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 16:04:11 +0000

    Add package PySrDaliGateway/0.9.1/pysrdaligateway-0.9.1.tar.gz

---
+++ packages/PySrDaliGateway/pysrdaligateway-0.9.1.tar.gz/pysrdaligateway-0.9.1/PySrDaliGateway/exceptions.py
@@ -0,0 +1,11 @@
+"""Custom exceptions for DALI Gateway operations"""
+
+from typing import Optional
+
+
+class DaliGatewayError(Exception):
+    """Base exception for DALI Gateway operations"""
+
+    def __init__(self, message: str, gw_sn: Optional[str] = None):
+        super().__init__(message)
+        self.gw_sn = gw_sn
