93c1b950f53da0e8c3667647233a1c8107482dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:04:05 +0000

    Add package svc-infra/0.1.152/svc_infra-0.1.152-py3-none-any.whl

---
+++ packages/svc-infra/svc_infra-0.1.152-py3-none-any.whl/svc_infra/cli/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import typer
+
+from svc_infra.cli.alembic_cmds import register as register_alembic
+from svc_infra.cli.scaffold_cmds import register as register_scaffold
+from svc_infra.cli.app_cmds import register as register_app, HELP
+from svc_infra.cli._bootstrap import pre_cli as _pre
+
+app = typer.Typer(no_args_is_help=True, add_completion=False, help=HELP)
+
+# Attach all commands to the ONE app
+_pre(app)
+register_app(app)
+register_alembic(app)
+register_scaffold(app)
+
+if __name__ == "__main__":
+    app()
