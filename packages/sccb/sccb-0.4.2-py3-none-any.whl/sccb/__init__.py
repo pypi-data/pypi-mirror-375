acfc90d010a48c1df019928ed38a6712493fbd0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 03:19:24 +0000

    Add package sccb/0.4.2/sccb-0.4.2-py3-none-any.whl

---
+++ packages/sccb/sccb-0.4.2-py3-none-any.whl/sccb/__init__.py
@@ -0,0 +1,9 @@
+"""
+SCCB - Shortcut Clipboard + Command Binder
+
+A customizable CLI tool for managing command shortcuts and clipboard snippets.
+"""
+
+__version__ = "0.4.2"
+__author__ = "Ajinkya Dhamdhere"
+__email__ = "aj.dhamdhere@gmail.com"
