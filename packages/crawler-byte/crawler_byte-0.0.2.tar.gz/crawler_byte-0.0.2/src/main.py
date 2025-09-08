55c84b2efbe6e2ef88e9d351263cf6de7997c04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 15:04:19 +0000

    Add package crawler-byte/0.0.2/crawler_byte-0.0.2.tar.gz

---
+++ packages/crawler-byte/crawler_byte-0.0.2.tar.gz/crawler_byte-0.0.2/src/main.py
@@ -0,0 +1,16 @@
+from crawlerbyte.crawler import crawl
+import os
+
+
+base_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+
+
+def main():
+    result = crawl(base_dir, max_depth=1000, action=lambda f: f)
+    print("Crawled Data:", result)
+
+
+
+
+if __name__ == "__main__":
+    main()
