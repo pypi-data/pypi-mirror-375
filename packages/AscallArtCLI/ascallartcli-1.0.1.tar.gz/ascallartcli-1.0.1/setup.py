e9790a205dc4bbca34feac8afb542c16775951cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:04:42 +0000

    Add package AscallArtCLI/1.0.1/ascallartcli-1.0.1.tar.gz

---
+++ packages/AscallArtCLI/ascallartcli-1.0.1.tar.gz/ascallartcli-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="AscallArtCLI",
+    version="1.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    description="Short description here",
+    long_description=long_description,          
+    long_description_content_type="text/markdown", 
+    author="Mohammed Al-Baqer",
+    url="https://pypi.org/project/AscallArtCLI/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
+
