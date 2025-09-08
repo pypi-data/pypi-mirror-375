950fee914423870749ddb1262d0f2b4c146c14bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 15:03:57 +0000

    Add package sinabs/3.0.4.dev25/sinabs-3.0.4.dev25-py3-none-any.whl

---
+++ packages/sinabs/sinabs-3.0.4.dev25-py3-none-any.whl/sinabs/backend/dynapcnn/chips/speck2e.py
@@ -0,0 +1,30 @@
+import samna
+from samna.speck2e.configuration import SpeckConfiguration
+
+from sinabs.backend.dynapcnn.dynapcnn_layer import DynapcnnLayer
+
+from .dynapcnn import DynapcnnConfigBuilder
+
+
+# Inherit DynapCNNConfigBuilder to share implementation with other DynapCNN/Speck devices
+class Speck2EConfigBuilder(DynapcnnConfigBuilder):
+    @classmethod
+    def get_samna_module(cls):
+        return samna.speck2e
+
+    @classmethod
+    def get_default_config(cls) -> "SpeckConfiguration":
+        return SpeckConfiguration()
+
+    @classmethod
+    def get_input_buffer(cls):
+        return samna.BasicSourceNode_speck2e_event_speck2e_input_event()
+
+    @classmethod
+    def get_output_buffer(cls):
+        return samna.BasicSinkNode_speck2e_event_output_event()
+
+    @classmethod
+    def get_dynapcnn_layer_config_dict(cls, layer: DynapcnnLayer):
+        config_dict = super().get_dynapcnn_layer_config_dict(layer=layer)
+        return config_dict
