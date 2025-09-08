0b00c771ddf97c31531d59c35e47abb3e6502b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 16:04:59 +0000

    Add package symmorademo/0.7.0/symmorademo-0.7.0.tar.gz

---
+++ packages/symmorademo/symmorademo-0.7.0.tar.gz/symmorademo-0.7.0/src/symmorademo/core.py
@@ -0,0 +1,6 @@
+def get_hello(gretting: str = "Symmora World" ) -> str:
+    return f"{"*"*5} version 7 {"*"*3} : Hello, {gretting}!!!"
+
+def symmora_add(a: int, b: int) -> int:
+    return a + b
+
