b77b9c0bdf288718d9aab5634b1f23299bee3ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 14:04:41 +0000

    Add package itkwasm-elastix-wasi/1.0.2/itkwasm_elastix_wasi-1.0.2.tar.gz

---
+++ packages/itkwasm-elastix-wasi/itkwasm_elastix_wasi-1.0.2.tar.gz/itkwasm_elastix_wasi-1.0.2/tests/test_elastix_wasm.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+from dataclasses import asdict
+import json
+
+import pytest
+
+from itkwasm_compare_images import compare_images
+from itkwasm_image_io import imread, imwrite
+
+from itkwasm_elastix_wasi import elastix
+
+def test_elastix_wasm():
+    parameter_object_filename = 'parameters_single.json'
+    parameter_object_filepath = Path(__file__).parent.parent.parent.parent / 'test' / 'data' / 'input' / parameter_object_filename
+    with open(parameter_object_filepath, 'r') as f:
+        parameter_object = json.load(f)
+
+    test_data_output_dir = Path(__file__).parent.parent.parent.parent / 'test' / 'data' / 'output'
+    test_data_output_dir.mkdir(parents=True, exist_ok=True)
+
+    fixed_filename = 'CT_2D_head_fixed.mha'
+    fixed_filepath = Path(__file__).parent.parent.parent.parent / 'test' / 'data' / 'input' / fixed_filename
+    fixed_image = imread(fixed_filepath)
+
+    moving_filename = 'CT_2D_head_moving.mha'
+    moving_filepath = Path(__file__).parent.parent.parent.parent / 'test' / 'data' / 'input' / moving_filename
+    moving_image = imread(moving_filepath)
+
+    result_image, _transform, _transform_parameter_object = elastix(parameter_object, fixed_image, moving_image)
+
+    output_dir = Path(__file__).parent.parent.parent.parent  / 'test' / 'data' / 'output'
+    output_dir.mkdir(parents=True, exist_ok=True)
+    output_filename = output_dir / fixed_filename.replace('fixed.mha', 'result.mha')
+
+    imwrite(result_image, output_filename)
+
+    expected_filename = Path(__file__).parent.parent.parent.parent / 'test' / 'data' / 'baseline' / fixed_filename.replace('fixed.mha', 'result.mha')
+    expected = imread(expected_filename)
+
+    metrics, diff, diffuchar = compare_images(result_image, baseline_images=[expected,])
+    assert metrics['almostEqual']
