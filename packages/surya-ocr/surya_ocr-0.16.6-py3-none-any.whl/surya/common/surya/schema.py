b0d680cdd981ca496ff6730b9826fd8c07b1fe77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 05:05:02 +0000

    Add package surya-ocr/0.16.6/surya_ocr-0.16.6-py3-none-any.whl

---
+++ packages/surya-ocr/surya_ocr-0.16.6-py3-none-any.whl/surya/common/surya/schema.py
@@ -0,0 +1,13 @@
+class TaskNames:
+    block_without_boxes = "block_without_boxes"
+    ocr_with_boxes = "ocr_with_boxes"
+    ocr_without_boxes = "ocr_without_boxes"
+    layout = "layout"
+
+
+TASK_NAMES = [
+    TaskNames.block_without_boxes,
+    TaskNames.ocr_with_boxes,
+    TaskNames.ocr_without_boxes,
+    TaskNames.layout
+]
