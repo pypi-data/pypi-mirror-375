d1b43fbdae961c47d788fe4993fd67b2b8211027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 16:04:46 +0000

    Add package csj-openapi-mcp/0.1.4/csj_openapi_mcp-0.1.4.tar.gz

---
+++ packages/csj-openapi-mcp/csj_openapi_mcp-0.1.4.tar.gz/csj_openapi_mcp-0.1.4/csjopenapimcp/server.py
@@ -0,0 +1,24 @@
+from fastmcp.server import FastMCP
+
+# ==== start 该部分编写在包中，媒体import即可 ========
+mcp = FastMCP(name="CSJ platform", instructions="use open api query info or edit info.")
+
+
+@mcp.tool(
+    name="获取天气",  # Custom tool name for the LLM
+    description="获取当天当地的天气 location: 地区",  # Custom description
+)
+def get_weather(location: str):
+    return location + " 阴天有雨"
+
+
+# ===== end =======
+
+
+# import 包之后，获取create_server().媒体在本地或者自身服务器上进行部署
+def main():
+    mcp.run(transport="stdio")
+
+
+if __name__ == '__main__':
+    main()
