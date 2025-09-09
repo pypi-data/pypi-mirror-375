e5147c4b2ebe2adee29517adccfb21c790d8f22f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 07:04:30 +0000

    Add package altastata/0.1.16/altastata-0.1.16-py3-none-any.whl

---
+++ packages/altastata/altastata-0.1.16-py3-none-any.whl/altastata/__init__.py
@@ -0,0 +1,32 @@
+"""
+AltaStata Python Package
+A powerful Python package for data processing and machine learning integration with Altastata.
+"""
+
+__version__ = "0.1.16"
+
+from .altastata_functions import AltaStataFunctions
+from .altastata_pytorch_dataset import AltaStataPyTorchDataset
+
+# Lazy import for TensorFlow
+def _import_tensorflow_dataset():
+    from .altastata_tensorflow_dataset import AltaStataTensorFlowDataset
+    return AltaStataTensorFlowDataset
+
+# Create a lazy loader for TensorFlow dataset
+class _LazyTensorFlowDataset:
+    def __init__(self):
+        self._dataset_class = None
+    
+    def __call__(self, *args, **kwargs):
+        if self._dataset_class is None:
+            self._dataset_class = _import_tensorflow_dataset()
+        return self._dataset_class(*args, **kwargs)
+
+AltaStataTensorFlowDataset = _LazyTensorFlowDataset()
+
+__all__ = [
+    'AltaStataFunctions',
+    'AltaStataPyTorchDataset',
+    'AltaStataTensorFlowDataset'
+]
