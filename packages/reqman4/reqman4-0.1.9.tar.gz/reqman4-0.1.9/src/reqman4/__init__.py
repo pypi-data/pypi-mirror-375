5c454118e17373e9ea629bb59e51c5f2b7860351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 05:05:29 +0000

    Add package reqman4/0.1.9/reqman4-0.1.9.tar.gz

---
+++ packages/reqman4/reqman4-0.1.9.tar.gz/reqman4-0.1.9/src/reqman4/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# #############################################################################
+# Copyright (C) 2025 manatlan manatlan[at]gmail(dot)com
+#
+# MIT licence
+#
+# https://github.com/manatlan/reqman4
+# #############################################################################
+
+"""
+Special keys :
+
+ * proxy: <string>          # default None
+ * timeout: <milliseconds>  # default 60_000 ms
+ * root: <string>           # default None
+ * switch: <dict/mapping>   # default {}
+
+"""
