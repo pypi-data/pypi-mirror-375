32304396b93cfd8dd763edc3a2aa2276b3e6da40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 05:05:28 +0000

    Add package pyngo/2.4.1/pyngo-2.4.1-py3-none-any.whl

---
+++ packages/pyngo/pyngo-2.4.1-py3-none-any.whl/pyngo/__init__.py
@@ -0,0 +1,15 @@
+"""Pydantic Package for Adding Models into a Django or Django Rest Framework Project"""
+
+__version__ = "2.4.1"
+
+from .errors import drf_error_details
+from .openapi import ParameterDict, openapi_params
+from .querydict import QueryDictModel, querydict_to_dict
+
+__all__ = (
+    "ParameterDict",
+    "QueryDictModel",
+    "querydict_to_dict",
+    "drf_error_details",
+    "openapi_params",
+)
