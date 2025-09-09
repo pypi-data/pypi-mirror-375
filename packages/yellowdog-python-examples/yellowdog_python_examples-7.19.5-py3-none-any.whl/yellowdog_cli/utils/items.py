63925973762f28536e35bf8031e368310a1288ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 16:05:35 +0000

    Add package yellowdog-python-examples/7.19.5/yellowdog_python_examples-7.19.5-py3-none-any.whl

---
+++ packages/yellowdog-python-examples/yellowdog_python_examples-7.19.5-py3-none-any.whl/yellowdog_cli/utils/items.py
@@ -0,0 +1,65 @@
+"""
+Utility class for YellowDog item types.
+"""
+
+from typing import TypeVar
+
+from yellowdog_client.model import (
+    Allowance,
+    Application,
+    ComputeRequirement,
+    ComputeRequirementTemplateSummary,
+    ComputeSourceTemplate,
+    ComputeSourceTemplateSummary,
+    ConfiguredWorkerPool,
+    Group,
+    Instance,
+    KeyringSummary,
+    MachineImageFamilySummary,
+    Namespace,
+    NamespacePolicy,
+    NamespaceStorageConfiguration,
+    Node,
+    ObjectPath,
+    PermissionDetail,
+    ProvisionedWorkerPool,
+    Role,
+    Task,
+    TaskGroup,
+    User,
+    Worker,
+    WorkerPoolSummary,
+    WorkRequirementSummary,
+)
+
+from yellowdog_cli.utils.cloudwizard_aws_types import AWSAvailabilityZone
+
+Item = TypeVar(
+    "Item",
+    AWSAvailabilityZone,
+    Allowance,
+    Application,
+    ComputeRequirement,
+    ComputeRequirementTemplateSummary,
+    ComputeSourceTemplate,
+    ComputeSourceTemplateSummary,
+    ConfiguredWorkerPool,
+    Group,
+    Instance,
+    KeyringSummary,
+    MachineImageFamilySummary,
+    Namespace,
+    NamespacePolicy,
+    NamespaceStorageConfiguration,
+    Node,
+    ObjectPath,
+    PermissionDetail,
+    ProvisionedWorkerPool,
+    Role,
+    Task,
+    TaskGroup,
+    User,
+    WorkRequirementSummary,
+    Worker,
+    WorkerPoolSummary,
+)
