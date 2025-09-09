da67d88005a9bb39b856821fb97662f6105e37df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 18:05:50 +0000

    Add package django-migrations-ci/0.12/django_migrations_ci-0.12-py3-none-any.whl

---
+++ packages/django-migrations-ci/django_migrations_ci-0.12-py3-none-any.whl/django_migrations_ci/backends/mysql.py
@@ -0,0 +1,39 @@
+from django.db.backends.utils import strip_quotes
+
+from django_migrations_ci import shell
+
+
+def dump(connection, output_file):
+    ctx, env = _ctx(connection.settings_dict)
+    if ctx["port"]:
+        mysqldump = "mysqldump -h {host} -P {port} -u {user} --result-file {output_file} {database}"  # noqa: E501
+    else:
+        mysqldump = (
+            "mysqldump -h {host} -u {user} --result-file {output_file} {database}"
+        )
+    shell.exec(mysqldump.format(output_file=output_file, **ctx), env)
+
+
+def database_exists(connection, database_name):
+    with connection.creation._nodb_cursor() as cursor:
+        cursor.execute(
+            "SELECT 1 FROM information_schema.schemata WHERE schema_name = %s",
+            [strip_quotes(database_name)],
+        )
+        return cursor.fetchone() is not None
+
+
+def _ctx(db_conf):
+    env = {"MYSQL_PWD": db_conf["PASSWORD"]}
+    try:
+        database = db_conf["TEST"]["NAME"] or db_conf["NAME"]
+    except KeyError:
+        database = db_conf["NAME"]
+
+    data = {
+        "host": db_conf["HOST"],
+        "port": db_conf["PORT"],
+        "user": db_conf["USER"],
+        "database": database,
+    }
+    return data, env
---
+++ packages/django-migrations-ci/django_migrations_ci-0.12-py3-none-any.whl/django_migrations_ci/backends/postgresql.py
@@ -0,0 +1,31 @@
+from django_migrations_ci import shell
+
+
+def dump(connection, output_file):
+    ctx, env = _ctx(connection.settings_dict)
+    if ctx["port"]:
+        pg_dump = "pg_dump --no-owner --inserts -h {host} -p {port} -U {user} -f {output_file} {database}"  # noqa: E501
+    else:
+        pg_dump = "pg_dump --no-owner --inserts -h {host} -U {user} -f {output_file} {database}"  # noqa: E501
+    shell.exec(pg_dump.format(output_file=output_file, **ctx), env)
+
+
+def database_exists(connection, database_name):
+    with connection.creation._nodb_cursor() as cursor:
+        return connection.creation._database_exists(cursor, database_name)
+
+
+def _ctx(db_conf):
+    env = {"PGPASSWORD": db_conf["PASSWORD"]}
+    try:
+        database = db_conf["TEST"]["NAME"] or db_conf["NAME"]
+    except KeyError:
+        database = db_conf["NAME"]
+
+    data = {
+        "host": db_conf["HOST"],
+        "port": db_conf["PORT"],
+        "user": db_conf["USER"],
+        "database": database,
+    }
+    return data, env
