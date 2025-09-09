1e7b867be0a7887ed016cefcfd9fae15e7d109a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 17:06:30 +0000

    Add package docling-onnx-models/0.1.2.dev1/docling_onnx_models-0.1.2.dev1-py3-none-any.whl

---
+++ packages/docling-onnx-models/docling_onnx_models-0.1.2.dev1-py3-none-any.whl/docling_onnx_models/tableformer/data_management/__init__.py
@@ -0,0 +1,10 @@
+#
+# Copyright IBM Corp. 2024 - 2024
+# SPDX-License-Identifier: MIT
+#
+
+"""ONNX tableformer data management."""
+
+from .tf_predictor import TFPredictor
+
+__all__ = ["TFPredictor"]
