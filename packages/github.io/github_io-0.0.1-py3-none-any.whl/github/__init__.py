25825f28925aa879a55cfc9c78ee52d07fd629f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 18:06:13 +0000

    Add package github.io/0.0.1/github_io-0.0.1-py3-none-any.whl

---
+++ packages/github.io/github_io-0.0.1-py3-none-any.whl/github/__init__.py
@@ -0,0 +1,7 @@
+# == __init__.py ==#
+
+__title__ = 'github.io'
+__authors__ = 'VarMonke', 'sudosnok'
+__version__ = '0.0.1'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2022-present VarMonke & sudosnok'
