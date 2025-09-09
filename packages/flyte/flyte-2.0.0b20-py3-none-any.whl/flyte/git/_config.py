9eb1ef3a5de143abd2fc54ead8ddd01305f6b515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 15:04:39 +0000

    Add package flyte/2.0.0b20/flyte-2.0.0b20-py3-none-any.whl

---
+++ packages/flyte/flyte-2.0.0b20-py3-none-any.whl/flyte/git/_config.py
@@ -0,0 +1,17 @@
+import pathlib
+import subprocess
+
+import flyte.config
+
+
+def config_from_root(path: pathlib.Path | str = ".flyte/config.yaml") -> flyte.config.Config:
+    """Get the config file from the git root directory.
+
+    By default, the config file is expected to be in `.flyte/config.yaml` in the git root directory.
+    """
+
+    result = subprocess.run(["git", "rev-parse", "--show-toplevel"], check=False, capture_output=True, text=True)
+    if result.returncode != 0:
+        raise RuntimeError(f"Failed to get git root directory: {result.stderr}")
+    root = pathlib.Path(result.stdout.strip())
+    return flyte.config.auto(root / path)
