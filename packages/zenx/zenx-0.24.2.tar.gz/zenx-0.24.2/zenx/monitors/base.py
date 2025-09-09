520c2d850ddb42e1cde4c47b8d5941e2f60afd9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 15:04:31 +0000

    Add package zenx/0.24.2/zenx-0.24.2.tar.gz

---
+++ packages/zenx/zenx-0.24.2.tar.gz/zenx-0.24.2/zenx/monitors/base.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import ClassVar, Dict, List, Type
+from structlog import BoundLogger
+
+from zenx.settings import Settings
+
+
+
+class Monitor(ABC):
+    name: ClassVar[str]
+    required_settings: ClassVar[List[str]]
+    _registry: ClassVar[Dict[str, Type[Monitor]]] = {}
+
+
+
+    def __init_subclass__(cls, **kwargs) -> None:
+        super().__init_subclass__(**kwargs)
+        if not hasattr(cls, "name"):
+            raise TypeError(f"Monitor subclass {cls.__name__} must have a 'name' attribute.")
+        cls._registry[cls.name] = cls
+
+
+    @classmethod
+    def get_monitor(cls, name: str) -> Type[Monitor]:
+        if name not in cls._registry:
+            raise ValueError(f"Monitor '{name}' is not registered. Available monitors: {list(cls._registry.keys())}")
+        return cls._registry[cls.name]
+
+
+    def __init__(self, logger: BoundLogger, settings: Settings) -> None:
+        self.logger = logger
+        self.settings = settings
+
+
+    @abstractmethod
+    async def open(self) -> None:
+        ...
+
+
+    @abstractmethod
+    async def process_stats(self, stats: Dict, producer: str) -> None:
+        ...
+
+
+    @abstractmethod
+    async def close(self) -> None:
+        ...
