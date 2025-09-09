04f7f1b58d65c6dfc0a745569539cfaf62257f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 03:18:15 +0000

    Add package clear-skies-akeyless-custom-producer/2.0.6/clear_skies_akeyless_custom_producer-2.0.6-py3-none-any.whl

---
+++ packages/clear-skies-akeyless-custom-producer/clear_skies_akeyless_custom_producer-2.0.6-py3-none-any.whl/clearskies_akeyless_custom_producer/__init__.py
@@ -0,0 +1,3 @@
+from clearskies_akeyless_custom_producer import endpoints
+
+__all__ = ["endpoints"]
---
+++ packages/clear-skies-akeyless-custom-producer/clear_skies_akeyless_custom_producer-2.0.6-py3-none-any.whl/clearskies_akeyless_custom_producer/endpoints/__init__.py
@@ -0,0 +1,4 @@
+from clearskies_akeyless_custom_producer.endpoints.no_input import NoInput
+from clearskies_akeyless_custom_producer.endpoints.with_input import WithInput
+
+__all__ = ["NoInput", "WithInput"]
