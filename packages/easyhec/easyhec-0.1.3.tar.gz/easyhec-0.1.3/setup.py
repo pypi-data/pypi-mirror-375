3ee89755b32e99fc9ea65878f74b4990dd4101a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 09:05:08 +0000

    Add package easyhec/0.1.3/easyhec-0.1.3.tar.gz

---
+++ packages/easyhec/easyhec-0.1.3.tar.gz/easyhec-0.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="easyhec",
+    version="0.1.3",
+    packages=find_packages(),
+    author="Stone Tao",
+    install_requires=[
+        "tyro",
+        "torch",
+        "tqdm",
+        "opencv-python",
+        "trimesh",
+        "transforms3d",
+        "matplotlib",
+        # ninja is used by nvdiffrast
+        "ninja>=1.11",
+    ],
+    extras_require={
+        "dev": [
+            "black",
+            "isort",
+            "pre-commit",
+        ],
+        "sim-maniskill": [
+            "mani_skill-nightly",
+        ],
+    },
+)
