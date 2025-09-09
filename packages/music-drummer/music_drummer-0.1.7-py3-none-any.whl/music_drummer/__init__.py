33680caa21a25fb6f0a9926dd36abba1a6799dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 15:04:39 +0000

    Add package music-drummer/0.1.7/music_drummer-0.1.7-py3-none-any.whl

---
+++ packages/music-drummer/music_drummer-0.1.7-py3-none-any.whl/music_drummer/__init__.py
@@ -0,0 +1,7 @@
+# The root of the entire project
+from music_drummer.music_drummer import Drummer
+
+# This is the version number of your code.  This will change each time you make
+# a new release.  The most popular approach is to use Semantic Versioning,
+# described at https://semver.org/
+__version__ = "0.1.7"
