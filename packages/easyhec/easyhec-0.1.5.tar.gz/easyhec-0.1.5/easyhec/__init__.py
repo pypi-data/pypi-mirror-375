f514e2a6a97adfd31fdc4fdc7a8affd1ee74751a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 09:05:18 +0000

    Add package easyhec/0.1.5/easyhec-0.1.5.tar.gz

---
+++ packages/easyhec/easyhec-0.1.5.tar.gz/easyhec-0.1.5/easyhec/__init__.py
@@ -0,0 +1,10 @@
+try:
+    import sam2
+except ModuleNotFoundError:
+    print("sam2 is not installed. Features using SAM2 for segmentation will not work. Follow the instructions at https://github.com/facebookresearch/sam2 to install it.")
+
+try:
+    import nvdiffrast
+except ModuleNotFoundError:
+    print("nvdiffrast is not installed. Please install it with pip install 'nvdiffrast @ git+https://github.com/NVlabs/nvdiffrast.git@729261dc64c4241ea36efda84fbf532cc8b425b8'")
+    exit(1)
