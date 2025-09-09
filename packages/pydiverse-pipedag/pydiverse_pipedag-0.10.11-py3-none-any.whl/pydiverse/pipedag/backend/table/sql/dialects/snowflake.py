852c550554bf98c7ab6eb7c72fcb008db675328b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 04:04:03 +0000

    Add package pydiverse-pipedag/0.10.11/pydiverse_pipedag-0.10.11-py3-none-any.whl

---
+++ packages/pydiverse-pipedag/pydiverse_pipedag-0.10.11-py3-none-any.whl/pydiverse/pipedag/backend/table/sql/dialects/snowflake.py
@@ -0,0 +1,83 @@
+# Copyright (c) QuantCo and pydiverse contributors 2025-2025
+# SPDX-License-Identifier: BSD-3-Clause
+
+import time
+import warnings
+from typing import Literal
+
+from pydiverse.pipedag.backend.table.sql import hooks
+from pydiverse.pipedag.backend.table.sql.hooks import (
+    IbisTableHook,
+)
+from pydiverse.pipedag.backend.table.sql.sql import SQLTableStore
+
+try:
+    import snowflake
+except ImportError as e:
+    warnings.warn(str(e), ImportWarning)
+    snowflake = None
+
+
+class SnowflakeTableStore(SQLTableStore):
+    """
+    SQLTableStore that supports `Snowflake`_.
+
+    Takes the same arguments as
+    :py:class:`SQLTableStore <pydiverse.pipedag.backend.table.SQLTableStore>`
+    """
+
+    _dialect_name = "snowflake"
+
+    def _default_isolation_level(self) -> str | None:
+        return None  # "READ UNCOMMITTED" does not exist in Snowflake
+
+    def optional_pause_for_db_transactionality(
+        self,
+        prev_action: Literal[
+            "table_drop",
+            "table_create",
+            "schema_drop",
+            "schema_create",
+            "schema_rename",
+        ],
+    ):
+        _ = prev_action
+        # The snowflake backend has transactionality problems with very quick
+        # DROP/CREATE or RENAME activities for both schemas and tables
+        # which happen in testing.
+        time.sleep(2)
+
+    def _init_database(self):
+        create_database = self.engine_url.database.split("/")[0]
+        with self.engine.connect() as conn:
+            if not [
+                x.name
+                for x in conn.exec_driver_sql("SHOW DATABASES").mappings().all()
+                if x.name.upper() == create_database.upper()
+            ]:
+                self._init_database_with_database(
+                    "snowflake",
+                    disable_exists_check=True,
+                    create_database=create_database,
+                )
+
+
+@SnowflakeTableStore.register_table(snowflake)
+class PolarsTableHook(hooks.PolarsTableHook):
+    @classmethod
+    def dialect_supports_connectorx(cls):
+        # ConnectorX (used by Polars read_database_uri) supports Snowflake.
+        return True
+
+
+try:
+    import ibis
+except ImportError:
+    ibis = None
+
+
+@SnowflakeTableStore.register_table(ibis)
+class IbisTableHook(IbisTableHook):
+    @classmethod
+    def _conn(cls, store: SnowflakeTableStore):
+        return ibis.snowflake._from_url(store.engine_url)
