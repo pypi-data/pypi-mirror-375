19b02076760597f0b7adaacb0cca95507606c2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 09:05:18 +0000

    Add package chime-frb-api/4.6.1/chime_frb_api-4.6.1-py3-none-any.whl

---
+++ packages/chime-frb-api/chime_frb_api-4.6.1-py3-none-any.whl/chime_frb_api/modules/__init__.py
@@ -0,0 +1,12 @@
+from chime_frb_api.modules import (  # noqa: F401
+    calibration,
+    candidates,
+    events,
+    mimic,
+    parameters,
+    sources,
+    swarm,
+    tns,
+    voe,
+    voe_subscribers,
+)
