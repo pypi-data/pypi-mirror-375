a7dc70b18cae7e88b1de163a18c85dd541c1d09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 15:05:06 +0000

    Add package quantum-scoring-sdk/0.1.0/quantum-scoring-sdk-0.1.0.tar.gz

---
+++ packages/quantum-scoring-sdk/quantum-scoring-sdk-0.1.0.tar.gz/quantum-scoring-sdk-0.1.0/README.md
@@ -0,0 +1,15 @@
+﻿# Quantum Scoring SDK
+
+Universal framework for business scoring and optimization.
+
+## Installation
+pip install quantum-scoring-sdk
+
+## Basic Usage
+from quantum_sdk import Optimizer
+opt = Optimizer(api_key='your-license-key')
+score = opt.evaluate(data, config)
+
+## License
+Proprietary software. Copyright (c) 2025 Jaime Alexander Jimenez Lozano.
+For commercial licenses contact: jaimeajl@hotmail.com
---
+++ packages/quantum-scoring-sdk/quantum-scoring-sdk-0.1.0.tar.gz/quantum-scoring-sdk-0.1.0/setup.py
@@ -0,0 +1,32 @@
+﻿from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+extensions = [
+    Extension('quantum_sdk.core', ['quantum_sdk/core.py'])
+]
+
+setup(
+    name='quantum-scoring-sdk',
+    version='0.1.0',
+    author='Jaime Alexander Jimenez Lozano',
+    author_email='jaimeajl@hotmail.com',
+    description='Framework universal para scoring y optimización de negocios',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    ext_modules=cythonize(extensions),
+    packages=find_packages(),
+    package_data={
+        'quantum_sdk': ['*.pyd', '*.so'],
+    },
+    install_requires=['requests', 'numpy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: Microsoft :: Windows",
+        "Development Status :: 3 - Alpha",
+    ],
+    python_requires='>=3.8',
+)
