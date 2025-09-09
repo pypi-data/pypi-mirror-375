a0c289676f8a5cfd6e1cf7022e339e647ce682b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 16:05:42 +0000

    Add package veeink-mcp-server/0.1.3/veeink_mcp_server-0.1.3-py3-none-any.whl

---
+++ packages/veeink-mcp-server/veeink_mcp_server-0.1.3-py3-none-any.whl/Veeink_MCP_Server/__init__.py
@@ -0,0 +1,35 @@
+"""
+FastMCP quickstart example.
+
+cd to the `examples/snippets/clients` directory and run:
+    uv run server fastmcp_quickstart stdio
+"""
+
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
+
+def main():
+    # stdio 是 Cherry Studio 默认支持的方式
+    mcp.run(transport="stdio")
+    # mcp.run(transport='sse')
+    # mcp.run(transport='streamable-http')
+
+if __name__ == "__main__":
+    main()
+
