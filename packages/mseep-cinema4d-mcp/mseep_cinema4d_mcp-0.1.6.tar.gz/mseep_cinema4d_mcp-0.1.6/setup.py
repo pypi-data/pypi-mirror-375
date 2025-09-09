222be152f219774bc2665642ae85ff46c97fcc4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 18:05:47 +0000

    Add package mseep-cinema4d-mcp/0.1.6/mseep_cinema4d_mcp-0.1.6.tar.gz

---
+++ packages/mseep-cinema4d-mcp/mseep_cinema4d_mcp-0.1.6.tar.gz/mseep_cinema4d_mcp-0.1.6/setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+)
