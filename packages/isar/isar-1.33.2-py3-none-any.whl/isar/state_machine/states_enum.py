e5e1b2befdde07772faddb657c0349e0b5b3afab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 03:18:17 +0000

    Add package isar/1.33.2/isar-1.33.2-py3-none-any.whl

---
+++ packages/isar/isar-1.33.2-py3-none-any.whl/isar/state_machine/states_enum.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class States(str, Enum):
+    Monitor = "monitor"
+    ReturningHome = "returning_home"
+    Stopping = "stopping"
+    Paused = "paused"
+    AwaitNextMission = "await_next_mission"
+    Home = "home"
+    RobotStandingStill = "robot_standing_still"
+    Offline = "offline"
+    BlockedProtectiveStop = "blocked_protective_stop"
+    UnknownStatus = "unknown_status"
+    InterventionNeeded = "intervention_needed"
+    Recharging = "recharging"
+
+    def __repr__(self):
+        return self.value
---
+++ packages/isar/isar-1.33.2-py3-none-any.whl/robot_interface/models/mission/status.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+
+class MissionStatus(str, Enum):
+    NotStarted = "not_started"
+    InProgress = "in_progress"
+    Paused = "paused"
+    Failed = "failed"
+    Cancelled = "cancelled"
+    Successful = "successful"
+    PartiallySuccessful = "partially_successful"
+
+
+class TaskStatus(str, Enum):
+    NotStarted = "not_started"
+    InProgress = "in_progress"
+    Paused = "paused"
+    Failed = "failed"
+    Cancelled = "cancelled"
+    Successful = "successful"
+    PartiallySuccessful = "partially_successful"
+
+
+class RobotStatus(Enum):
+    Available = "available"
+    Busy = "busy"
+    Home = "home"
+    Offline = "offline"
+    Blocked = "blocked"
+    BlockedProtectiveStop = "blockedprotectivestop"
+    ReturningHome = "returninghome"
+    InterventionNeeded = "interventionneeded"
+    Recharging = "recharging"
