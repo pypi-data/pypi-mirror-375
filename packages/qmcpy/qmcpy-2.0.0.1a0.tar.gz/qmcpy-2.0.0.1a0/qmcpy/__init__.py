76b076bb0388b6f3cac95833dfc25e2e709c1f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 08:04:28 +0000

    Add package qmcpy/2.0.0.1a0/qmcpy-2.0.0.1a0.tar.gz

---
+++ packages/qmcpy/qmcpy-2.0.0.1a0.tar.gz/qmcpy-2.0.0.1a0/qmcpy/__init__.py
@@ -0,0 +1,36 @@
+from .discrete_distribution import *
+from .true_measure import *
+from .integrand import *
+from .stopping_criterion import *
+from .kernel import (
+    KernelGaussian,
+    KernelSquaredExponential,
+    KernelRationalQuadratic,
+    KernelMatern12,
+    KernelMatern32,
+    KernelMatern52,
+    KernelShiftInvar,
+    KernelDigShiftInvar,
+    KernelDigShiftInvarAdaptiveAlpha,
+    KernelSI,
+    KernelDSI,
+    KernelDSIAA,
+    KernelMultiTask,
+    KernelMultiTaskDerivs,
+)
+from .fast_transform import (
+    fftbr,
+    ifftbr,
+    fwht,
+    omega_fftbr,
+    omega_fwht,
+    fftbr_torch,
+    ifftbr_torch,
+    fwht_torch,
+    omega_fftbr_torch,
+    omega_fwht_torch,
+)
+from .util import plot_proj,mlmc_test
+
+name = "qmcpy"
+__version__ = "2.0.0.1a"
