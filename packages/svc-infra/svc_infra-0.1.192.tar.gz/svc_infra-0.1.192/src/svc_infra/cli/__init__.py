8a1509e0ab45cfde2ff3781e10f869bc45a79beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 15:04:59 +0000

    Add package svc-infra/0.1.192/svc_infra-0.1.192.tar.gz

---
+++ packages/svc-infra/svc_infra-0.1.192.tar.gz/svc_infra-0.1.192/src/svc_infra/cli/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import typer
+
+from svc_infra.cli.foundation.typer_bootstrap import pre_cli
+from svc_infra.cli.cmds import (
+    register_alembic, register_scaffold, _HELP
+)
+
+app = typer.Typer(
+    no_args_is_help=True,
+    add_completion=False,
+    help=_HELP
+)
+pre_cli(app)
+register_alembic(app)
+register_scaffold(app)
+
+def main():
+    app()
+
+if __name__ == "__main__":
+    main()
