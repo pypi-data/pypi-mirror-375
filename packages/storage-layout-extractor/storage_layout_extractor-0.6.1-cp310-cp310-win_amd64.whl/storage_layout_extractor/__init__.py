ac748614263ace78149292e192798f3d8818981e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 06:06:12 +0000

    Add package storage-layout-extractor/0.6.1/storage_layout_extractor-0.6.1-cp310-cp310-win_amd64.whl

---
+++ packages/storage-layout-extractor/storage_layout_extractor-0.6.1-cp310-cp310-win_amd64.whl/storage_layout_extractor/__init__.py
@@ -0,0 +1,5 @@
+from .storage_layout_extractor import *
+
+__doc__ = storage_layout_extractor.__doc__
+if hasattr(storage_layout_extractor, "__all__"):
+    __all__ = storage_layout_extractor.__all__
