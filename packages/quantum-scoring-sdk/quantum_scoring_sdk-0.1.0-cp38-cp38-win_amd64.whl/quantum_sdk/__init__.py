b89caaa2f20fa13a3be2114aa7fc72617e7ee720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 15:05:06 +0000

    Add package quantum-scoring-sdk/0.1.0/quantum_scoring_sdk-0.1.0-cp38-cp38-win_amd64.whl

---
+++ packages/quantum-scoring-sdk/quantum_scoring_sdk-0.1.0-cp38-cp38-win_amd64.whl/quantum_sdk/__init__.py
@@ -0,0 +1,20 @@
+﻿# quantum_sdk/__init__.py
+import requests
+from .core import process_credit_scoring, hierarchical_score
+
+class Optimizer:
+    def __init__(self, api_key=None, validate=False):
+        self.api_key = api_key
+        if validate and api_key:
+            self.validate_key()
+    
+    def validate_key(self):
+        # Por ahora skip, activar cuando esté listo
+        pass
+    
+    def evaluate(self, data, config, mode='credit'):
+        if mode == 'credit':
+            return process_credit_scoring(data, config)
+        else:
+            # Modo genérico
+            return hierarchical_score(config['blocks'])
