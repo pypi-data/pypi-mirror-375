b36cd8aa78772e816cfb9463769c49da64b0787e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 05:05:32 +0000

    Add package sqlmesh/0.215.1.dev7/sqlmesh-0.215.1.dev7-py3-none-any.whl

---
+++ packages/sqlmesh/sqlmesh-0.215.1.dev7-py3-none-any.whl/sqlmesh/migrations/v0047_change_scd_string_to_column.py
@@ -0,0 +1,9 @@
+"""Changes the SCD Type 2 columns from strings to columns."""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
