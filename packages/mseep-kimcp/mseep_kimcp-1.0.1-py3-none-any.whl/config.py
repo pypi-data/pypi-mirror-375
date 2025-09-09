2465d33847f44581c22f28f3bb489efdefe38221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 17:06:07 +0000

    Add package mseep-kimcp/1.0.1/mseep_kimcp-1.0.1-py3-none-any.whl

---
+++ packages/mseep-kimcp/mseep_kimcp-1.0.1-py3-none-any.whl/config.py
@@ -0,0 +1,11 @@
+import os
+
+# Naver API credentials
+NAVER_CLIENT_ID = os.environ.get("NAVER_CLIENT_ID")
+NAVER_CLIENT_SECRET = os.environ.get("NAVER_CLIENT_SECRET")
+
+# Kakao API credentials
+KAKAO_REST_API_KEY = os.environ.get("KAKAO_REST_API_KEY")
+
+# SK Open API credentials
+SK_APP_KEY = os.environ.get("SK_APP_KEY")
