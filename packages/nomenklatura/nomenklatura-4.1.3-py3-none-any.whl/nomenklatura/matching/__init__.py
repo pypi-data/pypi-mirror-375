1ac726770a2926de574a31c06c2428abd4b93e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 04:03:47 +0000

    Add package nomenklatura/4.1.3/nomenklatura-4.1.3-py3-none-any.whl

---
+++ packages/nomenklatura/nomenklatura-4.1.3-py3-none-any.whl/nomenklatura/matching/__init__.py
@@ -0,0 +1,35 @@
+from typing import List, Type, Optional
+from nomenklatura.matching.regression_v1.model import RegressionV1
+from nomenklatura.matching.regression_v1.train import train_matcher as train_v1_matcher
+from nomenklatura.matching.name_based import NameMatcher, NameQualifiedMatcher
+from nomenklatura.matching.logic_v1.model import LogicV1
+from nomenklatura.matching.types import ScoringAlgorithm, ScoringConfig
+
+ALGORITHMS: List[Type[ScoringAlgorithm]] = [
+    LogicV1,
+    NameMatcher,
+    NameQualifiedMatcher,
+    RegressionV1,
+]
+
+DefaultAlgorithm = RegressionV1
+
+
+def get_algorithm(name: str) -> Optional[Type[ScoringAlgorithm]]:
+    """Return the scoring algorithm class with the given name."""
+    for algorithm in ALGORITHMS:
+        if algorithm.NAME == name:
+            return algorithm
+    return None
+
+
+__all__ = [
+    "RegressionV1",
+    "train_v1_matcher",
+    "DefaultAlgorithm",
+    "ScoringAlgorithm",
+    "NameMatcher",
+    "NameQualifiedMatcher",
+    "ScoringConfig",
+    "LogicV1",
+]
