ac1a1a6afc39b6174661af0fd33c669527942877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 08:04:34 +0000

    Add package gstaichi/2.2.1/gstaichi-2.2.1-cp313-cp313-win_amd64.whl

---
+++ packages/gstaichi/gstaichi-2.2.1-cp313-cp313-win_amd64.whl/gstaichi/lang/ast/__init__.py
@@ -0,0 +1,5 @@
+# type: ignore
+
+from gstaichi.lang.ast.ast_transformer_utils import ASTTransformerContext
+from gstaichi.lang.ast.checkers import KernelSimplicityASTChecker
+from gstaichi.lang.ast.transform import transform_tree
