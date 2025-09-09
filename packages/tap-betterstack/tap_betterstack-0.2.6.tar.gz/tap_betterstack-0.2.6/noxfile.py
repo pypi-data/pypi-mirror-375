4b7e3df65ecfd5f0f8f4e3fcf26049b12fb73346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 09:05:18 +0000

    Add package tap-betterstack/0.2.6/tap_betterstack-0.2.6.tar.gz

---
+++ packages/tap-betterstack/tap_betterstack-0.2.6.tar.gz/tap_betterstack-0.2.6/noxfile.py
@@ -0,0 +1,60 @@
+"""Nox configuration."""
+
+from __future__ import annotations
+
+import nox
+
+python_versions = [
+    "3.14",
+    "3.13",
+    "3.12",
+    "3.11",
+    "3.10",
+]
+nox.needs_version = ">=2025.2.9"
+nox.options.sessions = ("tests",)
+nox.options.default_venv_backend = "uv"
+
+UV_SYNC_COMMAND = (
+    "uv",
+    "sync",
+    "--locked",
+    "--no-dev",
+)
+
+
+@nox.session(python=python_versions)
+def tests(session: nox.Session) -> None:
+    """Execute pytest tests."""
+    env = {
+        "UV_PROJECT_ENVIRONMENT": session.virtualenv.location,
+    }
+    if isinstance(session.python, str):
+        env["UV_PYTHON"] = session.python
+
+    session.run_install(
+        *UV_SYNC_COMMAND,
+        "--group=ci",
+        "--group=testing",
+        env=env,
+    )
+    session.run("pytest", *session.posargs)
+
+
+@nox.session()
+def mypy(session: nox.Session) -> None:
+    """Check types."""
+    env = {
+        "UV_PROJECT_ENVIRONMENT": session.virtualenv.location,
+    }
+    if isinstance(session.python, str):
+        env["UV_PYTHON"] = session.python
+
+    session.run_install(
+        *UV_SYNC_COMMAND,
+        "--group=testing",
+        "--group=typing",
+        env=env,
+    )
+    args = session.posargs or ("tap_betterstack", "tests")
+    session.run("mypy", *args)
