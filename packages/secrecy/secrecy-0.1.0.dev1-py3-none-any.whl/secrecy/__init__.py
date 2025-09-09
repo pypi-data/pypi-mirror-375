a531a618ba8cb6204a8f6f454fd79ab21098e9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 07:04:18 +0000

    Add package secrecy/0.1.0.dev1/secrecy-0.1.0.dev1-py3-none-any.whl

---
+++ packages/secrecy/secrecy-0.1.0.dev1-py3-none-any.whl/secrecy/__init__.py
@@ -0,0 +1,38 @@
+"""
+Secrecy
+=======
+
+A simple CLI utility for file en-/decryption.
+
+"""
+
+from .main import (
+    DATA_DIR,
+    ENCRYPTION_DIR,
+    DECRYPTION_DIR,
+    generate_key,
+    cli,
+    encrypt,
+    decrypt
+)
+
+
+# ─── package metadata ───────────────────────────────────────────────────────────── ✦ ─
+#
+#
+__author__ = "Caleb Rice"
+__email__ = "hyletic@proton.me"
+__version__ = "0.1.0dev1"
+
+
+# ─── package-level exports ──────────────────────────────────────────────────────── ✦ ─
+#
+__all__ = [
+    "DATA_DIR",
+    "ENCRYPTION_DIR",
+    "DECRYPTION_DIR",
+    "generate_key",
+    "cli",
+    "encrypt",
+    "decrypt",
+]
