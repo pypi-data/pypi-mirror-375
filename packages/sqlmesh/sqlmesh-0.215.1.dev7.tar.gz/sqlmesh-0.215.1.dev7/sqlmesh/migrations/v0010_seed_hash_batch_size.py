5b30a358e4a266c00f0971de3c85afb5f5f4c1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 05:05:34 +0000

    Add package sqlmesh/0.215.1.dev7/sqlmesh-0.215.1.dev7.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.215.1.dev7.tar.gz/sqlmesh-0.215.1.dev7/sqlmesh/migrations/v0010_seed_hash_batch_size.py
@@ -0,0 +1,9 @@
+"""Seed metadata hashes now correctly include the batch_size."""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
---
+++ packages/sqlmesh/sqlmesh-0.215.1.dev7.tar.gz/sqlmesh-0.215.1.dev7/sqlmesh/migrations/v0054_fix_trailing_comments.py
@@ -0,0 +1,9 @@
+"""Fix support for trailing comments in SQL model definitions."""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
