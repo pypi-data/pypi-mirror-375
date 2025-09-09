bb5c22272cebfb84655c9f82105cbc6a4e0b0a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 04:03:56 +0000

    Add package symmora-math/0.1.0/symmora_math-0.1.0-py3-none-any.whl

---
+++ packages/symmora-math/symmora_math-0.1.0-py3-none-any.whl/symmora_math/__init__.py
@@ -0,0 +1,3 @@
+from .core import symmora_add, symmora_multiply, symmora_power
+
+__version__ = "0.1.0"
---
+++ packages/symmora-math/symmora_math-0.1.0-py3-none-any.whl/symmora_math/core.py
@@ -0,0 +1,9 @@
+def symmora_add(a, b):
+    return a + b
+
+def symmora_multiply(a, b):
+    return a * b
+
+def symmora_power(a, b):
+    return a ** b
+
