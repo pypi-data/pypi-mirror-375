4b8eef48c990eea38661dc129c7674c45a5a448b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 06:06:27 +0000

    Add package core-main-app/2.17.0/core_main_app-2.17.0.tar.gz

---
+++ packages/core-main-app/core_main_app-2.17.0.tar.gz/core_main_app-2.17.0/core_main_app/rest/blob_processing_module/views.py
@@ -0,0 +1,66 @@
+""" REST API views for blob processing modules.
+"""
+
+from drf_spectacular.utils import extend_schema, OpenApiResponse
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from core_main_app.access_control.exceptions import AccessControlError
+from core_main_app.components.blob_processing_module import (
+    api as blob_processing_module_api,
+)
+from core_main_app.rest.blob_processing_module.serializers import (
+    BlobProcessingModuleSerializer,
+)
+
+
+@extend_schema(
+    tags=["Blob Processing Module"],
+    description="CRUD views for blob processing modules",
+)
+class BlobProcessingModuleView(APIView):
+    """CRUD views for blob processing modules."""
+
+    permission_classes = (IsAuthenticated,)
+
+    @extend_schema(
+        summary="Retrieve the list of blob processing modules",
+        description="Retrieve the list of blob processing modules",
+        responses={
+            200: OpenApiResponse(
+                BlobProcessingModuleSerializer(many=True),
+                description="List of blob processing modules",
+            ),
+            403: OpenApiResponse(
+                description="User is not authenticated or has no access"
+            ),
+            500: OpenApiResponse(description="Internal server error"),
+        },
+    )
+    def get(self, request):
+        """Retrieve the list of blob processing modules
+        Args:
+            request: The HTTP request being sent
+        """
+        try:
+            blob_processing_module_list = blob_processing_module_api.get_all(
+                request.user
+            )
+            # Serialize object
+            serializer = BlobProcessingModuleSerializer(
+                blob_processing_module_list,
+                many=True,
+                context={"request": request},
+            )
+            # Return response
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except AccessControlError as exception:
+            content = {"message": str(exception)}
+            return Response(content, status=status.HTTP_403_FORBIDDEN)
+        except Exception as api_exception:
+            content = {"message": str(api_exception)}
+            return Response(
+                content, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
