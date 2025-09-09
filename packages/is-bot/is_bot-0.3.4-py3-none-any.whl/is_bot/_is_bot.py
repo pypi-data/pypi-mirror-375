8c9ad5e85a6e444dd36a41a342d7777d75ef2154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 16:04:44 +0000

    Add package is-bot/0.3.4/is_bot-0.3.4-py3-none-any.whl

---
+++ packages/is-bot/is_bot-0.3.4-py3-none-any.whl/is_bot/_is_bot.py
@@ -0,0 +1,40 @@
+import typing
+import functools
+from regex import regex
+
+from ._patterns import default_patterns
+
+
+@functools.lru_cache(maxsize=1, typed=True)
+def compile_patterns(patterns: frozenset) -> regex.Pattern:
+    pattern = '({})'.format('|'.join(patterns))
+    return regex.compile(pattern, flags=regex.IGNORECASE)
+
+
+class Bots:
+    def __init__(self, patterns: typing.Optional[typing.Iterable[str]] = None):
+        if patterns is None:
+            patterns = default_patterns
+        self.patterns = frozenset(patterns)
+
+    def extend(self, patterns: typing.Iterable[str]):
+        if isinstance(patterns, str):  # pragma: nocover
+            patterns = [patterns]
+        self.patterns = self.patterns | frozenset(patterns)
+
+    def exclude(self, patterns: typing.Iterable[str]):
+        if isinstance(patterns, str):  # pragma: nocover
+            patterns = [patterns]
+        self.patterns = self.patterns - frozenset(patterns)
+
+    def is_bot(self, ua: str) -> bool:
+        pattern = compile_patterns(self.patterns)
+        return bool(pattern.search(ua))
+
+    def matches(self, ua: str) -> typing.List[str]:  # pragma: nocover
+        return [s for s in self.patterns if regex.search(s, ua, regex.IGNORECASE)]
+
+    def find(self, ua: str) -> typing.Optional[str]:  # pragma: nocover
+        pattern = compile_patterns(self.patterns)
+        match = pattern.search(ua)
+        return match and match[0]
