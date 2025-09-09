c73b0171c2e7acf6bac98e28f39a801d1849176c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 15:04:39 +0000

    Add package mxboxutils/0.0.4/mxboxutils-0.0.4-py3-none-any.whl

---
+++ packages/mxboxutils/mxboxutils-0.0.4-py3-none-any.whl/mxboxutils/mxcfg.py
@@ -0,0 +1,10 @@
+import tomllib
+import os
+
+
+def load_toml(filepath):
+    if not os.path.exists(filepath):
+        return None
+
+    with open(filepath, 'rb') as f:
+        return tomllib.load(f)
