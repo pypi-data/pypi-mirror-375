5cbeb0343013563b865d211ced904aa3e96c89e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 14:05:04 +0000

    Add package ctxkit/1.2.1/ctxkit-1.2.1.tar.gz

---
+++ packages/ctxkit/ctxkit-1.2.1.tar.gz/ctxkit-1.2.1/src/ctxkit/__init__.py
@@ -0,0 +1,7 @@
+# Licensed under the MIT License
+# https://github.com/craigahobbs/ctxkit/blob/main/LICENSE
+
+
+from .main import process_config
+from .grok import grok_chat
+from .ollama import ollama_chat
