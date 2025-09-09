b6bf2610385526159a4d19bc7359e4349e21f86c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 14:04:45 +0000

    Add package foundry-sdk/0.0.239/foundry_sdk-0.0.239-py3-none-any.whl

---
+++ packages/foundry-sdk/foundry_sdk-0.0.239-py3-none-any.whl/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/foundry_sdk/utils/data_transformation.py
@@ -0,0 +1,24 @@
+import typing as t
+import pandas as pd
+
+def convert_to_df(relevant_cartegories: t.Dict[str, t.List[str]]) -> pd.DataFrame:
+    """
+    Converts a list of dictionaries to a pandas DataFrame.
+    
+    Parameters:
+        data (List[Dict]): The list of dictionaries to convert.
+        
+    Returns:
+        pd.DataFrame: The DataFrame created from the list of dictionaries.
+    """
+
+    category_relations = []
+    for sub_category, parent_categories in relevant_cartegories.items():
+        if parent_categories is None:
+            category_relations.append({"subCategory": sub_category, "parentCategory": pd.NA})
+        else:
+            for parent_category in parent_categories:
+                category_relations.append({"subCategory": sub_category, "parentCategory": parent_category})
+    category_relations_df = pd.DataFrame(category_relations)
+
+    return category_relations_df
---
+++ packages/foundry-sdk/foundry_sdk-0.0.239-py3-none-any.whl/build/lib/build/lib/foundry_sdk/db_mgmt/data_retrieval.py
@@ -0,0 +1,33 @@
+from typing import List, Tuple, Union, Optional
+from foundry_sdk.db_mgmt import SQLDatabase
+
+
+def retrieve_data(
+    db: SQLDatabase, table: str, columns: Union[str, List[str]], condition: Optional[str] = None
+) -> List[Tuple]:
+    """
+    Retrieve data from a table in the database.
+
+    Ensures column names are enclosed in double quotes for PostgreSQL,
+    unless columns == "*", in which case it is used as-is.
+    Allows single column name as a string or multiple as a list.
+    """
+    # Handle special case for selecting all columns
+    if isinstance(columns, str) and columns == "*":
+        columns_string = "*"
+    else:
+        # Ensure columns is a list
+        if isinstance(columns, str):
+            columns = [columns]
+        # Enclose column names in double quotes
+        columns_string = ", ".join(f'"{col}"' for col in columns)
+
+    # Construct query with optional condition
+    query = f"SELECT {columns_string} FROM {table}"
+    if condition:
+        query += f" WHERE {condition}"
+
+    result = db.execute_query(query, fetchall=True)
+    db.close()
+
+    return result
