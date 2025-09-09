4580fb4050be55625f3735fe3c79a676b3bbc296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 04:04:08 +0000

    Add package taha/0.1.2/taha-0.1.2.tar.gz

---
+++ packages/taha/taha-0.1.2.tar.gz/taha-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='taha',
+    version='0.1.2',
+    py_modules=['taha'],
+    author='NS-TAHA1515',
+    description='کتابخونه‌ی ساده برای تست نصب با pip',
+    python_requires='>=3.6',
+)
---
+++ packages/taha/taha-0.1.2.tar.gz/taha-0.1.2/taha.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+import requests
+from tkinter import colorchooser
+import turtle as t
+import random as ra
+from datetime import datetime
+import time
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+second = now.second
+microsecond = now.microsecond
+def ri (a, b):
+    return ra.randint(a, b)
+def key (a, b):
+    t.listen()
+    t.onkeypress(a, b)
+def click(a):
+    t.onscreenclick(a)
+def getcolor(tit):
+    return colorchooser.askcolor(title = tit)
+def rc (a):
+    return ra.choice(a)
+
+def leftclick (a):
+    t.onscreenclick(a, btn = 1)
+def middleclick (a):
+    t.onscreenclick(a, btn = 2)
+def rightclick (a):
+    t.onscreenclick(a, btn = 3)
+def move (x, y):
+    t.goto(x, y)
+def randcolor():
+    t.colormode(255)
+    r = ra.randint(1, 255)
+    g = ra.randint(1, 255)
+    b = ra.randint(1, 255)
+    t.color ((r, g, b))
+def rgbcolor(r, g, b):
+    t.colormode(255)
+    t.color ((r, g, b))
+def getping(url):
+    start = time.time()
+    requests.get(url)
+    end = time.time()
+    return round((end - start)* 1000)
+def mouseX ():
+    screen = t.Screen()
+    return screen.cv.winfo_pointerx() - screen.cv.winfo_rootx() - screen.window_width() // 2
+def mouseY():
+    screen = t.Screen()
+    return screen.window_height() // 2 - (screen.cv.winfo_pointery() - screen.cv.winfo_rooty())
+def hidecursor():
+    root = tk.Tk()
+    root.config(cursor = "none")
+    root.mainloop()
+def shapecursor(a):
+    root = tk.Tk()
+    root.config (cursor = a)
+    root.mainloop()
