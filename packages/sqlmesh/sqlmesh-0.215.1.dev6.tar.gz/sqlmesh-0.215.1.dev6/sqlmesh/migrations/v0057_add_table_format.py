3500439c15eb9b031bcd29ec7dd25f5d0134586b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 05:04:07 +0000

    Add package sqlmesh/0.215.1.dev6/sqlmesh-0.215.1.dev6.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.215.1.dev6.tar.gz/sqlmesh-0.215.1.dev6/sqlmesh/migrations/v0057_add_table_format.py
@@ -0,0 +1,9 @@
+"""Add table_format to the model top-level properties"""
+
+
+def migrate_schemas(state_sync, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(state_sync, **kwargs):  # type: ignore
+    pass
