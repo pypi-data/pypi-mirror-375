37ccff590ff3e0b21c464410d5462674f7fd2c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 15:05:05 +0000

    Add package PyKozo/0.0.3/pykozo-0.0.3.tar.gz

---
+++ packages/PyKozo/pykozo-0.0.3.tar.gz/pykozo-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='PyKozo',
+    version='0.0.3',
+    license='MIT',
+    description='PyKozo is a Python library that allows for complete web development using Python scripts.',
+    author='Miguel Nevado',
+    author_email='miangeldev@proton.me',
+    url='https://github.com/miangeldev/pykozo',
+    packages=find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=['pykozo','html','tags','web','development'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+    ]
+)
