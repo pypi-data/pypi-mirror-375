516fea8152287a0f16695e452c553935d894c3f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 03:17:52 +0000

    Add package pysatgeo/0.0.7/pysatgeo-0.0.7.tar.gz

---
+++ packages/pysatgeo/pysatgeo-0.0.7.tar.gz/pysatgeo-0.0.7/pysatgeo/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for pysatgeo."""
+
+__author__ = "Joao Pereira"
+__email__ = "joaopmpereira93@gmail.com"
+__version__ = "0.0.7"   # bump this whenever you release a new version
+
+# Expose functions from aux_functions.py at the top-level
+from .aux_functions import *
