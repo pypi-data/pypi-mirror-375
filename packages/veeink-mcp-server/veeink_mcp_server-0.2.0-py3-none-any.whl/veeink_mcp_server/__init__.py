14e17d6b361163d7a362927b56de0a1514de7faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 18:06:06 +0000

    Add package veeink-mcp-server/0.2.0/veeink_mcp_server-0.2.0-py3-none-any.whl

---
+++ packages/veeink-mcp-server/veeink_mcp_server-0.2.0-py3-none-any.whl/veeink_mcp_server/__init__.py
@@ -0,0 +1,30 @@
+
+"""
+FastMCP quickstart example.
+
+cd to the `examples/snippets/clients` directory and run:
+    uv run server fastmcp_quickstart stdio
+"""
+
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+
+# Add a dynamic greeting resource
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
+
+# main方法改变，并且使用stdio
+def main() -> None:
+    mcp.run(transport='stdio')
