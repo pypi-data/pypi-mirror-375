e4e24e7ff6e9b8129bf1ce4474c243030ff64578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 16:04:50 +0000

    Add package easyUI-Qt/0.0.4/easyui_qt-0.0.4-py3-none-any.whl

---
+++ packages/easyUI-Qt/easyui_qt-0.0.4-py3-none-any.whl/easyUIQt/__init__.py
@@ -0,0 +1,14 @@
+from os.path import dirname
+rootPath = dirname(__file__)
+
+class FileType:
+    xml = "*.xml"
+    fbx = "*.fbx"
+    max = "*.max"
+    xlsx = "*.xlsx"
+    xls = "*.xls"
+    csv = "*.csv"
+    json = "*.json"
+    png = "*.png"
+    tga = "*.tga"
+    folder = "folder"
