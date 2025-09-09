bed7d73ca63da95b2e2fdc8de1ca2a464610922c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 06:06:37 +0000

    Add package rootf/0.1.1/rootf-0.1.1-py3-none-any.whl

---
+++ packages/rootf/rootf-0.1.1-py3-none-any.whl/rootf/ascii_art/__init__.py
@@ -0,0 +1,366 @@
+ASCII_ART = {
+    # ARCH
+    "arch": r"""
+                   -`                 
+                  .o+`                
+                 `ooo/                
+                `+oooo:               
+               `+oooooo:              
+               -+oooooo+:             
+             `/:-:++oooo+:            
+            `/++++/+++++++:           
+           `/++++++++++++++:          
+          `/+++ooooooooooooo/`        
+         ./ooosssso++osssssso+`       
+        .oossssso-````/ossssss+`      
+       -osssssso.      :ssssssso.     
+      :osssssss/        osssso+++.    
+     /ossssssss/        +ssssooo/-    
+   `/ossssso+/:-        -:/+osssso+-  
+  `+sso+:-`                 `.-/+oso: 
+ `++:.                           `-/+/
+ .`                                 `/
+               Arch Linux
+    """,
+
+    "arch_mini": r"""
+        /\
+       /  \
+      / /\ \
+     /_/  \_\
+      Arch
+    """,
+
+    # UBUNTU
+    "ubuntu": r"""
+           .-/+oossssoo+/-.           
+       `:+ssssssssssssssssss+:`       
+     -+ssssssssssssssssssyyssss+-     
+   .ossssssssssssssssssdMMMNysssso.   
+  /ssssssssssshdmmNNmmyNMMMMhssssss/  
+ +ssssssssshmydMMMMMMMNddddyssssssss+ 
+osssssssssdMMMNhsssssssssshmmssssssso 
+osssssssssssshmmssssssssssydMMMMssssso
++ssssshdmNNNNdyyyysssssssssdMMMmyssss+
+/sssdMMMMMMMMMMMMMNhssssssssNMMMysssss/
++sssdMMMMMMMMMMMMMMMysssssssNMMMysssss+
++sssdMMMMMMMMMMMMMMMMyssssssNMMMysssss+
++ssssyMMMMdssssssssshmmsssssdMMMmyssss+
+ossssssdMMMhssssssssssydMMMMMyssssssso 
+oss+ossyNMMMNyssssssssshmmssssssssso  
+ +ssssssssshmydMMMMMMMNddddyssssssss+ 
+  /ssssssssssshdmNNNmmyNMMMMhssssss/  
+   .ossssssssssssssssssdMMMNysssso.   
+     -+ssssssssssssssssssyyssss+-     
+       `:+ssssssssssssssssss+:`       
+           .-/+oossssoo+/-.           
+             Ubuntu
+    """,
+
+    "ubuntu_mini": r"""
+        .-/''\-.   
+       |  ··  |  
+        '-\__/-'  
+        Ubuntu
+    """,
+
+    # DEBIAN
+    "debian": r"""
+                _____-----_____
+             .-''               ''-.
+          .-'                       '-.
+        .'                             '.
+       /    .-'''-.       .-'''-.       \
+      ;   .'       '.   .'       '.      ;
+     |   /           \ /           \     |
+     ;  |             |             |    ;
+      \  \           / \           /    /
+       '. '.       .'   '.       .'    .'
+         '-.`'---'`       `---'`.-'
+            '''--._______.--'''
+               Debian GNU/Linux
+    """,
+
+    "debian_mini": r"""
+       .-----
+      /     \
+      \-----/
+      Debian
+    """,
+
+    # FEDORA
+    "fedora": r"""
+              .--.
+             |o_o |
+             |:_/ |
+            //   \ \
+           (|     | )
+          /'\_   _/`\
+          \___)=(___/
+           Fedora
+    """,
+
+    "fedora_mini": r"""
+       .--.
+      (>< )
+       '--'
+      Fedora
+    """,
+
+    # CENTOS
+    "centos": r"""
+           o   o     
+           |   |     
+           .---.     
+        .-'     '-.  
+       / _________ \ 
+       ||         || 
+       ||  CENTOS || 
+       ||         || 
+       \ \_______/ / 
+        '-._____.-'  
+           CentOS
+    """,
+
+    "centos_mini": r"""
+       .---.
+      ( C )
+       '---'
+      CentOS
+    """,
+
+    # MANJARO
+    "manjaro": r"""
+        \ \    / /  ___ _ __ ___  
+         \ \/\/ /  / _ \ '__/ _ \ 
+          \_/\_/  |  __/ | | (_) |
+                   \___|_|  \___/ 
+         Manjaro Linux
+    """,
+
+    "manjaro_mini": r"""
+      |||||||
+      Manjaro
+    """,
+
+    # KALI
+    "kali": r"""
+              .''.
+             .:;' '.
+            .''     '.  
+           :  K A L I :
+           '.       .'
+             '...'
+           Kali Linux
+    """,
+
+    "kali_mini": r"""
+      .--.
+     (>< )
+      '--'
+      Kali
+    """,
+
+    # GENTOO
+    "gentoo": r"""
+               .-
+             (   )
+              `-'
+             .-.-.
+           .'  |  '.
+          /    |    \
+         /     |     \
+        ;      |      ;
+        |      |      |
+        |  GENTOO     |
+        ;      |      ;
+         \     |     /
+          '.   |   .'
+            `-._.-'
+           Gentoo
+    """,
+
+    "gentoo_mini": r"""
+       .-.
+      ( G )
+       '-'
+      Gentoo
+    """,
+
+    # ALPINE
+    "alpine": r"""
+               /\        
+              /  \       
+             /____\      
+             |    |      
+             |    |      
+             |____|      
+           Alpine Linux
+    """,
+
+    "alpine_mini": r"""
+        /\
+      /__\
+       Alpine
+    """,
+
+    # VOID
+    "void": r"""
+        \     /
+         \ _ /
+        -- (_) --
+         /   \
+        /     \
+         Void Linux
+    """,
+
+    "void_mini": r"""
+       .--.
+      ( V )
+       '-'
+      Void
+    """,
+
+    # LINUX MINT
+    "linuxmint": r"""
+       .----------------.
+      | .--------------. |
+      | |   MINT LTS   | |
+      | '--------------' |
+       '----------------'
+        Linux Mint
+    """,
+
+    "linuxmint_mini": r"""
+      [.MIN.T.]
+       Linux Mint
+    """,
+
+    # OPENSUSE
+    "opensuse": r"""
+        _.-'''''-._
+      .'  _     _  '.
+     /   (_)   (_)   \
+    |                 |
+    |  openSUSE Linux |
+    \               .'
+     '.           .'
+       '-._   _.-'
+           '''
+    """,
+
+    "opensuse_mini": r"""
+       .-.
+      ( S )
+       '-'
+      openSUSE
+    """,
+
+    # NIXOS
+    "nixos": r"""
+         _   _          
+        | \ | | ___ ___ 
+        |  \| |/ _ / __|
+        | |\  |  __\__ \
+        |_| \_|\___|___/
+         NixOS Linux
+    """,
+
+    "nixos_mini": r"""
+       .—.
+      ( N )
+       '—'
+      NixOS
+    """,
+
+    # SLACKWARE
+    "slackware": r"""
+       ____  _            _    
+      / ___|| | ___   ___| | __
+      \___ \| |/ _ \ / __| |/ /
+       ___) | | (_) | (__|   < 
+      |____/|_|\___/ \___|_|\_\
+       Slackware Linux
+    """,
+
+    "slackware_mini": r"""
+      S L A C K
+      Slackware
+    """,
+
+    # macOS
+    "macos": r"""
+             .:'
+         __ :'__
+      .'`  `-'  ``.
+     :  .--------.  :
+     : : macOS   : :
+     : :  Apple  : :
+     : :        : :
+      '.`.____.`.'
+         `--. .--'
+             `
+    """,
+
+    "macos_mini": r"""
+      .—.
+     ( M )
+      '—'
+      macOS
+    """,
+
+    # WINDOWS
+    "windows": r"""
+        .--------. .--------.
+       / .------. \/ .------. \
+      / /       \__/       \ \
+      | |                    | |
+      | |  Windows 10/11     | |
+      | |                    | |
+      \ \       /\       / /  
+       \ '------'  '------' /
+        '------------------'
+    """,
+
+    "windows_mini": r"""
+       [Win]
+       Windows
+    """,
+
+    # TERMUX — логотип Android
+    "termux": r"""
+             ________
+           .-'      '-.
+          /            \
+         |  /~\    /~\  |
+         | (o o)  (o o) |
+         |  \_/    \_/  |
+          \   ____     /
+           '-.____.-'
+           Termux (Android)
+    """,
+
+    "termux_mini": r"""
+       .--.
+      (🤖)
+       '-'
+      Termux
+    """,
+
+    # GENERIC LINUX (Tux)
+    "linux": r"""
+               .--.
+              |o_o |
+              |:_/ |
+             //   \ \
+            (|     | )
+           /'\_   _/`\
+           \___)=(___/
+             Linux
+    """,
+
+    "linux_mini": r"""
+       (Tux)
+       Linux
+    """,
+}
