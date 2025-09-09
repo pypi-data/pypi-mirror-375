cdf01aca244ecca97ecf692a3deb5be698f58a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 16:04:50 +0000

    Add package anytraverse/1.0.4/anytraverse-1.0.4-py3-none-any.whl

---
+++ packages/anytraverse/anytraverse-1.0.4-py3-none-any.whl/anytraverse/__init__.py
@@ -0,0 +1,10 @@
+from anytraverse.utils import _typing as typing
+from anytraverse.utils.pipelines.paper import build_pipeline_from_paper
+from anytraverse.utils import AnyTraverse
+
+
+__all__ = ["typing", "build_pipeline_from_paper", "AnyTraverse"]
+
+
+def main():
+    print("Welcome to AnyTraverse!")
