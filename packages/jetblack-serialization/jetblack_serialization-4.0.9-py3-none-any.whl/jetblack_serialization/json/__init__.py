147422f7f53df106636b7731421ef53cf171d2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 16:05:44 +0000

    Add package jetblack-serialization/4.0.9/jetblack_serialization-4.0.9-py3-none-any.whl

---
+++ packages/jetblack-serialization/jetblack_serialization-4.0.9-py3-none-any.whl/jetblack_serialization/json/__init__.py
@@ -0,0 +1,29 @@
+"""JSON Serialization"""
+
+from .annotations import JSONValue, JSONProperty, TypeSelector, Source
+from .serialization import (
+    serialize,
+    deserialize
+)
+from .typed_serializer import serialize_typed
+from .typed_deserializer import (
+    from_json_value,
+    deserialize_typed
+)
+from .untyped_serializer import serialize_untyped
+from .untyped_deserializer import deserialize_untyped
+
+__all__ = [
+    'JSONValue',
+    'JSONProperty',
+    'TypeSelector',
+    'Source',
+
+    'serialize',
+    'deserialize',
+    'from_json_value',
+    'serialize_typed',
+    'deserialize_typed',
+    'serialize_untyped',
+    'deserialize_untyped',
+]
