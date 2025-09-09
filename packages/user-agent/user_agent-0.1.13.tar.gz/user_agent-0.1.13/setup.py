c0a46e97140652dc0b227b282c40e12fb4f15a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 07:04:29 +0000

    Add package user-agent/0.1.13/user_agent-0.1.13.tar.gz

---
+++ packages/user-agent/user_agent-0.1.13.tar.gz/user_agent-0.1.13/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="user_agent",
+    version="0.1.13",
+    packages=["user_agent", "user_agent.data"],
+    install_requires=["six", "pytz"],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": {
+            "ua=user_agent.cli:script_ua",
+        }
+    },
+)
