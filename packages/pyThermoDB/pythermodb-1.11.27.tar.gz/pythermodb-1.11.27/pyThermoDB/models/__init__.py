b371386cb49a5df97421061e71143f67a3e1b52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 06:04:39 +0000

    Add package pyThermoDB/1.11.27/pythermodb-1.11.27.tar.gz

---
+++ packages/pyThermoDB/pythermodb-1.11.27.tar.gz/pythermodb-1.11.27/pyThermoDB/models/__init__.py
@@ -0,0 +1,24 @@
+from .ref import (
+    DataBookTableTypes,
+    PayLoadType,
+    DataResultType,
+    MatrixDataType,
+    DataResult,
+    EquationResult
+)
+
+from .references import Component, ComponentReferenceThermoDB, ReferenceThermoDB
+from .property import PropertyMatch
+
+__all__ = [
+    'DataBookTableTypes',
+    'PayLoadType',
+    'DataResultType',
+    'MatrixDataType',
+    'DataResult',
+    'EquationResult',
+    'Component',
+    'PropertyMatch',
+    'ComponentReferenceThermoDB',
+    'ReferenceThermoDB',
+]
