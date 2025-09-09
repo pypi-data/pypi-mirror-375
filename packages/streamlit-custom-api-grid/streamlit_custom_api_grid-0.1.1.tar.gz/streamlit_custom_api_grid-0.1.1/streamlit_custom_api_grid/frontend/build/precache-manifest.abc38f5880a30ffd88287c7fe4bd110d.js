173112f4411817fe05496ef8857c1b60fec97c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 09:05:10 +0000

    Add package streamlit-custom-api-grid/0.1.1/streamlit_custom_api_grid-0.1.1.tar.gz

---
+++ packages/streamlit-custom-api-grid/streamlit_custom_api_grid-0.1.1.tar.gz/streamlit_custom_api_grid-0.1.1/streamlit_custom_api_grid/frontend/build/precache-manifest.abc38f5880a30ffd88287c7fe4bd110d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28d0078295fe98d6c091e345f91d590",
+    "url": "./index.html"
+  },
+  {
+    "revision": "525a439e3491b599a826",
+    "url": "./static/css/2.739f1b7f.chunk.css"
+  },
+  {
+    "revision": "34aed0a5118234bc046a",
+    "url": "./static/css/main.9bb489bb.chunk.css"
+  },
+  {
+    "revision": "525a439e3491b599a826",
+    "url": "./static/js/2.53019075.chunk.js"
+  },
+  {
+    "revision": "d91272289fe6bde52047c4871a2902b0",
+    "url": "./static/js/2.53019075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34aed0a5118234bc046a",
+    "url": "./static/js/main.1db34d0b.chunk.js"
+  },
+  {
+    "revision": "be99a8b073eb36846b2d",
+    "url": "./static/js/runtime-main.1f7e4d05.js"
+  }
+]);
