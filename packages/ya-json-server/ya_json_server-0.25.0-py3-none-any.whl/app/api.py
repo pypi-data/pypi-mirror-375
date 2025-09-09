099125689b86d0eb852abc8871ee90ff1d5d091c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 09:05:11 +0000

    Add package ya-json-server/0.25.0/ya_json_server-0.25.0-py3-none-any.whl

---
+++ packages/ya-json-server/ya_json_server-0.25.0-py3-none-any.whl/app/api.py
@@ -0,0 +1,41 @@
+import uvicorn
+from fastapi import FastAPI
+
+from app.handlers.exceptions import APIException, api_exception_handler
+from app.views.routes import router
+
+description = """A REST API for JSON content with zero coding.
+
+Technologies::
+* Python 3.13
+* FastAPI 0.116
+"""
+app = FastAPI(
+    version="1.4.0",
+    title="Yet Another JSON Server",
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    contact={
+        "name": "Adriano Vieira",
+        "url": "https://www.adrianovieira.eng.br/",
+    },
+    description=description,
+    openapi_tags=[{"name": "API"}],
+)
+
+app.add_exception_handler(APIException, api_exception_handler)
+app.add_exception_handler(FileNotFoundError, api_exception_handler)
+app.add_exception_handler(NotImplementedError, api_exception_handler)
+app.add_exception_handler(Exception, api_exception_handler)
+
+app.include_router(router)
+
+
+def main():
+    uvicorn.run("app.api:app", host="0.0.0.0")  # pragma: no cover
+
+
+if __name__ == "__main__":
+    main()  # pragma: no cover
