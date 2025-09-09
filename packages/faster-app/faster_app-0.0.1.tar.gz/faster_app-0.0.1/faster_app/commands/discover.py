13a448ee26d9c381c69e1eb4917da2677943137a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 15:04:40 +0000

    Add package faster-app/0.0.1/faster_app-0.0.1.tar.gz

---
+++ packages/faster-app/faster_app-0.0.1.tar.gz/faster_app-0.0.1/faster_app/commands/discover.py
@@ -0,0 +1,24 @@
+"""
+自动发现 apps 目录下的 commands 模块和内置命令
+"""
+
+from faster_app.commands.base import CommandBase
+from faster_app.base import DiscoverBase
+
+
+class CommandDiscover(DiscoverBase):
+    INSTANCE_TYPE = CommandBase
+    TARGETS = [
+        {
+            "directory": "apps",
+            "filename": "commands.py",
+            "skip_dirs": ["__pycache__"],
+            "skip_files": [],
+        },
+        {
+            "directory": "commands/builtins",
+            "filename": None,
+            "skip_dirs": ["__pycache__"],
+            "skip_files": [],
+        },
+    ]
