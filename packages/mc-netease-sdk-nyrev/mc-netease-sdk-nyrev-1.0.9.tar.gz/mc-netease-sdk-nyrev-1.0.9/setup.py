53949ff7afbb434f1ca5f2cf3dc944bc931fb81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 08:04:50 +0000

    Add package mc-netease-sdk-nyrev/1.0.9/mc-netease-sdk-nyrev-1.0.9.tar.gz

---
+++ packages/mc-netease-sdk-nyrev/mc-netease-sdk-nyrev-1.0.9.tar.gz/mc-netease-sdk-nyrev-1.0.9/setup.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+| ==============================================
+|
+|   Copyright (c) 2025 Nuoyan
+|
+|   Author: Nuoyan
+|   Email : 1279735247@qq.com
+|   Gitee : https://gitee.com/charming-lee
+|   Date  : 2025-09-09
+|
+| ==============================================
+"""
+
+
+from setuptools import setup, find_packages
+
+
+try:
+    long_description = open("README.md").read()
+except:
+    try:
+        long_description = open("README.md", encoding="utf-8").read()
+    except:
+        long_description = "Netease ModSDK completion library revised version by Nuoyan\nSee https://github.com/charminglee/mc-netease-sdk-nyrev"
+
+
+setup(
+    name="mc-netease-sdk-nyrev",
+    version="1.0.9",
+    description="Netease ModSDK completion library revised version by Nuoyan",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Nuoyan",
+    author_email="1279735247@qq.com",
+    url="https://github.com/charminglee/mc-netease-sdk-nyrev",
+    license="MIT",
+    packages=find_packages(where="libs"),
+    package_dir={'': "libs"},
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=2.7, <4",
+    install_requires=[
+        "typing==3.7.4.3",
+        "typing-extensions==3.10.0.2"
+    ]
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
