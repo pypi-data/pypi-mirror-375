e0d2081e659d1eddea45b22fdef0bcbed631edb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 18:05:49 +0000

    Add package mseep-mcp-allthetools/0.1.4/mseep_mcp_allthetools-0.1.4.tar.gz

---
+++ packages/mseep-mcp-allthetools/mseep_mcp_allthetools-0.1.4.tar.gz/mseep_mcp_allthetools-0.1.4/src/mcp_allthetools/server.py
@@ -0,0 +1,51 @@
+from mcp.server.models import InitializationOptions
+from mcp.server.lowlevel import NotificationOptions, Server
+import mcp.types as types
+import mcp.server.stdio
+
+from mcp_allthetools.tools import tools
+
+server = Server("MCP-allthetools")
+
+
+@server.list_tools()
+async def handle_list_tools() -> list[types.Tool]:
+    """
+    List available tools.
+    Each tool specifies its arguments using JSON Schema validation.
+    """
+    return tools
+
+
+@server.call_tool()
+async def handle_call_tool(
+    name: str, arguments: dict | None
+) -> list[types.TextContent | types.ImageContent | types.EmbeddedResource]:
+    """
+    Handle tool execution requests.
+    Tools can modify server state and notify clients of changes.
+    """
+
+    return [
+        types.TextContent(
+            type="text",
+            text=f"example tool call result from {name} with arguments {arguments}",
+        )
+    ]
+
+
+async def main():
+    # Run the server using stdin/stdout streams
+    async with mcp.server.stdio.stdio_server() as (read_stream, write_stream):
+        await server.run(
+            read_stream,
+            write_stream,
+            InitializationOptions(
+                server_name="MCP-allthetools",
+                server_version="0.1.0",
+                capabilities=server.get_capabilities(
+                    notification_options=NotificationOptions(),
+                    experimental_capabilities={},
+                ),
+            ),
+        )
