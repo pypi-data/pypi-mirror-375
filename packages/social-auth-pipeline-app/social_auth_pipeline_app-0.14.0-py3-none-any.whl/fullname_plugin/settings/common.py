5d2fab755a14c4c7344f800094c2c912666456ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 16:04:44 +0000

    Add package social-auth-pipeline-app/0.14.0/social_auth_pipeline_app-0.14.0-py3-none-any.whl

---
+++ packages/social-auth-pipeline-app/social_auth_pipeline_app-0.14.0-py3-none-any.whl/fullname_plugin/settings/common.py
@@ -0,0 +1,21 @@
+
+PIPELINE_STEP = "fullname_plugin.pipeline.set_full_name"
+def plugin_settings(settings): 
+    pipeline = list(getattr(settings, "SOCIAL_AUTH_PIPELINE", []))
+    if PIPELINE_STEP not in pipeline:
+    # Insert after create_user if it exists, else append at the end
+        try:
+            idx = pipeline.index("common.djangoapps.third_party_auth.pipeline.associate_by_email_if_oauth")
+            pipeline.insert(idx + 1, PIPELINE_STEP)
+        except ValueError:
+            pipeline.append(PIPELINE_STEP)
+        settings.SOCIAL_AUTH_PIPELINE = tuple(pipeline)
+
+from django.conf import settings
+print("Common Fullnameplugin called")
+pipeline = list(getattr(settings, "SOCIAL_AUTH_PIPELINE", []))
+step = "fullname_plugin.pipeline.set_full_name"
+if step not in pipeline:
+    pipeline.append(step)
+
+SOCIAL_AUTH_PIPELINE = tuple(pipeline)
