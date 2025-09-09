2c682718c5f5e2f6a643a4d20f3da543ea91bb2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 03:17:37 +0000

    Add package slepc/3.23.3/slepc-3.23.3.tar.gz

---
+++ packages/slepc/slepc-3.23.3.tar.gz/slepc-3.23.3/include/slepcversion.h
@@ -0,0 +1,55 @@
+/*
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   SLEPc - Scalable Library for Eigenvalue Problem Computations
+   Copyright (c) 2002-, Universitat Politecnica de Valencia, Spain
+
+   This file is part of SLEPc.
+   SLEPc is distributed under a 2-clause BSD license (see LICENSE).
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
+
+#ifndef SLEPCVERSION_H
+#define SLEPCVERSION_H
+
+#define SLEPC_VERSION_RELEASE    1
+#define SLEPC_VERSION_MAJOR      3
+#define SLEPC_VERSION_MINOR      23
+#define SLEPC_VERSION_SUBMINOR   3
+#define SLEPC_RELEASE_DATE       "March 29, 2025"
+#define SLEPC_VERSION_DATE       "Sep 08, 2025"
+
+#if !defined (SLEPC_VERSION_GIT)
+#define SLEPC_VERSION_GIT        "v3.23.3"
+#endif
+
+#if !defined(SLEPC_VERSION_DATE_GIT)
+#define SLEPC_VERSION_DATE_GIT   "2025-09-08 11:56:38 +0200"
+#endif
+
+#define SLEPC_VERSION_EQ(MAJOR,MINOR,SUBMINOR) \
+ ((SLEPC_VERSION_MAJOR == (MAJOR)) &&       \
+  (SLEPC_VERSION_MINOR == (MINOR)) &&       \
+  (SLEPC_VERSION_SUBMINOR == (SUBMINOR)) && \
+  (SLEPC_VERSION_RELEASE  == 1))
+
+#define SLEPC_VERSION_ SLEPC_VERSION_EQ
+
+#define SLEPC_VERSION_LT(MAJOR,MINOR,SUBMINOR)          \
+  (SLEPC_VERSION_RELEASE == 1 &&                        \
+   (SLEPC_VERSION_MAJOR < (MAJOR) ||                    \
+    (SLEPC_VERSION_MAJOR == (MAJOR) &&                  \
+     (SLEPC_VERSION_MINOR < (MINOR) ||                  \
+      (SLEPC_VERSION_MINOR == (MINOR) &&                \
+       (SLEPC_VERSION_SUBMINOR < (SUBMINOR)))))))
+
+#define SLEPC_VERSION_LE(MAJOR,MINOR,SUBMINOR) \
+  (SLEPC_VERSION_LT(MAJOR,MINOR,SUBMINOR) || \
+   SLEPC_VERSION_EQ(MAJOR,MINOR,SUBMINOR))
+
+#define SLEPC_VERSION_GT(MAJOR,MINOR,SUBMINOR) \
+  (0 == SLEPC_VERSION_LE(MAJOR,MINOR,SUBMINOR))
+
+#define SLEPC_VERSION_GE(MAJOR,MINOR,SUBMINOR) \
+  (0 == SLEPC_VERSION_LT(MAJOR,MINOR,SUBMINOR))
+
+#endif
