b2312cbc55d749ace3fbfa20159a62f16d0cde6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 03:17:55 +0000

    Add package e7-switcher/1.0.6/e7_switcher-1.0.6-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/e7-switcher/e7_switcher-1.0.6-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/lib64/cmake/e7-switcher/e7-switcher-targets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "e7-switcher::e7-switcher" for configuration "Release"
+set_property(TARGET e7-switcher::e7-switcher APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(e7-switcher::e7-switcher PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libe7-switcher.a"
+  )
+
+list(APPEND _cmake_import_check_targets e7-switcher::e7-switcher )
+list(APPEND _cmake_import_check_files_for_e7-switcher::e7-switcher "${_IMPORT_PREFIX}/lib64/libe7-switcher.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
