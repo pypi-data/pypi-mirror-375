832bc36e5231bb95c90beda633549aa88bda7f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-09 06:05:37 +0000

    Add package adam-atmos/0.2.1/adam_atmos-0.2.1.tar.gz

---
+++ packages/adam-atmos/adam_atmos-0.2.1.tar.gz/adam_atmos-0.2.1/src/adam/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for ATMOS Analogue Digital Twin."""
+
+__author__ = """Robert Jackson, Seongha Park"""
+__email__ = 'rjackson@anl.gov'
+__version__ = '0.2.1'
+
+from . import io  # noqa
+from . import model   # noqa
+from . import vis  # noqa
+from . import util  # noqa
+
+
