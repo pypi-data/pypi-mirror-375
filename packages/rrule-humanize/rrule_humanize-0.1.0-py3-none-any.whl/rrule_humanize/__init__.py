bc698f5f416ea08858e7baabbdad03792259b193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 05:07:55 +0000

    Add package rrule-humanize/0.1.0/rrule_humanize-0.1.0-py3-none-any.whl

---
+++ packages/rrule-humanize/rrule_humanize-0.1.0-py3-none-any.whl/rrule_humanize/__init__.py
@@ -0,0 +1,9 @@
+# rrule-humanize package
+
+"""
+This package provides functionality to convert RRULE strings to human-readable text.
+"""
+
+from .humanize import humanize
+
+__all__ = ['humanize']
