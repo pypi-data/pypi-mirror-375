4384b16fb0c515c540c78eefd560125016709a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 04:05:39 +0000

    Add package rasaio-integrations/1.0.15/rasaio_integrations-1.0.15-py3-none-any.whl

---
+++ packages/rasaio-integrations/rasaio_integrations-1.0.15-py3-none-any.whl/rasaio/integrations/constants.py
@@ -0,0 +1,24 @@
+RASA_DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+DATA = "data"
+RESULTS = "results"
+
+# API_ENDPOINT = "api_endpoint"
+KEY = "key"
+RASA_TOKEN = "rasa-token"
+COMMUNITY_ID = "community_id"
+TYPE = "type"
+
+# # Person fields
+ID = "id"
+EXTERNAL_ID = "external_id"
+EMAIL = "email"
+FIRST_NAME = "first_name"
+LAST_NAME = "last_name"
+UPDATED = "updated"
+IS_SUBSCRIBED = "is_subscribed"
+UNSUBSCRIBE_REASON = "unsubscribe_reason"
+IS_ACTIVE = "is_active"
+IS_ARCHIVED = "is_archived"
+STATUS = "status"
+CREATED = "created"
