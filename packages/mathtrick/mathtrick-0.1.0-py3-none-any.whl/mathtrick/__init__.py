d5c26bcb7ceff17565b4fe3ac9053cbd631d50a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 17:05:53 +0000

    Add package mathtrick/0.1.0/mathtrick-0.1.0-py3-none-any.whl

---
+++ packages/mathtrick/mathtrick-0.1.0-py3-none-any.whl/mathtrick/__init__.py
@@ -0,0 +1,3 @@
+from .core import sum_addition, methTrick
+
+__all__ = ["sum_addition", "methTrick"]
---
+++ packages/mathtrick/mathtrick-0.1.0-py3-none-any.whl/mathtrick/core.py
@@ -0,0 +1,9 @@
+def sum_addition(a, b):
+    return a + b
+
+class methTrick:
+    def __init__(self, value):
+        self.value = value
+
+    def double(self):
+        return self.value * 2
