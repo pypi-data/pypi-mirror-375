660dba503663fccf8d3b92a07510dadcac993f54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 07:05:10 +0000

    Add package toisto/0.37.0/toisto-0.37.0-py3-none-any.whl

---
+++ packages/toisto/toisto-0.37.0-py3-none-any.whl/toisto/ui/style.py
@@ -0,0 +1,14 @@
+"""Text styles."""
+
+from rich.theme import Theme
+
+theme = Theme(
+    {
+        "deleted": "underline bright_red",
+        "inserted": "bright_green",
+        "language": "light_goldenrod2 bold",
+        "quiz": "medium_purple1",
+        "secondary": "grey69",
+        "retention": "grey50",
+    }
+)
