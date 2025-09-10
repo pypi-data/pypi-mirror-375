8bbe7d6ab35261361caa715b4d505c131c9c6097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 14:06:22 +0000

    Add package portpy/1.1.1/portpy-1.1.1.tar.gz

---
+++ packages/portpy/portpy-1.1.1.tar.gz/portpy-1.1.1/portpy/ai/data/__init__.py
@@ -0,0 +1,47 @@
+# Copyright 2025, the PortPy Authors
+#
+# Licensed under the Apache License, Version 2.0 with the Commons Clause restriction.
+# You may obtain a copy of the Apache 2 License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# ----------------------------------------------------------------------
+# Commons Clause Restriction Notice:
+# PortPy is licensed under Apache 2.0 with the Commons Clause.
+# You may use, modify, and share the code for non-commercial
+# academic and research purposes only.
+# Commercial use — including offering PortPy as a service,
+# or incorporating it into a commercial product — requires
+# a separate commercial license.
+# ----------------------------------------------------------------------
+
+    def __init__(self, opt):
+        """Initialize this class
+
+        Step 1: create a dataset instance given the name [dataset_mode]
+        Step 2: create a multi-threaded data loader.
+        """
+        self.opt = opt
+        dataset_class = find_dataset_using_name(opt.dataset_mode)
+        self.dataset = dataset_class(opt)
+        print("dataset [%s] was created" % type(self.dataset).__name__)
+        self.dataloader = torch.utils.data.DataLoader(
+            self.dataset,
+            batch_size=opt.batch_size,
+            # shuffle=not opt.serial_batches,(Change it back to turn on the shuffle
+            shuffle=not opt.serial_batches,
+            num_workers=int(opt.num_threads))
+
+    def load_data(self):
+        return self
+
+    def __len__(self):
+        """Return the number of data in the dataset"""
+        return min(len(self.dataset), self.opt.max_dataset_size)
+
+    def __iter__(self):
+        """Return a batch of data"""
+        for i, data in enumerate(self.dataloader):
+            if i * self.opt.batch_size >= self.opt.max_dataset_size:
+                break
+            yield data
