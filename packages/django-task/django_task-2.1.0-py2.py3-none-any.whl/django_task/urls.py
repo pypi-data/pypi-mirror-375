c9f89c586c75c8e2f8d2476a1fe1f571e50572a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 03:13:43 +0000

    Add package django-task/2.1.0/django_task-2.1.0-py2.py3-none-any.whl

---
+++ packages/django-task/django_task-2.1.0-py2.py3-none-any.whl/django_task/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from django.contrib import admin
+from . import views
+
+app_name = "django_task"
+
+urlpatterns = [
+    path('info/', views.tasks_info_api, name="django_tasks_info"),
+    path('add/', views.task_add_api, name="django_task_add"),
+    path('<str:app_label>/<str:model_name>/<uuid:pk>/run/<int:is_async>/', views.task_run_api, name="django_task_run"),
+    path('<str:app_label>/<str:model_name>/<uuid:pk>/run/', views.task_run_api, name="django_task_run"),
+]
