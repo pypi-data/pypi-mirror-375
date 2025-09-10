0db38d4bad0901d634e1f89152ad254c70bfb4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 03:13:44 +0000

    Add package isar/1.33.3/isar-1.33.3.tar.gz

---
+++ packages/isar/isar-1.33.3.tar.gz/isar-1.33.3/tests/test_double/blob_storage.py
@@ -0,0 +1,20 @@
+from typing import List
+
+from isar.storage.storage_interface import StorageException, StorageInterface
+from robot_interface.models.inspection.inspection import Inspection
+from robot_interface.models.mission.mission import Mission
+
+
+class StorageFake(StorageInterface):
+    will_fail: bool = False
+
+    def __init__(self) -> None:
+        self.stored_inspections: List[Inspection] = []
+
+    def store(self, inspection: Inspection, mission: Mission):
+        if self.will_fail:
+            raise StorageException("Fake failed on purpose")
+        self.stored_inspections.append(inspection)
+
+    def blob_exists(self, inspection: Inspection) -> bool:
+        return inspection in self.stored_inspections
