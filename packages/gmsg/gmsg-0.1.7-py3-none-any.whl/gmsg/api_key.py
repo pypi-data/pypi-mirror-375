eb14737ac08dc5c96749700aa70efccec43fe95a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 04:05:34 +0000

    Add package gmsg/0.1.7/gmsg-0.1.7-py3-none-any.whl

---
+++ packages/gmsg/gmsg-0.1.7-py3-none-any.whl/gmsg/api_key.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import getpass
+
+from pathlib import Path
+
+
+def get_or_set_api_key():
+    try:
+        rc = os.path.join(Path.home(), ".gmsgrc")
+        if not os.path.isfile(rc):
+            print(
+                "Get your key from https://ai.google.dev/gemini-api/docs/api-key"
+            )
+            key_inp = getpass.getpass("Enter your Gemini API KEY: ").strip()
+            with open(rc, "w") as file:
+                file.write(key_inp)
+
+            return key_inp
+
+        with open(rc, "r") as f:
+            return f.read().strip()
+    except KeyboardInterrupt:
+        sys.exit(0)
