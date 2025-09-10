97911192ff25b91eb7214ee4bc7064034ee6bf6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 14:07:14 +0000

    Add package container-manager-mcp/0.0.8/container_manager_mcp-0.0.8.tar.gz

---
+++ packages/container-manager-mcp/container_manager_mcp-0.0.8.tar.gz/container_manager_mcp-0.0.8/container_manager_mcp/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from container_manager_mcp.container_manager import (
+    main,
+    create_manager,
+    ContainerManagerBase,
+    DockerManager,
+    PodmanManager,
+)
+from container_manager_mcp.container_manager_mcp import container_manager_mcp
+
+"""
+container-manager
+
+Manage your containers using docker, podman, compose, or docker swarm!
+"""
+
+__all__ = [
+    "main",
+    "create_manager",
+    "container_manager_mcp",
+    "ContainerManagerBase",
+    "DockerManager",
+    "PodmanManager",
+]
