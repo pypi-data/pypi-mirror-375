edeeba8055964eea392c476590997bb902104e2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 15:06:46 +0000

    Add package mytestlib-1/1.0.0/mytestlib_1-1.0.0.tar.gz

---
+++ packages/mytestlib-1/mytestlib_1-1.0.0.tar.gz/mytestlib_1-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mytestlib-1',
+    version='1.0.0',
+    url='https://pypi.org/manage/projects/',
+    author='Yosuke Saka',
+    author_email='yosuke.saka3416@gmail.com',
+    description='My Test Library.',
+    packages=find_packages()
+)
