4986db833457e9354ff4018b30447c88d2828058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 16:06:19 +0000

    Add package NanoRepeat/1.8.2/nanorepeat-1.8.2.tar.gz

---
+++ packages/NanoRepeat/nanorepeat-1.8.2.tar.gz/nanorepeat-1.8.2/src/NanoRepeat/__init__.py
@@ -0,0 +1,6 @@
+"""
+NanoRepeat: quantification of Short Tandem Repeats (STRs) from long-read sequencing data
+"""
+
+__author__ = 'Li Fang and Kai Wang'
+__credits__ = "Children's Hospital of Philadelphia"
