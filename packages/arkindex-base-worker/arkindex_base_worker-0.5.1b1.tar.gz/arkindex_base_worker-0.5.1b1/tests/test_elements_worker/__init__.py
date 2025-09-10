bcb888b8c6f6b3d9b07bb8219654da38f41f9e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 04:05:57 +0000

    Add package arkindex-base-worker/0.5.1b1/arkindex_base_worker-0.5.1b1.tar.gz

---
+++ packages/arkindex-base-worker/arkindex_base_worker-0.5.1b1.tar.gz/arkindex_base_worker-0.5.1b1/tests/test_elements_worker/__init__.py
@@ -0,0 +1,7 @@
+# API calls during worker configuration
+BASE_API_CALLS = [
+    (
+        "GET",
+        "http://testserver/api/v1/process/workers/56785678-5678-5678-5678-567856785678/",
+    ),
+]
