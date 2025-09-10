a0a0f9dcc5166991383fc1d79f9bb9f06fc49614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 15:05:30 +0000

    Add package odoo-addon-account-invoice-pricelist/16.0.1.1.0.2/odoo_addon_account_invoice_pricelist-16.0.1.1.0.2-py3-none-any.whl

---
+++ packages/odoo-addon-account-invoice-pricelist/odoo_addon_account_invoice_pricelist-16.0.1.1.0.2-py3-none-any.whl/odoo/addons/account_invoice_pricelist/__manifest__.py
@@ -0,0 +1,17 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account - Pricelist on Invoices",
+    "version": "16.0.1.1.0",
+    "summary": "Add partner pricelist on invoices",
+    "category": "Accounting & Finance",
+    "author": "GRAP," "Therp BV," "Tecnativa," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-invoicing",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/account_invoice_view.xml",
+        "reports/account_invoice_report_view.xml",
+    ],
+    "installable": True,
+}
