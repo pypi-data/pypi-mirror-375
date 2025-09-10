951c6c7f756bedac12da8d428dc6fb653e7afef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 08:05:18 +0000

    Add package chemotools/0.1.16/chemotools-0.1.16.tar.gz

---
+++ packages/chemotools/chemotools-0.1.16.tar.gz/chemotools-0.1.16/tests/augmentation/test_gaussian_broadening.py
@@ -0,0 +1,6 @@
+# Test compliance with scikit-learn
+# def test_compliance_gaussian_broadening():
+#    # Arrange
+#    transformer = GaussianBroadening()
+#    # Act & Assert
+#    check_estimator(transformer)
