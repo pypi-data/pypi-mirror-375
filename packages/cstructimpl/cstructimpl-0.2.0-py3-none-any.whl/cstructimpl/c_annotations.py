abd70f819c5dd6b310d3bedcaa28a0859d98af44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 03:13:36 +0000

    Add package cstructimpl/0.2.0/cstructimpl-0.2.0-py3-none-any.whl

---
+++ packages/cstructimpl/cstructimpl-0.2.0-py3-none-any.whl/cstructimpl/c_annotations.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Autocast:
+    """Marker class. Specify if the built object should be autocasted to the dataclass
+    actual type."""
+
+    pass
