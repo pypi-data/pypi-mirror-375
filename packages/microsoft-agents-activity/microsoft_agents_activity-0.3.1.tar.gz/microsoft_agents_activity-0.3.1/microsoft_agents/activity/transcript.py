bfc880d50438bd2e4373936d3f849f2f83ad0fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 14:07:21 +0000

    Add package microsoft-agents-activity/0.3.1/microsoft_agents_activity-0.3.1.tar.gz

---
+++ packages/microsoft-agents-activity/microsoft_agents_activity-0.3.1.tar.gz/microsoft_agents_activity-0.3.1/microsoft_agents/activity/transcript.py
@@ -0,0 +1,13 @@
+from .activity import Activity
+from .agents_model import AgentsModel
+
+
+class Transcript(AgentsModel):
+    """Transcript.
+
+    :param activities: A collection of Activities that conforms to the
+     Transcript schema.
+    :type activities: list[~microsoft_agents.protocols.models.Activity]
+    """
+
+    activities: list[Activity] = None
