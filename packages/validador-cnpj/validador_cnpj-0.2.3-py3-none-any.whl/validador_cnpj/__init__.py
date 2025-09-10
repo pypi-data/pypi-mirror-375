d4a5843570517c3a1a79df5936787fd1a88476f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 05:08:03 +0000

    Add package validador-cnpj/0.2.3/validador_cnpj-0.2.3-py3-none-any.whl

---
+++ packages/validador-cnpj/validador_cnpj-0.2.3-py3-none-any.whl/validador_cnpj/__init__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    "padronizar_cnpj",
+    "normalizar_cnpj_udf",
+    "cnpj_eh_valido_udf",
+    "tipo_cnpj_udf",
+    "mascarar_cnpj_udf",
+]
+from .core import (
+    padronizar_cnpj,
+    normalizar_cnpj_udf,
+    cnpj_eh_valido_udf,
+    tipo_cnpj_udf,
+    mascarar_cnpj_udf,
+)
+
+__version__ = "0.2.3"
