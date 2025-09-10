2e0ac98b465ddd9659ca9ab573c632b78d278105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 16:06:37 +0000

    Add package oups/2025.9.5/oups-2025.9.5.tar.gz

---
+++ packages/oups/oups-2025.9.5.tar.gz/oups-2025.9.5/oups/store/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Created on Wed Dec  1 18:35:00 2021.
+
+@author: pierrot
+
+"""
+from .indexer import is_toplevel
+from .indexer import sublevel
+from .indexer import toplevel
+from .ordered_parquet_dataset import OrderedParquetDataset
+from .ordered_parquet_dataset import check_cmidx
+from .ordered_parquet_dataset import conform_cmidx
+from .ordered_parquet_dataset import write
+from .store import Store
