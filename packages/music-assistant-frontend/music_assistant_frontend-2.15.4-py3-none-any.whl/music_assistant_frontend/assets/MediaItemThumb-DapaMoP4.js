897bbcd99840501ae05f3b1336b3e87585416687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 03:13:29 +0000

    Add package music-assistant-frontend/2.15.4/music_assistant_frontend-2.15.4-py3-none-any.whl

---
+++ packages/music-assistant-frontend/music_assistant_frontend-2.15.4-py3-none-any.whl/music_assistant_frontend/assets/MediaItemThumb-DapaMoP4.js
@@ -0,0 +1,1 @@
+import{c6 as o}from"./index-B-lUZalq.js";import{_ as a}from"./_plugin-vue_export-helper-DlAUqK2U.js";const t=a(o,[["__scopeId","data-v-565bc716"]]);export{t as M};
---
+++ packages/music-assistant-frontend/music_assistant_frontend-2.15.4-py3-none-any.whl/music_assistant_frontend/assets/VSpacer-DDLrofv-.js
@@ -0,0 +1,1 @@
+/* empty css              */import{cI as r}from"./index-B-lUZalq.js";const e=r("v-spacer","div","VSpacer");export{e as V};
