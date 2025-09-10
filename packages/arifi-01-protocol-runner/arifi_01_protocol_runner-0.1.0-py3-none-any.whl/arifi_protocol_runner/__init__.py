2f869d82692d3f5aedfe45f0b3ee4819f58ead20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 17:05:50 +0000

    Add package arifi-01-protocol-runner/0.1.0/arifi_01_protocol_runner-0.1.0-py3-none-any.whl

---
+++ packages/arifi-01-protocol-runner/arifi_01_protocol_runner-0.1.0-py3-none-any.whl/arifi_protocol_runner/__init__.py
@@ -0,0 +1,20 @@
+"""
+Arifi-01 Protocol Runner
+========================
+
+Implementasi PROTOKOL FONDASI: MATRIKS KETERIKATAN Arifi-01
+
+Dibuat oleh Dr. Profesor Arifi Razzaq (GitHub: razzaqinspires, PyPI: arzzq)
+"""
+
+from .arifi_protocol_runner import main, generate_code, analyze_code, evolution_step
+
+__all__ = [
+    "main",
+    "generate_code",
+    "analyze_code",
+    "evolution_step",
+]
+
+__version__ = "0.1.0"
+__author__ = "Dr. Profesor Arifi Razzaq"
