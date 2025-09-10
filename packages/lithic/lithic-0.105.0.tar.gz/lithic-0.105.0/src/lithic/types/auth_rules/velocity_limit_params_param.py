bcaae2ab9a88022b89bd90dc78a91f5affafd42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 06:05:45 +0000

    Add package lithic/0.105.0/lithic-0.105.0.tar.gz

---
+++ packages/lithic/lithic-0.105.0.tar.gz/lithic-0.105.0/src/lithic/types/auth_rules/velocity_limit_params_param.py
@@ -0,0 +1,69 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Optional
+from typing_extensions import Literal, Required, TypedDict
+
+from ..._types import SequenceNotStr
+from .velocity_limit_params_period_window_param import VelocityLimitParamsPeriodWindowParam
+
+__all__ = ["VelocityLimitParamsParam", "Filters"]
+
+
+class Filters(TypedDict, total=False):
+    exclude_countries: Optional[SequenceNotStr[str]]
+    """ISO-3166-1 alpha-3 Country Codes to exclude from the velocity calculation.
+
+    Transactions matching any of the provided will be excluded from the calculated
+    velocity.
+    """
+
+    exclude_mccs: Optional[SequenceNotStr[str]]
+    """Merchant Category Codes to exclude from the velocity calculation.
+
+    Transactions matching this MCC will be excluded from the calculated velocity.
+    """
+
+    include_countries: Optional[SequenceNotStr[str]]
+    """ISO-3166-1 alpha-3 Country Codes to include in the velocity calculation.
+
+    Transactions not matching any of the provided will not be included in the
+    calculated velocity.
+    """
+
+    include_mccs: Optional[SequenceNotStr[str]]
+    """Merchant Category Codes to include in the velocity calculation.
+
+    Transactions not matching this MCC will not be included in the calculated
+    velocity.
+    """
+
+
+class VelocityLimitParamsParam(TypedDict, total=False):
+    filters: Required[Filters]
+
+    period: Required[VelocityLimitParamsPeriodWindowParam]
+    """The size of the trailing window to calculate Spend Velocity over in seconds.
+
+    The minimum value is 10 seconds, and the maximum value is 2678400 seconds (31
+    days).
+    """
+
+    scope: Required[Literal["CARD", "ACCOUNT"]]
+
+    limit_amount: Optional[int]
+    """
+    The maximum amount of spend velocity allowed in the period in minor units (the
+    smallest unit of a currency, e.g. cents for USD). Transactions exceeding this
+    limit will be declined.
+    """
+
+    limit_count: Optional[int]
+    """
+    The number of spend velocity impacting transactions may not exceed this limit in
+    the period. Transactions exceeding this limit will be declined. A spend velocity
+    impacting transaction is a transaction that has been authorized, and optionally
+    settled, or a force post (a transaction that settled without prior
+    authorization).
+    """
