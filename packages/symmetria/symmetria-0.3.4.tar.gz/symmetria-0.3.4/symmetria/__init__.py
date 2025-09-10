3d7a823b41494fcd15c3b34246399314a3d64953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 03:13:38 +0000

    Add package symmetria/0.3.4/symmetria-0.3.4.tar.gz

---
+++ packages/symmetria/symmetria-0.3.4.tar.gz/symmetria-0.3.4/symmetria/__init__.py
@@ -0,0 +1,17 @@
+from symmetria.elements.cycle import Cycle
+from symmetria.elements.permutation import Permutation
+from symmetria.generators.random.api import random, random_generator, random_permutation
+from symmetria.generators.algorithm.api import generate, permutation_generator
+from symmetria.elements.cycle_decomposition import CycleDecomposition
+
+__version__ = "0.3.4"
+__all__ = [
+    "__version__",
+    "permutation_generator",
+    "random",
+    "random_generator",
+    "random_permutation",
+    "Permutation",
+    "Cycle",
+    "CycleDecomposition",
+]
