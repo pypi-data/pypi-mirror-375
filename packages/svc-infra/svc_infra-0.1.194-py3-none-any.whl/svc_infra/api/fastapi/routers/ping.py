813393150396bace9d6637da29becd1e072c694e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 04:06:20 +0000

    Add package svc-infra/0.1.194/svc_infra-0.1.194-py3-none-any.whl

---
+++ packages/svc-infra/svc_infra-0.1.194-py3-none-any.whl/svc_infra/api/fastapi/routers/ping.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter, Response, status
+import logging
+
+router = APIRouter()
+
+@router.get("/ping", status_code=status.HTTP_200_OK)
+def ping():
+    logging.info("Health check: /ping endpoint accessed. Service is responsive.")
+    return Response(status_code=status.HTTP_200_OK)
