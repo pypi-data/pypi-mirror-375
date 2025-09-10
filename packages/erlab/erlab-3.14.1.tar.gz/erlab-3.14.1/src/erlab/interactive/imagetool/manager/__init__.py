b2cfe952af610f445f7fc184fd0dfe121821d89d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 15:05:30 +0000

    Add package erlab/3.14.1/erlab-3.14.1.tar.gz

---
+++ packages/erlab/erlab-3.14.1.tar.gz/erlab-3.14.1/src/erlab/interactive/imagetool/manager/__init__.py
@@ -0,0 +1,140 @@
+"""Manager for multiple ImageTool windows.
+
+.. image:: ../images/manager_light.png
+    :align: center
+    :alt: ImageToolManager window screenshot
+    :class: only-light
+    :width: 600px
+
+.. only:: format_html
+
+    .. image:: ../images/manager_dark.png
+        :align: center
+        :alt: ImageToolManager window screenshot
+        :class: only-dark
+        :width: 600px
+
+This module provides a GUI application for managing multiple ImageTool windows. The
+application can be started by running the script `itool-manager` from the command line
+in the environment where the package is installed.
+
+Python scripts communicate with the manager using a socket connection with the default
+port number 45555. The port number can be changed by setting the environment variable
+``ITOOL_MANAGER_PORT``.
+
+"""
+
+from __future__ import annotations
+
+__all__ = [
+    "PORT",
+    "ImageToolManager",
+    "is_running",
+    "load_in_manager",
+    "main",
+    "replace_data",
+    "show_in_manager",
+]
+
+
+import logging
+import sys
+import typing
+
+from qtpy import QtGui, QtWidgets
+
+import erlab
+from erlab.interactive.imagetool.manager._mainwindow import _ICON_PATH, ImageToolManager
+from erlab.interactive.imagetool.manager._server import (
+    PORT,
+    is_running,
+    load_in_manager,
+    replace_data,
+    show_in_manager,
+)
+
+logger = logging.getLogger(__name__)
+
+
+_manager_instance: ImageToolManager | None = None
+"""Reference to the running manager instance."""
+
+_always_use_socket: bool = False
+"""Internal flag to use sockets within same process for test coverage."""
+
+
+class _InitDialog(QtWidgets.QDialog):
+    def __init__(self) -> None:
+        super().__init__()
+        layout = QtWidgets.QVBoxLayout()
+        self.setLayout(layout)
+
+        self.label = QtWidgets.QLabel(
+            "An instance of ImageToolManager is already running.\n"
+            "Retry after closing the existing instance."
+        )
+        self.buttonBox = QtWidgets.QDialogButtonBox(
+            QtWidgets.QDialogButtonBox.StandardButton.Ok
+            | QtWidgets.QDialogButtonBox.StandardButton.Cancel
+        )
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        layout.addWidget(self.label)
+        layout.addWidget(self.buttonBox)
+
+
+def main(execute: bool = True) -> None:
+    """Start the ImageToolManager application.
+
+    Running ``itool-manager`` from a shell will invoke this function.
+    """
+    global _manager_instance
+
+    if sys.platform == "win32":  # pragma: no cover
+        import ctypes
+
+        # Set the AppUserModelID for Windows taskbar grouping
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
+            "erlab.imagetool.manager"
+        )
+
+    qapp = typing.cast(
+        "QtWidgets.QApplication | None", QtWidgets.QApplication.instance()
+    )
+    if not qapp:
+        qapp = QtWidgets.QApplication(sys.argv)
+
+    qapp.setStyle("Fusion")
+    qapp.setWindowIcon(QtGui.QIcon(_ICON_PATH))
+    qapp.setApplicationName("imagetool-manager")
+    qapp.setApplicationDisplayName("ImageTool Manager")
+    qapp.setApplicationVersion(erlab.__version__)
+
+    while is_running():  # pragma: no branch
+        dialog = _InitDialog()
+        if dialog.exec() != QtWidgets.QDialog.DialogCode.Accepted:
+            break
+    else:
+        _manager_instance = ImageToolManager()
+        _manager_instance.show()
+        _manager_instance.activateWindow()
+        if execute:
+            qapp.exec()
+            _manager_instance = None
+
+
+def _get_recent_directory() -> str:
+    """Return the most recent directory used by the ImageToolManager.
+
+    Used internally to set the default directory for various file dialogs in tools
+    launched inside the manager. Returns an empty string if no directory has been set
+    yet, or if the manager is running in a different process.
+
+    """
+    if (
+        _manager_instance is not None
+        and _manager_instance._recent_directory is not None
+    ):
+        return str(_manager_instance._recent_directory)
+    return ""
---
+++ packages/erlab/erlab-3.14.1.tar.gz/erlab-3.14.1/src/erlab/interactive/imagetool/manager/_server.py
@@ -0,0 +1,379 @@
+"""Server that listens to incoming data."""
+
+from __future__ import annotations
+
+__all__ = [
+    "PORT",
+    "is_running",
+    "load_in_manager",
+    "replace_data",
+    "show_in_manager",
+]
+
+import concurrent.futures
+import contextlib
+import errno
+import logging
+import os
+import pathlib
+import pickle
+import socket
+import struct
+import tempfile
+import threading
+import typing
+import uuid
+
+import xarray as xr
+from qtpy import QtCore
+
+import erlab
+from erlab.interactive.imagetool._mainwindow import _ITOOL_DATA_NAME
+
+if typing.TYPE_CHECKING:
+    from collections.abc import Collection, Iterable
+
+    import numpy.typing as npt
+
+
+logger = logging.getLogger(__name__)
+
+PORT: int = int(os.getenv("ITOOL_MANAGER_PORT", "45555"))
+"""Port number for the manager server.
+
+The default port number 45555 can be overridden by setting the environment variable
+``ITOOL_MANAGER_PORT``.
+"""
+
+
+def _save_pickle(obj: typing.Any, filename: str) -> None:
+    with open(filename, "wb") as file:
+        pickle.dump(obj, file, protocol=-1)
+
+
+def _load_pickle(filename: str) -> typing.Any:
+    with open(filename, "rb") as file:
+        return pickle.load(file)
+
+
+def _recv_all(conn, size):
+    data = b""
+    while len(data) < size:
+        part = conn.recv(size - len(data))
+        data += part
+    return data
+
+
+class _ManagerServer(QtCore.QThread):
+    sigReceived = QtCore.Signal(list, dict)
+    sigLoadRequested = QtCore.Signal(list, str, dict)
+    sigReplaceRequested = QtCore.Signal(list, list)
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.stopped = threading.Event()
+
+    def _handle_client(self, conn: socket.socket) -> None:
+        with conn:
+            # If no data is received in 2 seconds, close the connection
+            conn.settimeout(2.0)
+
+            # Receive the size of the data first
+            data_size_bytes = _recv_all(conn, 4)
+            data_size = struct.unpack(">L", data_size_bytes)[0]
+
+            # Receive the actual data
+            kwargs_bytes = _recv_all(conn, data_size)
+
+            try:
+                kwargs: dict = pickle.loads(kwargs_bytes)
+                logger.debug("Received data: %s", kwargs)
+
+                if "__ping" in kwargs:
+                    logger.debug("Received ping")
+                    return
+
+                # kwargs["__filename"] contains the list of paths to pickled data
+
+                # If "__loader_name" key is present, kwargs["__filename"] will be a list
+                # of paths to raw data files instead
+
+                files = kwargs.pop("__filename")
+                loader_name = kwargs.pop("__loader_name", None)
+
+                # If key "__replace_idxs" is present, replaces data in existing tools
+                # instead of adding new ones. Other kwargs are ignored.
+
+                replace_index_list = kwargs.pop("__replace_idxs", None)
+
+                if loader_name is not None:
+                    self.sigLoadRequested.emit(files, loader_name, kwargs)
+                    logger.debug("Emitted file and loader info")
+                else:
+                    if replace_index_list:
+                        self.sigReplaceRequested.emit(
+                            [_load_pickle(f) for f in files], replace_index_list
+                        )
+                    else:
+                        self.sigReceived.emit([_load_pickle(f) for f in files], kwargs)
+                    logger.debug("Emitted loaded data")
+                    # Clean up temporary files
+                    for f in files:
+                        os.remove(f)
+                        dirname = os.path.dirname(f)
+                        if os.path.isdir(dirname):
+                            with contextlib.suppress(OSError):
+                                os.rmdir(dirname)
+                    logger.debug("Cleaned up temporary files")
+
+            except (
+                pickle.UnpicklingError,
+                AttributeError,
+                EOFError,
+                ImportError,
+                IndexError,
+            ):
+                logger.exception("Failed to unpickle received data")
+
+    def run(self) -> None:
+        self.stopped.clear()
+        logger.debug("Starting server...")
+
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        try:
+            soc.bind(("127.0.0.1", PORT))
+            soc.listen()
+            logger.info("Server is listening...")
+
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                while not self.stopped.is_set():
+                    try:
+                        soc.settimeout(1.0)
+                        conn, addr = soc.accept()
+                        logger.debug("Connection accepted from %s", addr)
+                        executor.submit(self._handle_client, conn)
+                    except TimeoutError:
+                        # Timeout occurred, continue to accept new connections
+                        continue
+                    except Exception:
+                        if not self.stopped.is_set():
+                            logger.exception(
+                                "Unexpected error while accepting connection"
+                            )
+                        break
+        except Exception:
+            logger.exception("Server encountered an error")
+        finally:
+            soc.close()
+            logger.debug("Socket closed")
+
+
+def _send_dict(contents: dict[str, typing.Any]) -> None:
+    contents = pickle.dumps(contents, protocol=-1)
+
+    logger.debug("Connecting to server")
+    client_socket = socket.socket()
+
+    try:
+        client_socket.connect(("localhost", PORT))
+    except Exception:
+        logger.exception("Failed to connect to server")
+    else:
+        # Send the size of the data first
+        logger.debug("Sending data")
+        client_socket.sendall(struct.pack(">L", len(contents)))
+        client_socket.sendall(contents)
+    finally:
+        client_socket.close()
+
+    logger.debug("Data sent successfully")
+
+
+def _ping_server() -> bool:
+    """Ping the ImageToolManager server.
+
+    Returns
+    -------
+    bool
+        True if the ping was successful, False otherwise.
+    """
+    client_socket = socket.socket()
+
+    to_send = pickle.dumps({"__ping": True}, protocol=-1)
+    try:
+        client_socket.connect(("localhost", PORT))
+    except (TimeoutError, ConnectionRefusedError):
+        return False
+    else:
+        client_socket.sendall(struct.pack(">L", len(to_send)))
+        client_socket.sendall(to_send)
+    finally:
+        client_socket.close()
+    return True
+
+
+def is_running() -> bool:
+    """Check whether an instance of ImageToolManager is active.
+
+    Returns
+    -------
+    bool
+        True if an instance of ImageToolManager is running, False otherwise.
+    """
+    if erlab.interactive.imagetool.manager._manager_instance is not None:
+        return True
+    return _ping_server()
+
+
+def load_in_manager(
+    paths: Iterable[str | os.PathLike], loader_name: str, **load_kwargs
+) -> None:
+    """Load and display data in the ImageToolManager.
+
+    Parameters
+    ----------
+    paths
+        List of paths containing the data to be displayed in the ImageTool window.
+    loader_name
+        Name of the loader to use to load the data. The loader must be registered in
+        :attr:`erlab.io.loaders`.
+    **load_kwargs
+        Additional keyword arguments passed onto the load method of the loader.
+    """
+    if not erlab.interactive.imagetool.manager.is_running():
+        raise RuntimeError(
+            "ImageToolManager is not running. Please start the ImageToolManager "
+            "application before using this function"
+        )
+
+    path_list: list[str] = []
+    for p in paths:
+        path = pathlib.Path(p)
+        if not path.exists():
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
+        path_list.append(str(path))
+
+    loader: str = erlab.io.loaders[
+        loader_name
+    ].name  # Trigger exception if loader_name is not registered
+
+    if (
+        erlab.interactive.imagetool.manager._manager_instance is not None
+        and not erlab.interactive.imagetool.manager._always_use_socket
+    ):
+        # If the manager is running in the same process, directly pass the data
+        erlab.interactive.imagetool.manager._manager_instance._data_load(
+            path_list, loader, load_kwargs
+        )
+        return
+
+    load_kwargs["__filename"] = path_list
+    load_kwargs["__loader_name"] = loader
+
+    _send_dict(load_kwargs)
+
+
+def show_in_manager(
+    data: Collection[xr.DataArray | npt.NDArray]
+    | xr.DataArray
+    | npt.NDArray
+    | xr.Dataset
+    | xr.DataTree,
+    **kwargs,
+) -> None:
+    """Create and display ImageTool windows in the ImageToolManager.
+
+    Parameters
+    ----------
+    data
+        The data to be displayed in the ImageTool window. See :func:`itool
+        <erlab.interactive.imagetool.itool>` for more information.
+    link
+        Whether to enable linking between multiple ImageTool windows, by default
+        `False`.
+    link_colors
+        Whether to link the color maps between multiple linked ImageTool windows, by
+        default `True`.
+    **kwargs
+        Keyword arguments passed onto :class:`ImageTool
+        <erlab.interactive.imagetool.ImageTool>`.
+
+    """
+    if not erlab.interactive.imagetool.manager.is_running():
+        raise RuntimeError(
+            "ImageToolManager is not running. Please start the ImageToolManager "
+            "application before using this function"
+        )
+
+    logger.debug("Parsing input data into DataArrays")
+
+    if isinstance(data, xr.Dataset) and _ITOOL_DATA_NAME in data:
+        # Dataset created with ImageTool.to_dataset()
+        input_data: list[xr.DataArray] | list[xr.Dataset] = [data]
+    else:
+        input_data = erlab.interactive.imagetool.core._parse_input(data)
+
+    if (
+        erlab.interactive.imagetool.manager._manager_instance is not None
+        and not erlab.interactive.imagetool.manager._always_use_socket
+    ):
+        # If the manager is running in the same process, directly pass the data
+        erlab.interactive.imagetool.manager._manager_instance._data_recv(
+            input_data, kwargs
+        )
+        return
+
+    # Save the data to a temporary file
+    logger.debug("Pickling data to temporary files")
+    tmp_dir = tempfile.mkdtemp(prefix="erlab_manager_")
+
+    files: list[str] = []
+
+    for dat in input_data:
+        fname = str(uuid.uuid4())
+        fname = os.path.join(tmp_dir, fname)
+        _save_pickle(dat.load(), fname)
+        files.append(fname)
+
+    kwargs["__filename"] = files
+
+    _send_dict(kwargs)
+
+
+def replace_data(
+    index: int | Collection[int],
+    data: Collection[xr.DataArray | npt.NDArray]
+    | xr.DataArray
+    | npt.NDArray
+    | xr.Dataset
+    | xr.DataTree,
+) -> None:
+    """Replace data in existing ImageTool windows.
+
+    Parameters
+    ----------
+    index
+        Index or indices of the ImageTool windows to replace data in. If ``data``
+        corresponds to a single ImageTool window, for instance a single DataArray,
+        ``index`` is allowed to be a single integer. If ``data`` corresponds to multiple
+        ImageTool windows, for instance a list of DataArrays, ``index`` must be a list
+        of integers with the same length as ``data``. All indices must be valid
+        indices of existing ImageTool windows.
+    data
+        Data to replace the existing data in the ImageTool windows. See :func:`itool
+        <erlab.interactive.imagetool.itool>` for more information.
+    """
+    data_list = erlab.interactive.imagetool.core._parse_input(data)
+
+    if isinstance(index, int):
+        index = [index]
+
+    if len(data_list) != len(index):
+        raise ValueError(
+            f"Mismatch between number of data ({len(data_list)}) "
+            f"and provided indices ({len(index)})"
+        )
+
+    show_in_manager(data_list, __replace_idxs=index)
