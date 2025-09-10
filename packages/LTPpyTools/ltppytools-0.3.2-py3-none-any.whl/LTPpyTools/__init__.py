4e14da3c4b163abfc357413a6630f43908d416bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 03:13:21 +0000

    Add package LTPpyTools/0.3.2/ltppytools-0.3.2-py3-none-any.whl

---
+++ packages/LTPpyTools/ltppytools-0.3.2-py3-none-any.whl/LTPpyTools/__init__.py
@@ -0,0 +1,15 @@
+""""""
+"""by LTPLAX"""
+"""version 0.3.2"""
+"""Please use Tools.init before use"""
+from .music import music
+from .flush import flush
+from .log import log
+from .option import option
+__all__=["music","flush","log","option"]
+
+def init():
+    print("LTPpyTools 0.1 Normal mode ready.")
+
+
+
