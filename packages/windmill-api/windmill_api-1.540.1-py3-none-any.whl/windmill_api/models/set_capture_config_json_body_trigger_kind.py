7fcf57ed4651763e30fd72fb12313445cf0541a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 04:06:27 +0000

    Add package windmill-api/1.540.1/windmill_api-1.540.1-py3-none-any.whl

---
+++ packages/windmill-api/windmill_api-1.540.1-py3-none-any.whl/windmill_api/models/set_capture_config_json_body_trigger_kind.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class SetCaptureConfigJsonBodyTriggerKind(str, Enum):
+    EMAIL = "email"
+    GCP = "gcp"
+    HTTP = "http"
+    KAFKA = "kafka"
+    MQTT = "mqtt"
+    NATS = "nats"
+    POSTGRES = "postgres"
+    SQS = "sqs"
+    WEBHOOK = "webhook"
+    WEBSOCKET = "websocket"
+
+    def __str__(self) -> str:
+        return str(self.value)
