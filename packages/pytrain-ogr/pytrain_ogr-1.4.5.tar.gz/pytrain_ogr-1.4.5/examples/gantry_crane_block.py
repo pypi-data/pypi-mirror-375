4d01fdddf708319f3f28b8160ce8115a80cd431b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 07:05:05 +0000

    Add package pytrain-ogr/1.4.5/pytrain_ogr-1.4.5.tar.gz

---
+++ packages/pytrain-ogr/pytrain_ogr-1.4.5.tar.gz/pytrain_ogr-1.4.5/examples/gantry_crane_block.py
@@ -0,0 +1,37 @@
+# noinspection PyUnresolvedReferences,PyPackageRequirements
+from pytrain import GantryCrane, PowerDistrict, Switch
+
+g = GantryCrane(
+    address=87,
+    cab_left_pin=20,
+    cab_right_pin=21,
+    ro_left_pin=12,
+    ro_right_pin=16,
+    bo_down_pin=8,
+    bo_up_pin=7,
+    mag_pin=23,
+    led_pin=24,
+    cab_rotary_encoder=True,
+)
+
+pd13 = PowerDistrict(
+    13,
+    on_pin=17,
+    off_pin=27,
+    on_led_pin=22,
+)
+
+pd14 = PowerDistrict(
+    14,
+    on_pin=10,
+    off_pin=25,
+    on_led_pin=26,
+)
+
+sw1 = Switch(
+    1,
+    thru_pin=5,
+    out_pin=6,
+    thru_led_pin=13,
+    out_led_pin=19,
+)
