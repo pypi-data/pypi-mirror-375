5203c6e4cc67b97d14b9c02b315dfd8b43c9a821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 14:06:28 +0000

    Add package streamlit-ai-voice-chat/0.1.8/streamlit_ai_voice_chat-0.1.8.tar.gz

---
+++ packages/streamlit-ai-voice-chat/streamlit_ai_voice_chat-0.1.8.tar.gz/streamlit_ai_voice_chat-0.1.8/streamlit_ai_voice_chat/frontend/build/precache-manifest.52999d9ecfa99273d6cea40273fff0a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d877823ede3332c4c3f7884380b9bd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a00b04e1d8fe490a990e",
+    "url": "./static/css/main.934f655c.chunk.css"
+  },
+  {
+    "revision": "e24aafc57b69b385a626",
+    "url": "./static/js/2.4707f9ef.chunk.js"
+  },
+  {
+    "revision": "a00b04e1d8fe490a990e",
+    "url": "./static/js/main.c33dd945.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
