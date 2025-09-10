e4cc5536f57cdcc8099decfceb983522e4fa93b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 14:06:31 +0000

    Add package DuckyFrameworks/0.1.12/duckyframeworks-0.1.12.tar.gz

---
+++ packages/DuckyFrameworks/duckyframeworks-0.1.12.tar.gz/duckyframeworks-0.1.12/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DuckyFrameworks",            # nombre de tu paquete
+    version="0.1.12",                  # versión inicial
+    packages=find_packages(include=["DF*", "DF.*"]),          # busca automáticamente tus submódulos
+    install_requires=[                 # dependencias externas (si tienes)
+        # "kivy>=2.2.0",
+    ],
+    author="José Pato",
+    author_email="patodequeso222@gmail.com",
+    description="La libreria oficial de Ducky (no Duckyngscript) para poder usar Frameworks de manera sencilla y hay mucho de donde escojer",
+    long_description=open("README.md", encoding="utf-8").read(),  # opcional
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
