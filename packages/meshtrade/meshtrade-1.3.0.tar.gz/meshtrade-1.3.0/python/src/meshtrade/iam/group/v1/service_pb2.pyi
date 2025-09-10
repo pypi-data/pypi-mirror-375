8a9816500bae365a2f1ed6994670d6fe3f906c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 03:13:16 +0000

    Add package meshtrade/1.3.0/meshtrade-1.3.0.tar.gz

---
+++ packages/meshtrade/meshtrade-1.3.0.tar.gz/meshtrade-1.3.0/python/src/meshtrade/iam/group/v1/service_pb2.pyi
@@ -0,0 +1,38 @@
+from meshtrade.iam.group.v1 import group_pb2 as _group_pb2
+from meshtrade.iam.role.v1 import role_pb2 as _role_pb2
+from meshtrade.option.v1 import method_type_pb2 as _method_type_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from collections.abc import Iterable as _Iterable, Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class GetGroupRequest(_message.Message):
+    __slots__ = ("name",)
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    name: str
+    def __init__(self, name: _Optional[str] = ...) -> None: ...
+
+class ListGroupsRequest(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class ListGroupsResponse(_message.Message):
+    __slots__ = ("groups",)
+    GROUPS_FIELD_NUMBER: _ClassVar[int]
+    groups: _containers.RepeatedCompositeFieldContainer[_group_pb2.Group]
+    def __init__(self, groups: _Optional[_Iterable[_Union[_group_pb2.Group, _Mapping]]] = ...) -> None: ...
+
+class SearchGroupsRequest(_message.Message):
+    __slots__ = ("name",)
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    name: str
+    def __init__(self, name: _Optional[str] = ...) -> None: ...
+
+class SearchGroupsResponse(_message.Message):
+    __slots__ = ("groups",)
+    GROUPS_FIELD_NUMBER: _ClassVar[int]
+    groups: _containers.RepeatedCompositeFieldContainer[_group_pb2.Group]
+    def __init__(self, groups: _Optional[_Iterable[_Union[_group_pb2.Group, _Mapping]]] = ...) -> None: ...
