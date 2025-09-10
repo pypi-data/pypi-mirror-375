802efa8d4ec753b7acfeff76865dfca5ab4a257a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 08:05:41 +0000

    Add package py-lerchs-grossmann/0.0.2/py_lerchs_grossmann-0.0.2-py3-none-any.whl

---
+++ packages/py-lerchs-grossmann/py_lerchs_grossmann-0.0.2-py3-none-any.whl/py_lerchs_grossmann/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2025-present MauMau <mau.aravena@outlook.com>
+#
+# SPDX-License-Identifier: BSD-3-Clause
+from .build_df import (
+    main,
+)
+
+__all__ = [
+    "main",
+]
