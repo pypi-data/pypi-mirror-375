2c87d0da8620ae6dcd546fa80d60e857d6cf3218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 17:05:51 +0000

    Add package autumn-py/2.7.1/autumn_py-2.7.1-py3-none-any.whl

---
+++ packages/autumn-py/autumn_py-2.7.1-py3-none-any.whl/autumn/models/meta.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import TYPE_CHECKING, Optional
+
+from pydantic import BaseModel, ConfigDict
+
+from .products import ProductItem
+
+if TYPE_CHECKING:
+    from typing import Dict, Any
+
+    Metadata = Dict[str, Any]
+
+
+__all__ = ("Empty", "AppEnv", "AttachOption", "CustomerData")
+
+
+class Empty(BaseModel): ...
+
+
+class AppEnv(str, Enum):
+    PRODUCTION = "production"
+    SANDBOX = "sandbox"
+    LIVE = "live"
+
+
+class AttachOption(BaseModel):
+    feature_id: str
+    quantity: int
+
+
+class CustomerData(BaseModel):
+    name: Optional[str] = None
+    email: Optional[str] = None
+    fingerprint: Optional[str] = None
+
+
+class CheckoutLine(BaseModel):
+    description: str
+    amount: int
+    item: ProductItem
+
+
+class Cycle(BaseModel):
+    starts_at: int
+    total: float
+
+
+class QueryDP(BaseModel):
+    period: int
+
+    model_config = ConfigDict(extra="allow")
+
+    def get_usage(self, key: str) -> Optional[int]:
+        if self.__pydantic_extra__ is None:
+            return None
+
+        return self.__pydantic_extra__.get(key)
