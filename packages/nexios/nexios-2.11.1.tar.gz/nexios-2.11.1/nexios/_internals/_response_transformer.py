5afca2440a26e1afea268a19209fe9c646a8a575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 16:06:45 +0000

    Add package nexios/2.11.1/nexios-2.11.1.tar.gz

---
+++ packages/nexios/nexios-2.11.1.tar.gz/nexios-2.11.1/nexios/_internals/_response_transformer.py
@@ -0,0 +1,62 @@
+import asyncio
+import typing
+
+from nexios.dependencies import Context, current_context
+from nexios.http import Request, Response
+from nexios.http.response import BaseResponse
+from nexios.types import ASGIApp, Receive, Scope, Send
+from nexios.utils.async_helpers import is_async_callable
+from nexios.utils.concurrency import run_in_threadpool
+from pydantic import BaseModel
+from enum import Enum
+
+
+async def request_response(
+    func: typing.Callable[[Request, Response], typing.Awaitable[Response]],
+) -> ASGIApp:
+    """
+    Takes a function or coroutine `func(request) -> response`,
+    and returns an ASGI application.
+    """
+    assert asyncio.iscoroutinefunction(func), "Endpoints must be async"
+
+    async def app(scope: Scope, receive: Receive, send: Send) -> None:
+        response_manager = Response._instance
+        if not response_manager:
+            request = Request(scope, receive, send)
+            response_manager = Response(request)
+        else:
+            request = response_manager._request
+
+        ctx = Context(
+            request=request,
+            response=response_manager,
+            user=getattr(request, "user", None),
+            app=request.app,
+            base_app=getattr(request, "base_app", None),
+        )
+        token = current_context.set(ctx)
+        try:
+            if is_async_callable(func):
+                func_result = await func(
+                    request, response_manager, **request.path_params
+                )
+            else:
+                func_result = await run_in_threadpool(
+                    func, request, response_manager, **request.path_params
+                )
+        finally:
+            current_context.reset(token)
+        if isinstance(func_result, (dict, list, str, int, float)):
+            response_manager.json(func_result)
+
+        elif isinstance(func_result, BaseResponse):
+            response_manager.make_response(func_result)
+        elif isinstance(func_result, BaseModel):
+            response_manager.json(func_result.model_dump())
+        elif isinstance(func_result, Enum):
+            response_manager.json(func_result.value)
+        response = response_manager.get_response()
+        return await response(scope, receive, send)
+
+    return app
