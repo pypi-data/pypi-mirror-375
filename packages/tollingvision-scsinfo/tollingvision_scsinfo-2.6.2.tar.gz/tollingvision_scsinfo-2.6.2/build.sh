b88e8a1fb7f28d82762cb96b1edd8826281eb16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 17:05:41 +0000

    Add package tollingvision-scsinfo/2.6.2/tollingvision_scsinfo-2.6.2.tar.gz

---
+++ packages/tollingvision-scsinfo/tollingvision_scsinfo-2.6.2.tar.gz/tollingvision_scsinfo-2.6.2/build.sh
@@ -0,0 +1,1 @@
+python3 -m grpc_tools.protoc --python_out=src/tollingvision_scsinfo/ --grpc_python_out=src/tollingvision_scsinfo/ --proto_path=../../service/src/main/proto/ TollingVisionService.proto
