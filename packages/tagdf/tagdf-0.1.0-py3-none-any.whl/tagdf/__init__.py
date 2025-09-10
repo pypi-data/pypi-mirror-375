b4fc6d1cf36fbbaec9c3f57a34166ebab2fcbd22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 14:07:00 +0000

    Add package tagdf/0.1.0/tagdf-0.1.0-py3-none-any.whl

---
+++ packages/tagdf/tagdf-0.1.0-py3-none-any.whl/tagdf/__init__.py
@@ -0,0 +1,12 @@
+"""tagdf - Annotate and label data structures.
+
+This package will provide simple utilities to annotate and label
+Pandas DataFrames and general Python objects.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Idin"
+__email__ = "py@idin.net"
+__description__ = "Tools for annotating and labelling DataFrames and Python objects"
+
+__all__ = []
