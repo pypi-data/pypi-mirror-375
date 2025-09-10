550b9f16bfdc24c50b93e32a8dd65e1cd9589ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 07:04:47 +0000

    Add package electric-emission-cost/0.0.5/electric_emission_cost-0.0.5-py2.py3-none-any.whl

---
+++ packages/electric-emission-cost/electric_emission_cost-0.0.5-py2.py3-none-any.whl/electric_emission_cost/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Main package for electric-emission-cost (EEC)."""
+
+__author__ = "WE3Lab"
+__email__ = "fchapin@stanford.edu"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.5"
+
+
+def get_module_version():
+    return __version__
