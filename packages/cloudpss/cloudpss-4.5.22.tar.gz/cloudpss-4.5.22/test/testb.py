91663bc8e532a5372a41c3adecb421d25f792f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 16:06:39 +0000

    Add package cloudpss/4.5.22/cloudpss-4.5.22.tar.gz

---
+++ packages/cloudpss/cloudpss-4.5.22.tar.gz/cloudpss-4.5.22/test/testb.py
@@ -0,0 +1,16 @@
+import os,sys
+import time
+
+import socketio
+import threading
+import struct
+Uninitialized = type("Uninitialized", (), {})()
+
+def pp():
+   x=b=c=q=x=t=Uninitialized
+   print(x,b,c,q,x,t)
+                         
+if __name__ == '__main__':
+   pp()
+   
+        
