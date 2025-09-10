aaff4d3986dbba7d999a21e12c79557a58298f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 04:06:48 +0000

    Add package vansarah/1.0.2/vansarah-1.0.2-py3-none-any.whl

---
+++ packages/vansarah/vansarah-1.0.2-py3-none-any.whl/vansarah/__init__.py
@@ -0,0 +1,25 @@
+__version__ = '1.0.2'
+
+from loguru import logger
+import sys
+
+# Remove default handler
+logger.remove()
+
+# Add custom handler with clean format including module and line number
+logger.add(
+    sys.stderr,
+    format="<green>{time:HH:mm:ss}</green> | <cyan>{module:>16}:{line}</cyan> | <level>{level: >8}</level> | <level>{message}</level>",
+    colorize=True,
+    level="INFO" # "DEBUG" to enable logger.debug("message") and up prints 
+                 # "ERROR" to enable only logger.error("message") prints
+                 # etc
+)
+
+# Disable before release or as needed
+logger.disable("vansarah")
+
+from .model import KModel
+from .pipeline import KPipeline
+
+__all__ = ["KModel", "KPipeline"]
