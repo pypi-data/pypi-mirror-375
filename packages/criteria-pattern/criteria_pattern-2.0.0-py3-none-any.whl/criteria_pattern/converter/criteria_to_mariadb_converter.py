8856b9a8068a8ce8d09a302a008133cbde02770e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 17:05:34 +0000

    Add package criteria-pattern/2.0.0/criteria_pattern-2.0.0-py3-none-any.whl

---
+++ packages/criteria-pattern/criteria_pattern-2.0.0-py3-none-any.whl/criteria_pattern/converter/criteria_to_mariadb_converter.py
@@ -0,0 +1,30 @@
+"""
+Criteria to MariaDB converter module.
+"""
+
+from .criteria_to_mysql_converter import CriteriaToMysqlConverter
+
+
+class CriteriaToMariadbConverter(CriteriaToMysqlConverter):
+    """
+    Criteria to MariaDB converter.
+
+    MariaDB is highly compatible with MySQL, so this converter inherits from the MySQL converter. This separate class
+    allows for future MariaDB-specific optimizations or features.
+
+    Example:
+    ```python
+    from criteria_pattern import Criteria, Filter, Operator
+    from criteria_pattern.converter import CriteriaToMariadbConverter
+
+    is_adult = Criteria(filters=[Filter(field='age', operator=Operator.GREATER_OR_EQUAL, value=18)])
+    email_is_gmail = Criteria(filters=[Filter(field='email', operator=Operator.ENDS_WITH, value='@gmail.com')])
+    email_is_yahoo = Criteria(filters=[Filter(field='email', operator=Operator.ENDS_WITH, value='@yahoo.com')])
+
+    query, parameters = CriteriaToMariadbConverter.convert(criteria=is_adult & (email_is_gmail | email_is_yahoo), table='user')
+    print(query)
+    print(parameters)
+    # >>> SELECT * FROM user WHERE (age >= %(parameter_0)s AND (email LIKE '%%' || %(parameter_1)s OR email LIKE '%%' || %(parameter_2)s));
+    # >>> {'parameter_0': 18, 'parameter_1': '@gmail.com', 'parameter_2': '@yahoo.com'}
+    ```
+    """  # noqa: E501  # fmt: skip
