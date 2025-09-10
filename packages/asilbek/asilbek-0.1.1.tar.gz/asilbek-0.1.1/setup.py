0a12684b4a6693b9886343cbb432bd587bb308aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 03:13:36 +0000

    Add package asilbek/0.1.1/asilbek-0.1.1.tar.gz

---
+++ packages/asilbek/asilbek-0.1.1.tar.gz/asilbek-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="asilbek",
+    version="0.1.1",
+    description="Sadullayev Asilbek tomonidan yaratilgan Python kutubxona",
+    author="Asilbek Sadullayev",
+    packages=find_packages(),
+    python_requires=">=3.7",
+)
