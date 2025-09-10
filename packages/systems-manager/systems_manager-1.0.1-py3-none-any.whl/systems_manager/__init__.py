07cd11c20f15c30ad933ebc28e58186e04076006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 14:07:21 +0000

    Add package systems-manager/1.0.1/systems_manager-1.0.1-py3-none-any.whl

---
+++ packages/systems-manager/systems_manager-1.0.1-py3-none-any.whl/systems_manager/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from systems_manager.systems_manager import SystemsManager, main, setup_logging
+from systems_manager.systems_manager_mcp import systems_manager_mcp
+
+"""
+system-manager
+
+Install/Update/Clean/Manage your System!
+"""
+
+__all__ = ["SystemsManager", "main", "setup_logging", "systems_manager_mcp"]
