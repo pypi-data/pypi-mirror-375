9fcffba63ce9e986274ea96e826895cb95c043f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 03:13:38 +0000

    Add package aek-auto-mlbuilder/0.3.1/aek_auto_mlbuilder-0.3.1-py3-none-any.whl

---
+++ packages/aek-auto-mlbuilder/aek_auto_mlbuilder-0.3.1-py3-none-any.whl/aek_auto_mlbuilder/__init__.py
@@ -0,0 +1,5 @@
+from .base import BaseModel
+from .utils import split_data
+from .linear_regression import LinearRegressor
+from .logistic_regression import LogisticClassifier
+from .decision_tree import DecisionTreeModel
