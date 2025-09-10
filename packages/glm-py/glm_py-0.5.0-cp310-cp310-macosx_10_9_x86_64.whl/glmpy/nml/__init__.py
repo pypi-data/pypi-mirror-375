7f2d5382144a24d1731e9ad440329a6b8291e32d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 16:06:23 +0000

    Add package glm-py/0.5.0/glm_py-0.5.0-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/glm-py/glm_py-0.5.0-cp310-cp310-macosx_10_9_x86_64.whl/glmpy/nml/__init__.py
@@ -0,0 +1,5 @@
+# from .glm_nml_depr import (
+#     NML, NMLGLMSetup, NMLMixing, NMLWQSetup, NMLMorphometry, NMLTime,
+#     NMLOutput, NMLInitProfiles, NMLLight, NMLBirdModel, NMLSediment,
+#     NMLSnowIce, NMLMeteorology, NMLInflow, NMLOutflow
+# )
