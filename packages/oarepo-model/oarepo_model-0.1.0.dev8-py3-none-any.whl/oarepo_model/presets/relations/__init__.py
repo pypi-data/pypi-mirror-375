3cf3811e4ad1e0f614b3c9e768eebe5f8de949e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 17:05:31 +0000

    Add package oarepo-model/0.1.0.dev8/oarepo_model-0.1.0.dev8-py3-none-any.whl

---
+++ packages/oarepo-model/oarepo_model-0.1.0.dev8-py3-none-any.whl/oarepo_model/presets/relations/__init__.py
@@ -0,0 +1,15 @@
+#
+# Copyright (c) 2025 CESNET z.s.p.o.
+#
+# This file is a part of oarepo-model (see http://github.com/oarepo/oarepo-model).
+#
+# oarepo-model is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+"""A module for defining presets for model relations."""
+
+from __future__ import annotations
+
+from .record_relations import RecordRelationsPreset
+
+relations_preset = [RecordRelationsPreset]
