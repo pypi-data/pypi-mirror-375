b879bfd6a6cbfd65893421e02559b12c30fb44d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 17:05:43 +0000

    Add package memos/0.30.1/memos-0.30.1-py3-none-any.whl

---
+++ packages/memos/memos-0.30.1-py3-none-any.whl/memos/static/_app/immutable/chunks/CV91wnX_.js
@@ -0,0 +1,20 @@
+import"./CWj6FrbW.js";import{p as d,c as n,f as m,s as h,n as i,b as c,a as l}from"./DPjJDdIH.js";import{s as f,r as u}from"./BjMvBzXH.js";import{I as $}from"./bqOBoKf4.js";function L(o,a){d(a,!0);/**
+ * @license @lucide/svelte v0.507.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */let s=u(a,["$$slots","$$events","$$legacy"]);const p=[["path",{d:"M12 2v4"}],["path",{d:"m16.2 7.8 2.9-2.9"}],["path",{d:"M18 12h4"}],["path",{d:"m16.2 16.2 2.9 2.9"}],["path",{d:"M12 18v4"}],["path",{d:"m4.9 19.1 2.9-2.9"}],["path",{d:"M2 12h4"}],["path",{d:"m4.9 4.9 2.9 2.9"}]];$(o,f({name:"loader"},()=>s,{get iconNode(){return p},children:(r,v)=>{var t=n(),e=m(t);h(e,()=>a.children??i),c(r,t)},$$slots:{default:!0}})),l()}export{L};
