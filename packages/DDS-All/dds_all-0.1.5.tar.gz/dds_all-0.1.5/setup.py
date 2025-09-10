8e0be0582ebb4ab3a0a3973e8d11bf32c866a40a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 03:13:19 +0000

    Add package DDS-All/0.1.5/dds_all-0.1.5.tar.gz

---
+++ packages/DDS-All/dds_all-0.1.5.tar.gz/dds_all-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DDS_All",  # 保持与包目录名一致
+    version="0.1.5",  # 更新版本号
+    packages=find_packages(),
+    package_data={
+        "DDS_All": ["*.pyd", "*.dll"],  # 关键修正：包名改为 DDS_All
+    },
+    include_package_data=True,  # 确保包含非Python文件
+    description="",
+    author="JackyJia",
+)
