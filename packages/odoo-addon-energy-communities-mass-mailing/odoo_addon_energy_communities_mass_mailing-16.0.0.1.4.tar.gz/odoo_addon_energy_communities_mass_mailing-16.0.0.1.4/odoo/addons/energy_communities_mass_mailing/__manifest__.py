b277f71994f751eab0cc776f33f374e5b114755f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 16:06:01 +0000

    Add package odoo-addon-energy-communities-mass-mailing/16.0.0.1.4/odoo_addon_energy_communities_mass_mailing-16.0.0.1.4.tar.gz

---
+++ packages/odoo-addon-energy-communities-mass-mailing/odoo_addon_energy_communities_mass_mailing-16.0.0.1.4.tar.gz/odoo_addon_energy_communities_mass_mailing-16.0.0.1.4/odoo/addons/energy_communities_mass_mailing/__manifest__.py
@@ -0,0 +1,34 @@
+{
+    "name": "Mass mailing - Energy Communities",
+    "summary": """
+        Use mass_mailing on a energy communities setup
+    """,
+    "description": """
+        Use mass_mailing on a energy communities setup
+    """,
+    "author": "Som Comunitats",
+    "website": "https://git.coopdevs.org/coopdevs/comunitats-energetiques/odoo-ce",
+    "category": "Marketing",
+    "version": "16.0.0.1.4",
+    "license": "AGPL-3",
+    # any module necessary for this one to work correctly
+    "depends": [
+        "sale",
+        "energy_communities",
+        "mass_mailing",
+        "mass_mailing_partner",
+        "mass_mailing_list_dynamic",
+    ],
+    # always loaded
+    "data": [
+        "security/ir_rule_data.xml",
+        "security/res_users_role_data.xml",
+        "views/mailing_mailing_views_menus.xml",
+        "views/mailing_mailing_views.xml",
+        "views/mailing_list_views.xml",
+        "views/mailing_contact_views.xml",
+        "views/utm_views.xml",
+        "wizards/mail_compose_message_views.xml",
+        "wizards/partner_mail_list_wizard.xml",
+    ],
+}
