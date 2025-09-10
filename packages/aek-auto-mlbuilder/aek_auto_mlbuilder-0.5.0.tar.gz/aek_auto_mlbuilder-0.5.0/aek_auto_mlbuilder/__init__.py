ea4e6a9975ef9bab34a647f59ee70688b1ea6a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 17:05:31 +0000

    Add package aek-auto-mlbuilder/0.5.0/aek_auto_mlbuilder-0.5.0.tar.gz

---
+++ packages/aek-auto-mlbuilder/aek_auto_mlbuilder-0.5.0.tar.gz/aek_auto_mlbuilder-0.5.0/aek_auto_mlbuilder/__init__.py
@@ -0,0 +1,7 @@
+from .base import BaseModel
+from .utils import split_data
+from .linear_regression import LinearRegressor
+from .logistic_regression import LogisticClassifier
+from .decision_tree import DecisionTreeModel
+from .knn import KNNModel
+from .random_forest import RandomForestModel
