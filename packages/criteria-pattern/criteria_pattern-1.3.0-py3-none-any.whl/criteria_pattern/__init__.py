0b029e609f42148bd283d170aa3ed6d9e12511ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 16:06:52 +0000

    Add package criteria-pattern/1.3.0/criteria_pattern-1.3.0-py3-none-any.whl

---
+++ packages/criteria-pattern/criteria_pattern-1.3.0-py3-none-any.whl/criteria_pattern/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '1.3.0'
+
+from .models import Criteria, Direction, Filter, Operator, Order, PageNumber, PageSize
+
+__all__ = (
+    'Criteria',
+    'Direction',
+    'Filter',
+    'Operator',
+    'Order',
+    'PageNumber',
+    'PageSize',
+)
