419f4bc8af3c8891d1539e0a85831c5f6db49585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 03:13:32 +0000

    Add package php-framework-scaffolder/0.1.117/php_framework_scaffolder-0.1.117.tar.gz

---
+++ packages/php-framework-scaffolder/php_framework_scaffolder-0.1.117.tar.gz/php_framework_scaffolder-0.1.117/php_framework_scaffolder/frameworks/phpixie.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from php_framework_detector.core.models import FrameworkType
+
+from php_framework_scaffolder.frameworks.base import BaseFrameworkSetup
+
+
+class PhPixieSetup(BaseFrameworkSetup):
+    def __init__(self):
+        super().__init__(FrameworkType.PHPIXIE)
+
+    def get_setup_commands(self) -> list[list[str]]:
+        raise NotImplementedError('Not implemented')
+
+    def get_routes_command(self) -> list[str]:
+        raise NotImplementedError('Not implemented')
