5424571f283fa64171e800eb765e954efc2db1ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 04:05:37 +0000

    Add package yayarl/2.2.0/yayarl-2.2.0.tar.gz

---
+++ packages/yayarl/yayarl-2.2.0.tar.gz/yayarl-2.2.0/docs/changes.rst
@@ -0,0 +1,28 @@
+=========
+Changelog
+=========
+
+2.2.0 (2025-09-09)
+==================
+
+Features
+--------
+
+- Bring up to date with `aio-libs/yarl` (1.20.1)
+- Bump `requests` to `~ 2.32.0`
+
+2.1.0 (2023-11-15)
+==================
+
+Features
+--------
+
+- Bring up to date with `aio-libs/yarl` ()
+
+2.0.0 (2023-09-28)
+==================
+
+Features
+--------
+
+- First version of the yayarl fork; add `requests` functionality.
---
+++ packages/yayarl/yayarl-2.2.0.tar.gz/yayarl-2.2.0/packaging/pep517_backend/_compat.py
@@ -0,0 +1,27 @@
+"""Cross-python stdlib shims."""
+
+import os
+import sys
+from collections.abc import Iterator
+from contextlib import contextmanager
+from pathlib import Path
+
+if sys.version_info >= (3, 11):
+    from contextlib import chdir as chdir_cm
+
+    from tomllib import loads as load_toml_from_string
+else:
+    from tomli import loads as load_toml_from_string
+
+    @contextmanager  # type: ignore[no-redef]
+    def chdir_cm(path: "os.PathLike[str]") -> Iterator[None]:
+        """Temporarily change the current directory, recovering on exit."""
+        original_wd = Path.cwd()
+        os.chdir(path)
+        try:
+            yield
+        finally:
+            os.chdir(original_wd)
+
+
+__all__ = ("chdir_cm", "load_toml_from_string")  # noqa: WPS410
