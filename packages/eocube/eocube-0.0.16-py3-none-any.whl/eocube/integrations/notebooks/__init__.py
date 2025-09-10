63ce3dd86683cf6f82f1590dd7e81ce97b398f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 05:07:09 +0000

    Add package eocube/0.0.16/eocube-0.0.16-py3-none-any.whl

---
+++ packages/eocube/eocube-0.0.16-py3-none-any.whl/eocube/integrations/notebooks/__init__.py
@@ -0,0 +1,29 @@
+import os
+import s3fs
+
+from ..auth import ROCS_DEFAULT_STORAGE_ENDPOINT
+from ..auth.client import get_eocube_sign
+
+access_key = os.environ.get("AWS_ACCESS_KEY_ID")
+secret_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+session_token = os.environ.get("AWS_SESSION_TOKEN", ROCS_DEFAULT_STORAGE_ENDPOINT)
+
+fs = s3fs.S3FileSystem(
+    anon=False,
+    key=access_key,
+    secret=secret_key,
+    token=session_token,
+    asynchronous=False,
+)
+
+_sign = get_eocube_sign(fs)
+
+
+def setup_notebook():
+    import nest_asyncio
+
+    nest_asyncio.apply()
+
+
+def sign_in_place(entry):
+    _sign(entry)
