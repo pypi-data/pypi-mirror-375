26092206291fa993baaeba25e0026ce735f38c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 15:05:22 +0000

    Add package clinicedc/2.0.1/clinicedc-2.0.1.tar.gz

---
+++ packages/clinicedc/clinicedc-2.0.1.tar.gz/clinicedc-2.0.1/src/edc_lab_results/model_mixins/insulin_model_mixin.py
@@ -0,0 +1,40 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from edc_glucose.model_mixin_factory import fasting_model_mixin_factory
+
+from edc_constants.choices import YES_NO
+from edc_reportable.units import MICRO_IU_MILLILITER, MICRO_IU_MILLILITER_DISPLAY
+
+from ..model_mixin_factory import reportable_result_model_mixin_factory
+
+
+class BaseInsulinModelMixin(
+    reportable_result_model_mixin_factory(
+        utest_id="ins",
+        verbose_name="Insulin",
+        units_choices=((MICRO_IU_MILLILITER, MICRO_IU_MILLILITER_DISPLAY),),
+        validators=[MinValueValidator(0.0), MaxValueValidator(999.0)],
+    ),
+    models.Model,
+):
+    class Meta:
+        abstract = True
+
+
+class InsulinModelMixin(
+    BaseInsulinModelMixin,
+    fasting_model_mixin_factory(
+        None,
+        fasting=models.CharField(
+            verbose_name="Has the participant fasted?",
+            max_length=15,
+            choices=YES_NO,
+            null=True,
+            blank=False,
+            help_text="As reported by patient",
+        ),
+    ),
+    models.Model,
+):
+    class Meta:
+        abstract = True
