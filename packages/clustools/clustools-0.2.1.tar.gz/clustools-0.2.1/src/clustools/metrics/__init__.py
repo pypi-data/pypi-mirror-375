dae7164115bb7a786bae25e12f0533d862288a64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-10 16:06:06 +0000

    Add package clustools/0.2.1/clustools-0.2.1.tar.gz

---
+++ packages/clustools/clustools-0.2.1.tar.gz/clustools-0.2.1/src/clustools/metrics/__init__.py
@@ -0,0 +1,5 @@
+"""Evaluation metrics for clustering results (scalar scores)."""
+
+from clustools.metrics.inertia import compute_inertia
+
+__all__ = ["compute_inertia"]
---
+++ packages/clustools/clustools-0.2.1.tar.gz/clustools-0.2.1/src/clustools/utils/__init__.py
@@ -0,0 +1,5 @@
+"""Utility functions for handling labels, cluster centers, and related tasks."""
+
+from clustools.utils import cluster_centers, labels
+
+__all__ = ["cluster_centers", "labels"]
