f1e95cf8a4043785bfddf9e0fcb023171d17356f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 08:06:39 +0000

    Add package lotterybr/1.0.2/lotterybr-1.0.2-py3-none-any.whl

---
+++ packages/lotterybr/lotterybr-1.0.2-py3-none-any.whl/lotterybr/__init__.py
@@ -0,0 +1,4 @@
+from .get_data import get_data, InvalidGameError, InvalidTypeError, InvalidLanguageError
+from .open_app import open_app, InvalidLanguageError
+
+__all__ = ["get_data", "open_app", "InvalidGameError", "InvalidTypeError", "InvalidLanguageError"]
