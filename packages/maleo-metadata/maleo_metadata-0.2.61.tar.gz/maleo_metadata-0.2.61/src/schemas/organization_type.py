73b4ce07ab9bfc44d5154d9da8a9515681f26c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 15:05:31 +0000

    Add package maleo-metadata/0.2.61/maleo_metadata-0.2.61.tar.gz

---
+++ packages/maleo-metadata/maleo_metadata-0.2.61.tar.gz/maleo_metadata-0.2.61/src/schemas/organization_type.py
@@ -0,0 +1,17 @@
+from maleo.schemas.request import (
+    ReadSingleQuery as BaseReadSingleQuery,
+    ReadPaginatedMultipleQuery,
+)
+from ..mixins.organization_type import Granularity
+
+
+class CommonQuery(Granularity):
+    pass
+
+
+class ReadSingleQuery(CommonQuery, BaseReadSingleQuery):
+    pass
+
+
+class ReadMultipleQuery(CommonQuery, ReadPaginatedMultipleQuery):
+    pass
