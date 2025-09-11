e684b43a3f4ddd6a75955eabd5a6e6eb86f75f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 08:07:04 +0000

    Add package pesapal_client/0.1.2/pesapal_client-0.1.2.tar.gz

---
+++ packages/pesapal_client/pesapal_client-0.1.2.tar.gz/pesapal_client-0.1.2/pesapal_client/exceptions.py
@@ -0,0 +1,9 @@
+class PesapalException(Exception):
+    def __init__(self, *, typ: str, code: str, message: str):
+        super().__init__()
+        self.type = typ
+        self.code = code
+        self.message = message
+
+    def __str__(self):
+        return f"({self.type} - {self.code}): {self.message}"
