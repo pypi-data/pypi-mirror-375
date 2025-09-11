fb5c0239d3bab82d2d9c62717f4034dc21922307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 08:06:52 +0000

    Add package daft/0.6.1/daft-0.6.1-cp39-abi3-manylinux_2_24_aarch64.whl

---
+++ packages/daft/daft-0.6.1-cp39-abi3-manylinux_2_24_aarch64.whl/daft/functions/__init__.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from daft.expressions import Expression
+
+from daft.functions.ai import (
+    classify_text,
+    embed_text,
+    embed_image,
+)
+from daft.functions.columnar import (
+    columns_sum,
+    columns_mean,
+    columns_avg,
+    columns_min,
+    columns_max,
+)
+from daft.functions.llm import llm_generate
+from daft.functions.misc import monotonically_increasing_id, format, file
+from daft.functions.window import (
+    row_number,
+    rank,
+    dense_rank,
+)
+
+
+__all__ = [
+    "classify_text",
+    "columns_avg",
+    "columns_max",
+    "columns_mean",
+    "columns_min",
+    "columns_sum",
+    "dense_rank",
+    "embed_image",
+    "embed_text",
+    "file",
+    "format",
+    "llm_generate",
+    "monotonically_increasing_id",
+    "rank",
+    "row_number",
+]
