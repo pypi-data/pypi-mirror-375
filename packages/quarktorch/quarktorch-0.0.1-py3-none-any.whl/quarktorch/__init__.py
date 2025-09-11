c11fe1c186ba97c1d88365a5478ad66702cfe786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 15:06:28 +0000

    Add package quarktorch/0.0.1/quarktorch-0.0.1-py3-none-any.whl

---
+++ packages/quarktorch/quarktorch-0.0.1-py3-none-any.whl/quarktorch/__init__.py
@@ -0,0 +1,14 @@
+from .core import Variable
+from .core import Function
+from .core import using_config
+from .core import no_grad
+from .core import as_array
+from .core import as_variable
+from .core import setup_variable
+from .core import Parameter
+
+__version__ = "0.0.1"
+__author__ = "Runke Zhong"
+__url__ = "https://github.com/SyJarvis/QuarkTorch"
+
+setup_variable()
