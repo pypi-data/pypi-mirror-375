054218eb1dffb210d46c97295d5a874f6e54ca00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 08:06:54 +0000

    Add package maleo-metadata/0.2.60/maleo_metadata-0.2.60-py3-none-any.whl

---
+++ packages/maleo-metadata/maleo_metadata-0.2.60-py3-none-any.whl/maleo/metadata/schemas/system_role.py
@@ -0,0 +1,17 @@
+from maleo.schemas.request import (
+    ReadSingleQuery as BaseReadSingleQuery,
+    ReadPaginatedMultipleQuery,
+)
+from ..mixins.system_role import Granularity
+
+
+class CommonQuery(Granularity):
+    pass
+
+
+class ReadSingleQuery(CommonQuery, BaseReadSingleQuery):
+    pass
+
+
+class ReadMultipleQuery(CommonQuery, ReadPaginatedMultipleQuery):
+    pass
---
+++ packages/maleo-metadata/maleo_metadata-0.2.60-py3-none-any.whl/maleo/metadata/schemas/user_type.py
@@ -0,0 +1,17 @@
+from maleo.schemas.request import (
+    ReadSingleQuery as BaseReadSingleQuery,
+    ReadPaginatedMultipleQuery,
+)
+from ..mixins.user_type import Granularity
+
+
+class CommonQuery(Granularity):
+    pass
+
+
+class ReadSingleQuery(CommonQuery, BaseReadSingleQuery):
+    pass
+
+
+class ReadMultipleQuery(CommonQuery, ReadPaginatedMultipleQuery):
+    pass
