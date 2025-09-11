1959c6d18fd156169c2b26c7de8805c2700d1b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 04:07:04 +0000

    Add package FuyaoDownloadMusic/0.0.1/fuyaodownloadmusic-0.0.1.tar.gz

---
+++ packages/FuyaoDownloadMusic/fuyaodownloadmusic-0.0.1.tar.gz/fuyaodownloadmusic-0.0.1/tests/test.py
@@ -0,0 +1,24 @@
+"""
+encoding:   -*- coding: utf-8 -*-
+@Time           :  2025/9/10 20:41
+@Project_Name   :  FuyaoDownloadMusic
+@Author         :  lhw
+@File_Name      :  test.py
+
+功能描述
+
+实现步骤
+
+"""
+from src.FuyaoDownloadMusic.download import DownloadMusic
+import FuyaoDownloadMusic
+
+dm = DownloadMusic(
+    musicSrcKey="netease",  # 音乐源
+    # cookieStr="...",  # 音乐平台的会员关键cookie
+)
+
+print(dm.search({
+    "keyword": "知我",
+    "limit": 30
+}))
