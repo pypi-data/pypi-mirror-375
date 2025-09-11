c92c0fcd9092e782514bc98fb6801bdb20f60865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 03:18:17 +0000

    Add package vlhmath/0.1.0/vlhmath-0.1.0-py3-none-any.whl

---
+++ packages/vlhmath/vlhmath-0.1.0-py3-none-any.whl/vlhmath/trigonometry.py
@@ -0,0 +1,26 @@
+import math
+
+class Trigonometry:
+    @staticmethod
+    def sin(a):
+        return math.sin(a)
+
+    @staticmethod
+    def cos(a):
+        return math.cos(a)
+
+    @staticmethod
+    def tan(a):
+        return math.tan(a)
+
+    @staticmethod
+    def asin(a):
+        return math.asin(a)
+
+    @staticmethod
+    def acos(a):
+        return math.acos(a)
+
+    @staticmethod
+    def atan(a):
+        return math.atan(a)
