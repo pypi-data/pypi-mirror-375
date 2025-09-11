9339c25436f9720558e71e11601e9836611957c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 06:06:14 +0000

    Add package plone.behavior/2.0.3/plone_behavior-2.0.3.tar.gz

---
+++ packages/plone.behavior/plone_behavior-2.0.3.tar.gz/plone_behavior-2.0.3/setup.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "2.0.3"
+desc = "Infrastructure for maintaining a registry of available behaviors"
+
+long_description = (
+    f"{Path('README.rst').read_text()}\n{Path('CHANGES.rst').read_text()}"
+)
+
+setup(
+    name="plone.behavior",
+    version=version,
+    description=desc,
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    # Get more strings from
+    # https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+    ],
+    keywords="Plone behavior registry",
+    author="Martin Aspeli",
+    author_email="optilude@gmail.com",
+    maintainer="Plone Foundation - Release Team",
+    maintainer_email="releaseteam@plone.org",
+    url="https://github.com/plone/plone.behavior",
+    license="BSD",
+    packages=find_packages("src"),
+    namespace_packages=["plone"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "setuptools",
+        "zope.annotation",
+        "zope.component",
+        "zope.configuration",
+        "zope.interface",
+        "zope.schema",
+    ],
+    extras_require={
+        "test": [
+            "zope.lifecycleevent",
+        ],
+    },
+    entry_points="""
+    """,
+)
---
+++ packages/plone.behavior/plone_behavior-2.0.3.tar.gz/plone_behavior-2.0.3/src/plone/behavior/__init__.py
@@ -0,0 +1,7 @@
+# Convenience import
+from plone.behavior.annotation import AnnotationStorage  # noqa
+
+import logging
+
+
+logger = logging.getLogger("plone.behavior")
