6c42e12f621976e2ea35b0ea94e64e808e585e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 05:06:50 +0000

    Add package plone.locking/4.0.0a1/plone_locking-4.0.0a1.tar.gz

---
+++ packages/plone.locking/plone_locking-4.0.0a1.tar.gz/plone_locking-4.0.0a1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "4.0.0a1"
+
+setup(
+    name="plone.locking",
+    version=version,
+    description="webdav locking support",
+    long_description=open("README.rst").read() + "\n" + open("CHANGES.rst").read(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="locking webdav plone",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://pypi.org/project/plone.locking",
+    license="GPL version 2",
+    packages=find_packages(),
+    namespace_packages=["plone"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    extras_require=dict(
+        test=[
+            "plone.app.contenttypes[test]",
+            "plone.app.testing",
+            "plone.dexterity",
+            "plone.testing",
+        ]
+    ),
+    install_requires=[
+        "setuptools",
+        "plone.base",
+        "Zope",
+    ],
+)
