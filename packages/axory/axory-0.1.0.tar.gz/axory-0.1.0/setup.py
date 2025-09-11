0527085489615fc638963217f6ee9d85e2f82194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 04:06:53 +0000

    Add package axory/0.1.0/axory-0.1.0.tar.gz

---
+++ packages/axory/axory-0.1.0.tar.gz/axory-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read long description from README.md safely
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="axory",
+    version="0.1.0",  # Increment this for future releases
+    packages=find_packages(),  # Automatically find packages
+    install_requires=[
+        "requests>=2.30.0",  # Pin a minimum version
+    ],
+    author="AxoryAI",
+    author_email="ayush.sahu@axory.ai",
+    description="AxoryAI SDK for Deepfake Detection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.10",
+    url="https://github.com/ayushmansahu601/detectifai_sdk",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+    include_package_data=True,  # Include non-Python files if needed
+)
