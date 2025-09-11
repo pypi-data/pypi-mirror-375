6ed099fc1bede0e23993fe4b9e89d8970277e1cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 04:07:08 +0000

    Add package hdit/0.0.1b6/hdit-0.0.1b6-py3-none-any.whl

---
+++ packages/hdit/hdit-0.0.1b6-py3-none-any.whl/hdit/modules/attention/basic_core.py
@@ -0,0 +1,29 @@
+import torch
+from functools import reduce
+from .. import flags
+
+
+@flags.compile_wrap
+def scale_for_cosine_sim(q, k, scale, eps):
+    dtype = reduce(torch.promote_types, (q.dtype, k.dtype, scale.dtype, torch.float32))
+    sum_sq_q = torch.sum(q.to(dtype)**2, dim=-1, keepdim=True)
+    sum_sq_k = torch.sum(k.to(dtype)**2, dim=-1, keepdim=True)
+    sqrt_scale = torch.sqrt(scale.to(dtype))
+    scale_q = sqrt_scale * torch.rsqrt(sum_sq_q + eps)
+    scale_k = sqrt_scale * torch.rsqrt(sum_sq_k + eps)
+    return q * scale_q.to(q.dtype), k * scale_k.to(k.dtype)
+
+
+@flags.compile_wrap
+def scale_for_cosine_sim_qkv(qkv, scale, eps):
+    q, k, v = qkv.unbind(2)
+    q, k = scale_for_cosine_sim(q, k, scale[:, None], eps)
+    return torch.stack((q, k, v), dim=2)
+
+
+def checkpoint(function, *args, **kwargs):
+    if flags.get_checkpointing():
+        kwargs.setdefault("use_reentrant", True)
+        return torch.utils.checkpoint.checkpoint(function, *args, **kwargs)
+    else:
+        return function(*args, **kwargs)
---
+++ packages/hdit/hdit-0.0.1b6-py3-none-any.whl/hdit/modules/models/hourglass_diffusion_transformer/__init__.py
@@ -0,0 +1,6 @@
+from . import core
+from . import layers
+from .core import HourglassVisionTransformer
+from .core import HourglassVisionTransformer as HViT
+from .core import HourglassDiffusionTransformer
+from .core import HourglassDiffusionTransformer as HDiT
