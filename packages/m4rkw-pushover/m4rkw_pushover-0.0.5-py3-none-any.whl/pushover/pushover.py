8c9043135f70b06af1812d78a694a7970e5feb6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 08:07:08 +0000

    Add package m4rkw-pushover/0.0.5/m4rkw_pushover-0.0.5-py3-none-any.whl

---
+++ packages/m4rkw-pushover/m4rkw_pushover-0.0.5-py3-none-any.whl/pushover/pushover.py
@@ -0,0 +1,28 @@
+import requests
+
+ENDPOINT = "https://api.pushover.net/1/messages.json"
+
+class Pushover:
+
+    def __init__(self, user_key: str, api_token: str):
+        self.user_key = user_key
+        self.api_token = api_token
+
+    def send_message(self, message: str, title: str | None = None, priority: int = 0, sound: str | None = None, url: str | None = None) -> dict:
+        return self.send(message, title, priority, sound, url)
+
+    def send(self, message: str, title: str | None = None, priority: int = 0, sound: str | None = None, url: str | None = None) -> dict:
+        payload = {
+            "token": self.api_token,
+            "user": self.user_key,
+            "message": message,
+            "priority": priority,
+            "url": url
+        }
+        if title:
+            payload["title"] = title
+        if sound:
+            payload["sound"] = sound
+
+        response = requests.post(ENDPOINT, data=payload)
+        return response.json()
