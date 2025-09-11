edb142d5691f8babb17cc1e7454b0b980c60a50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 06:05:53 +0000

    Add package plone.portlets/3.0.3/plone_portlets-3.0.3.tar.gz

---
+++ packages/plone.portlets/plone_portlets-3.0.3.tar.gz/plone_portlets-3.0.3/setup.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "3.0.3"
+
+long_description = (
+    f"{Path('README.rst').read_text()}\n{Path('CHANGES.rst').read_text()}"
+)
+
+setup(
+    name="plone.portlets",
+    version=version,
+    description="An extension of zope.viewlet to support dynamic portlets",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    # Get more strings from
+    # https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="portlet viewlet",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://github.com/plone/plone.portlets",
+    license="GPL version 2",
+    packages=find_packages("src"),
+    namespace_packages=["plone"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    extras_require=dict(
+        test=[
+            "zope.browserpage",
+            "zope.configuration",
+            "zope.location",
+            "zope.security",
+            "zope.site",
+            "zope.testing",
+        ]
+    ),
+    install_requires=[
+        "BTrees",
+        "setuptools",
+        "ZODB",
+        "plone.memoize",
+        "persistent",
+        "zope.annotation",
+        "zope.component",
+        "zope.container",
+        "zope.contentprovider",
+        "zope.interface",
+        "zope.location",
+        "zope.publisher",
+        "zope.schema",
+    ],
+)
