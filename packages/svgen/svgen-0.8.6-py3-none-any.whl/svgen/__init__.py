489772043b637f1198232f31fc95cb2b247bbd5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 14:05:37 +0000

    Add package svgen/0.8.6/svgen-0.8.6-py3-none-any.whl

---
+++ packages/svgen/svgen-0.8.6-py3-none-any.whl/svgen/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.2.3
+# hash=57d97050b2a832983524d8749c66786c
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A tool for working with scalable vector graphics."
+PKG_NAME = "svgen"
+VERSION = "0.8.6"
---
+++ packages/svgen/svgen-0.8.6-py3-none-any.whl/svgen/element/svg.py
@@ -0,0 +1,53 @@
+"""
+svgen - A module for the 'svg' element.
+"""
+
+# third-party
+from vcorelib.dict import GenericStrDict
+
+# internal
+from svgen.attribute import XMLNS, Attribute
+from svgen.attribute.viewbox import ViewBox
+from svgen.element import Element
+from svgen.element.rect import Rect
+
+
+class Svg(Element):
+    """A class for svg elements."""
+
+    def __init__(
+        self, viewbox: ViewBox, document: bool = True, **extra
+    ) -> None:
+        """Construct a new svg element (or document)."""
+
+        self.viewbox = viewbox
+        attrs: list[Attribute] = [self.viewbox]
+        if document:
+            attrs.append(XMLNS)
+        super().__init__(attrib=attrs, **extra)
+
+
+def add_background_grid(
+    svg: Svg, background: GenericStrDict, grid: GenericStrDict
+) -> None:
+    """
+    Add background and grid objects to an svg element, if they're specified
+    in their respective configurations.
+    """
+
+    del grid
+
+    to_add: list[Element] = []
+
+    # Add a colored background rectangle, if at least 'color' is specified.
+    if "color" in background:
+        to_add.append(
+            Rect.centered(
+                svg.viewbox,
+                color=background["color"],
+                prop=background.get("property", "fill"),
+            )
+        )
+
+    # Add any new children to the beginning of the document.
+    svg.children = to_add + svg.children
