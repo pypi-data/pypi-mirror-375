f6cf3442571d5b17903724f78b5e54da891ed85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 04:06:34 +0000

    Add package taulu/0.8.0/taulu-0.8.0-cp313-cp313t-win32.whl

---
+++ packages/taulu/taulu-0.8.0-cp313-cp313t-win32.whl/taulu/__init__.py
@@ -0,0 +1,17 @@
+from .grid import GridDetector, TableGrid
+from .page_cropper import PageCropper
+from .header_aligner import HeaderAligner
+from .header_template import HeaderTemplate
+from .table_indexer import TableIndexer
+from .split import Split
+from .main import main
+
+__all__ = [
+    "GridDetector",
+    "TableGrid",
+    "PageCropper",
+    "HeaderAligner",
+    "HeaderTemplate",
+    "TableIndexer",
+    "Split",
+]
