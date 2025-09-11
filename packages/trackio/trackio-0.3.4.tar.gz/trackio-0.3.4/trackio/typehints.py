f34ebffcb3dad59da4a609662e285f355530e53b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 07:06:38 +0000

    Add package trackio/0.3.4/trackio-0.3.4.tar.gz

---
+++ packages/trackio/trackio-0.3.4.tar.gz/trackio-0.3.4/trackio/typehints.py
@@ -0,0 +1,17 @@
+from typing import Any, TypedDict
+
+from gradio import FileData
+
+
+class LogEntry(TypedDict):
+    project: str
+    run: str
+    metrics: dict[str, Any]
+    step: int | None
+
+
+class UploadEntry(TypedDict):
+    project: str
+    run: str
+    step: int | None
+    uploaded_file: FileData
