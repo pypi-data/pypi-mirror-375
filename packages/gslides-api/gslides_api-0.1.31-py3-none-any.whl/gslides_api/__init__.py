7b466262fb2cabe523d023fdeb06b85bece6273c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 03:18:29 +0000

    Add package gslides-api/0.1.31/gslides_api-0.1.31-py3-none-any.whl

---
+++ packages/gslides-api/gslides_api-0.1.31-py3-none-any.whl/gslides_api/__init__.py
@@ -0,0 +1,4 @@
+from ._version import __version__, __version_info__
+from .client import GoogleAPIClient, initialize_credentials
+from .presentation import Presentation
+from .page.slide import Slide
