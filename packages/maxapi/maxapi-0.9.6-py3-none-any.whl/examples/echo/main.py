f2c55c0ef9d6b0342273270c1a424bbe7de62b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 14:06:33 +0000

    Add package maxapi/0.9.6/maxapi-0.9.6-py3-none-any.whl

---
+++ packages/maxapi/maxapi-0.9.6-py3-none-any.whl/examples/echo/main.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+
+from maxapi import Bot, Dispatcher
+from maxapi.filters import F
+from maxapi.types import MessageCreated
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot('тут_ваш_токен')
+dp = Dispatcher()
+
+
+@dp.message_created(F.message.body.text)
+async def echo(event: MessageCreated):
+    await event.message.answer(f"Повторяю за вами: {event.message.body.text}")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
