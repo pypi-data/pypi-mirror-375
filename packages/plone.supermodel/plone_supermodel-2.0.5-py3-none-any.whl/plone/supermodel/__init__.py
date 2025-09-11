586a47297e8a4f1123f2c75b956d5e7bbb4c0a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 06:05:33 +0000

    Add package plone.supermodel/2.0.5/plone_supermodel-2.0.5-py3-none-any.whl

---
+++ packages/plone.supermodel/plone_supermodel-2.0.5-py3-none-any.whl/plone/supermodel/__init__.py
@@ -0,0 +1,46 @@
+from io import BytesIO
+from plone.supermodel import model
+from plone.supermodel import parser
+from plone.supermodel import serializer
+from plone.supermodel import utils
+from plone.supermodel.interfaces import FILENAME_KEY
+from plone.supermodel.interfaces import IXMLToSchema
+from zope.interface import moduleProvides
+
+
+# Cache models by absolute filename
+_model_cache = {}
+
+
+def xmlSchema(filename, schema="", policy="", _frame=2):
+    _model = loadFile(filename, policy=policy, _frame=_frame + 1)
+    return _model.schemata[schema]
+
+
+def loadFile(filename, reload=False, policy="", _frame=2):
+    path = utils.relativeToCallingPackage(filename, _frame)
+    if reload or path not in _model_cache:
+        parsed_model = parser.parse(path, policy=policy)
+        for schema in parsed_model.schemata.values():
+            schema.setTaggedValue(FILENAME_KEY, path)
+        _model_cache[path] = parsed_model
+    return _model_cache[path]
+
+
+def loadString(model, policy=""):
+    if not isinstance(model, bytes):
+        model = model.encode()
+    return parser.parse(BytesIO(model), policy=policy)
+
+
+def serializeSchema(schema, name=""):
+    return serializeModel(model.Model({name: schema}))
+
+
+def serializeModel(model):
+    return serializer.serialize(model)
+
+
+moduleProvides(IXMLToSchema)
+
+__all__ = ("xmlSchema", "loadFile", "loadString", "serializeSchema", "serializeModel")
---
+++ packages/plone.supermodel/plone_supermodel-2.0.5-py3-none-any.whl/plone/supermodel/converters.py
@@ -0,0 +1,127 @@
+from plone.supermodel.interfaces import IToUnicode
+from plone.supermodel.utils import fieldTypecast
+from zope.component import adapter
+from zope.dottedname.resolve import resolve
+from zope.interface import implementer
+from zope.schema.interfaces import IBytes
+from zope.schema.interfaces import IDate
+from zope.schema.interfaces import IDatetime
+from zope.schema.interfaces import IField
+from zope.schema.interfaces import IFromUnicode
+from zope.schema.interfaces import IInterfaceField
+from zope.schema.interfaces import IObject
+
+import datetime
+import time
+
+
+# Defaults
+
+
+@implementer(IFromUnicode)
+@adapter(IField)
+class DefaultFromUnicode:
+    def __init__(self, context):
+        self.context = context
+
+    def fromUnicode(self, value):
+        return fieldTypecast(self.context, value)
+
+
+@implementer(IToUnicode)
+@adapter(IField)
+class DefaultToUnicode:
+    def __init__(self, context):
+        self.context = context
+
+    def toUnicode(self, value):
+        if isinstance(value, bytes):
+            return value.decode()
+        return str(value)
+
+
+# Date/time fields
+
+
+@implementer(IFromUnicode)
+@adapter(IDate)
+class DateFromUnicode:
+    format = "%Y-%m-%d"
+
+    def __init__(self, context):
+        self.context = context
+
+    def fromUnicode(self, value):
+        t = time.strptime(value, self.format)
+        d = datetime.date(*t[:3])
+        self.context.validate(d)
+        return d
+
+
+@implementer(IFromUnicode)
+@adapter(IDatetime)
+class DatetimeFromUnicode:
+    format = "%Y-%m-%d %H:%M:%S"
+
+    def __init__(self, context):
+        self.context = context
+
+    def fromUnicode(self, value):
+        t = time.strptime(value[:19], self.format)
+        d = datetime.datetime(*t[:7])
+        self.context.validate(d)
+        return d
+
+
+# Interface fields
+
+
+@implementer(IFromUnicode)
+@adapter(IInterfaceField)
+class InterfaceFieldFromUnicode:
+    def __init__(self, context):
+        self.context = context
+
+    def fromUnicode(self, value):
+        iface = resolve(value)
+        self.context.validate(iface)
+        return iface
+
+
+@implementer(IToUnicode)
+@adapter(IInterfaceField)
+class InterfaceFieldToUnicode:
+    def __init__(self, context):
+        self.context = context
+
+    def toUnicode(self, value):
+        return str(value.__identifier__)
+
+
+# Object fields - we can read, but not write, as there is no way to know
+# the original dotted name of an object in memory (and the id() is not
+# particularly useful)
+
+
+@implementer(IFromUnicode)
+@adapter(IObject)
+class ObjectFromUnicode:
+    def __init__(self, context):
+        self.context = context
+
+    def fromUnicode(self, value):
+        obj = resolve(value)
+        self.context.validate(obj)
+        return obj
+
+
+@implementer(IToUnicode)
+@adapter(IBytes)
+class BytesToUnicode:
+    def __init__(self, context):
+        self.context = context
+
+    def toUnicode(self, value):
+        if isinstance(value, bytes):
+            return value.decode()
+        return str(value)
