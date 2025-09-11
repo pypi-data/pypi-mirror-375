a4247986d64c0c8f2ead2cf0a293c03621c223a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 08:07:04 +0000

    Add package autumn-py/2.7.3/autumn_py-2.7.3.tar.gz

---
+++ packages/autumn-py/autumn_py-2.7.3.tar.gz/autumn_py-2.7.3/autumn/aio/client.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional
+
+from .http import AsyncHTTPClient
+from ..client import Client
+from ..features import Features
+from ..customers import Customers
+from ..products import Products
+from ..error import AutumnError
+
+try:
+    import aiohttp
+except ImportError:
+    raise AutumnError(
+        "aiohttp is not installed. Please install it with `pip install aiohttp`",
+        "missing_dependency",
+    )
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
+    from .shed import (
+        AttachParams,
+        CheckParams,
+        TrackParams,
+        CheckoutParams,
+        QueryParams,
+    )
+
+__all__ = ("AsyncClient",)
+
+
+class AsyncClient(Client):
+    """
+    The ``async`` client class for interacting with the Autumn API.
+
+    This class is also exposed as ``autumn.Autumn``.
+
+    The ``AsyncClient`` automatically retries requests up to 5 times, exponentially backing off between attempts.
+
+    Note that session creation is lazy. This means that the ``AsyncClient`` will not attempt to create a session until the first request is made.
+
+    Parameters
+    ----------
+    token: str
+        The API key to use for authentication.
+    base_url: Optional[str]
+        The base URL of the Autumn API. This is useful when you are self-hosting Autumn and need to point to your own instance.
+    max_retries: int
+        The maximum number of retries to attempt for failed requests.
+    session: Optional[:class:`~aiohttp.ClientSession`]
+        The session to use for requests. If not provided, a new session will be created **lazily**.
+
+    Attributes
+    ----------
+    customers: :class:`~autumn.customers.Customers`
+        An interface to Autumn's customer API.
+    features: :class:`~autumn.features.Features`
+        An interface to Autumn's feature API.
+    products: :class:`~autumn.products.Products`
+        An interface to Autumn's product API.
+
+    """
+
+    attach: AttachParams  # type: ignore
+    check: CheckParams  # type: ignore
+    track: TrackParams  # type: ignore
+    checkout: CheckoutParams  # type: ignore
+    query: QueryParams  # type: ignore
+
+    def __init__(
+        self,
+        token: str,
+        *,
+        base_url: Optional[str] = None,
+        max_retries: int = 5,
+        session: Optional[aiohttp.ClientSession] = None,
+    ) -> None:
+        from .. import BASE_URL, VERSION
+
+        _base_url = base_url or BASE_URL
+        _base_url = _base_url.rstrip("/")
+
+        attempts = max_retries + 1
+        self.http = AsyncHTTPClient(
+            _base_url,
+            VERSION,
+            token,
+            attempts=attempts,
+            session=session,
+        )
+        self.customers = Customers(self.http)
+        self.features = Features(self.http)
+        self.products = Products(self.http)
+
+        self.cancel = self.products.cancel
+
+    async def __aenter__(self) -> Self:
+        return self
+
+    async def __aexit__(self, _exc_type, _exc, _tb):
+        await self.close()
+
+    async def close(self):
+        await self.http.close()
---
+++ packages/autumn-py/autumn_py-2.7.3.tar.gz/autumn_py-2.7.3/autumn/aio/shed.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Protocol
+
+if TYPE_CHECKING:
+    from typing import Optional, List, Awaitable, Dict, Any, Union, Literal
+
+    from ..models.meta import AttachOption
+    from ..models.features import Feature
+    from ..models.meta import CustomerData
+    from ..models.response import (
+        AttachResponse,
+        CheckResponse,
+        TrackResponse,
+        CheckoutResponse,
+        QueryResponse,
+    )
+
+
+__all__ = ("AttachParams", "CheckParams", "TrackParams")
+
+
+class AttachParams(Protocol):
+    def __call__(
+        self,
+        customer_id: str,
+        *,
+        product_id: Optional[str] = None,
+        product_ids: Optional[List[str]] = None,
+        success_url: Optional[str] = None,
+        force_checkout: bool = False,
+        features: Optional[List[Feature]] = None,
+        entity_id: Optional[str] = None,
+        customer_data: Optional[CustomerData] = None,
+        free_trial: Optional[bool] = None,
+        options: Optional[List[AttachOption]] = None,
+    ) -> Awaitable[AttachResponse]: ...
+
+
+class CheckParams(Protocol):
+    def __call__(
+        self,
+        customer_id: str,
+        *,
+        product_id: Optional[str] = None,
+        feature_id: Optional[str] = None,
+        required_balance: Optional[int] = 1,
+        send_event: bool = False,
+        with_preview: bool = False,
+        entity_id: Optional[str] = None,
+        customer_data: Optional[CustomerData] = None,
+    ) -> Awaitable[CheckResponse]: ...
+
+
+class TrackParams(Protocol):
+    def __call__(
+        self,
+        customer_id: str,
+        feature_id: Optional[str] = None,
+        *,
+        value: int = 1,
+        entity_id: Optional[str] = None,
+        event_name: Optional[str] = None,
+        idempotency_key: Optional[str] = None,
+        properties: Optional[Dict[str, Any]] = None,
+        customer_data: Optional[CustomerData] = None,
+    ) -> Awaitable[TrackResponse]: ...
+
+
+class CheckoutParams(Protocol):
+    def __call__(
+        self,
+        customer_id: str,
+        product_id: str,
+        *,
+        success_url: Optional[str] = None,
+    ) -> Awaitable[CheckoutResponse]: ...
+
+class QueryParams(Protocol):
+    def __call__(
+        self,
+        customer_id: str,
+        feature_id: Union[str, List[str]],
+        *,
+        range: Literal["24h", "7d", "30d", "90d", "last_cycle"] = "30d"
+    ) -> Awaitable[QueryResponse]: ...
