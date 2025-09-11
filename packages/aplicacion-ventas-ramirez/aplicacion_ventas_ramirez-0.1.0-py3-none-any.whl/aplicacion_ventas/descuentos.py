e581fd7ad2e2b7856e48b17b705b388f1f27c0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 15:05:25 +0000

    Add package aplicacion-ventas-ramirez/0.1.0/aplicacion_ventas_ramirez-0.1.0-py3-none-any.whl

---
+++ packages/aplicacion-ventas-ramirez/aplicacion_ventas_ramirez-0.1.0-py3-none-any.whl/aplicacion_ventas/descuentos.py
@@ -0,0 +1,10 @@
+from .excepciones import DescuentoInvalidoError
+
+class Descuentos:
+    def __init__(self,porcentaje):
+        if not (0 <= porcentaje <= 1):
+            raise DescuentoInvalidoError("El porcentaje de descuento debe estar entre 0 y 1.")
+        self.porcentaje=porcentaje
+
+    def aplicar_decuento(self,precio):
+        return precio * self.porcentaje
