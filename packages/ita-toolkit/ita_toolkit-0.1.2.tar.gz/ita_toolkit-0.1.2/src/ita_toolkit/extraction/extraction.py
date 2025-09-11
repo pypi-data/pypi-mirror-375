e2b2f312faa5f2e87df5c0574d84c274a89194a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 05:07:08 +0000

    Add package ita-toolkit/0.1.2/ita_toolkit-0.1.2.tar.gz

---
+++ packages/ita-toolkit/ita_toolkit-0.1.2.tar.gz/ita_toolkit-0.1.2/src/ita_toolkit/extraction/extraction.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+import pyodbc, struct
+from azure import identity
+
+class Extraction:
+    def get_azure_conn():
+        load_dotenv()
+        connection_string = os.environ.get("AZURE_SQL_CONNECTIONSTRING", None)
+
+        if connection_string is None:
+            raise Exception("Missing ENV Var: AZURE_SQL_CONNECTIONSTRING")
+
+        credential = identity.DefaultAzureCredential(exclude_interactive_browser_credential=False)
+        token_bytes = credential.get_token("https://database.windows.net/.default").token.encode("UTF-16-LE")
+        token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
+        SQL_COPT_SS_ACCESS_TOKEN = 1256
+
+        conn = pyodbc.connect(connection_string, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct})
+
+        return conn
