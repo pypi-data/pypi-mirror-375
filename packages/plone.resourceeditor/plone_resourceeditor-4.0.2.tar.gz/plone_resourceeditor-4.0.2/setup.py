a71e7a43071c4e62e4847661c3e91756d8a0d2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 06:05:51 +0000

    Add package plone.resourceeditor/4.0.2/plone_resourceeditor-4.0.2.tar.gz

---
+++ packages/plone.resourceeditor/plone_resourceeditor-4.0.2.tar.gz/plone_resourceeditor-4.0.2/setup.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "4.0.2"
+
+long_description = (
+    f"{Path('README.rst').read_text()}\n{Path('CHANGES.rst').read_text()}"
+)
+
+setup(
+    name="plone.resourceeditor",
+    version=version,
+    description="Integrates ACE editor into Plone",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    # Get more strings from
+    # https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="ace resource editor",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://github.com/plone/plone.resourceeditor",
+    license="GPL",
+    packages=find_packages("src"),
+    namespace_packages=["plone"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "plone.base",
+        "plone.resource",
+        "plone.staticresources",
+        "setuptools",
+        "Products.CMFCore",
+        "Zope",
+    ],
+    extras_require={"test": ["plone.app.testing"]},
+    entry_points="""
+    """,
+)
