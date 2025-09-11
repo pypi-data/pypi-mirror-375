d5c104825b8ff4bdd4bc99feec738c62c433702c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 04:06:56 +0000

    Add package stealthify/0.0.1/stealthify-0.0.1.tar.gz

---
+++ packages/stealthify/stealthify-0.0.1.tar.gz/stealthify-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import find_packages, setup
+
+setup(
+    name="stealthify",
+    version="0.0.1",
+    author="Arjun Shankar",
+    author_email="arjun.sha2425@gmail.com",
+    license="Apache-2.0",
+    packages=find_packages(),
+    long_description_content_type="text/markdown",
+)
