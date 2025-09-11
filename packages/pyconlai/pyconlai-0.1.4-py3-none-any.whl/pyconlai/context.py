5359ad1c5b63facc845e558c4adeef75bf245451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 03:18:13 +0000

    Add package pyconlai/0.1.4/pyconlai-0.1.4-py3-none-any.whl

---
+++ packages/pyconlai/pyconlai-0.1.4-py3-none-any.whl/pyconlai/context.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from omegaconf import OmegaConf
+
+
+@dataclass
+class ConLPoCArguments:
+    # data args
+    data_cache_dir: str = "./data/"
+    partition_method: str = "hetero"
+    partition_alpha: float = 10.
+    random_seed: int = 42
+
+    # optimizer
+    optimizer: str = "dsgd"
+
+    # device args
+    worker_num: int = 1
+
+    @classmethod
+    def from_yml(cls, yml_path: str):
+        loaded = OmegaConf.load(yml_path)
+        read_conf = ConLPoCArguments(**loaded.poc)
+
+        base_conf = OmegaConf.structured(ConLPoCArguments)
+        merged = OmegaConf.merge(base_conf, read_conf)
+        return cls(**merged)
---
+++ packages/pyconlai/pyconlai-0.1.4-py3-none-any.whl/pyconlai/datasets/mixin.py
@@ -0,0 +1,51 @@
+from logging import getLogger
+from abc import ABCMeta
+from torch.utils.data import Dataset, DataLoader
+from ..context import ConLPoCArguments
+
+
+class FedDatasetsMixin(metaclass=ABCMeta):
+    def __init__(self, poc_args: ConLPoCArguments, train_data: Dataset, valid_data: Dataset,
+                 batch_size: int, inner_loop: int =10, class_num=0):
+        self._train_data = train_data
+        self._valid_data = valid_data
+        self._class_num = class_num
+        self._batch_size = batch_size
+        self._inner_loop = inner_loop
+        self._clients_num = poc_args.worker_num
+        self._random_seed = poc_args.random_seed
+
+        self._partition_method = poc_args.partition_method
+        self._partition_alpha = poc_args.partition_alpha
+
+        self._train_data_loader = DataLoader(dataset=self._train_data, batch_size=self._batch_size,
+                                             shuffle=True, drop_last=True)
+        self._valid_data_loader = DataLoader(dataset=self._valid_data, batch_size=self._batch_size,
+                                             shuffle=False, drop_last=True)
+        self._train_data_num = len(self._train_data)
+        self._valid_data_num = len(self._valid_data)
+
+        self._fed_train_data_num = {}
+        self._fed_train_data_loader = {}
+        self._fed_valid_data_loader = {}
+
+        self._logger = getLogger("FedDatasets")
+
+    def fed_dataset(self, client_id: int):
+        return {
+            "train": self._fed_train_data_loader[client_id],
+            "valid": self._fed_valid_data_loader[client_id],
+            "num": self._fed_train_data_num[client_id]
+        }
+
+    @property
+    def train_data_loader(self) -> DataLoader:
+        return self._train_data_loader
+
+    @property
+    def valid_data_loader(self) -> DataLoader:
+        return self._valid_data_loader
+
+    @property
+    def class_num(self):
+        return self._class_num
