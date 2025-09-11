ffaae91e23d2ef1e261e385d8340a3d09c7e1d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 08:07:18 +0000

    Add package llama-stack-provider-lmeval/0.2.4/llama_stack_provider_lmeval-0.2.4.tar.gz

---
+++ packages/llama-stack-provider-lmeval/llama_stack_provider_lmeval-0.2.4.tar.gz/llama_stack_provider_lmeval-0.2.4/src/llama_stack_provider_lmeval/provider.py
@@ -0,0 +1,18 @@
+from llama_stack.providers.datatypes import (
+    AdapterSpec,
+    Api,
+    ProviderSpec,
+    remote_provider_spec,
+)
+
+
+def get_provider_spec() -> ProviderSpec:
+    return remote_provider_spec(
+        api=Api.eval,
+        adapter=AdapterSpec(
+            adapter_type="lmeval",
+            pip_packages=["kubernetes"],
+            config_class="llama_stack_provider_lmeval.config.LMEvalEvalProviderConfig",
+            module="llama_stack_provider_lmeval",
+        ),
+    )
