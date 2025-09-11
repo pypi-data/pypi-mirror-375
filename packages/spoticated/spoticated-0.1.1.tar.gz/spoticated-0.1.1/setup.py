291820499651f7697adefb101030eb7ba0ced0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 09:07:27 +0000

    Add package spoticated/0.1.1/spoticated-0.1.1.tar.gz

---
+++ packages/spoticated/spoticated-0.1.1.tar.gz/spoticated-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spoticated',
+    version='0.1.1',
+    packages=find_packages(),
+    author='fswair',
+    description='A python package that is spoticated',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
