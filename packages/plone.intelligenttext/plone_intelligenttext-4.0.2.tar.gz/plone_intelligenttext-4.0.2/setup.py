40a94e9a66cfdfc970e5459819842bd9a83b3b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 06:06:10 +0000

    Add package plone.intelligenttext/4.0.2/plone_intelligenttext-4.0.2.tar.gz

---
+++ packages/plone.intelligenttext/plone_intelligenttext-4.0.2.tar.gz/plone_intelligenttext-4.0.2/setup.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "4.0.2"
+
+long_description = (
+    f"{Path('README.rst').read_text()}\n{Path('CHANGES.rst').read_text()}"
+)
+
+setup(
+    name="plone.intelligenttext",
+    version=version,
+    description="Provides transforms from text/x-web-intelligent to "
+    "text/html and vice versa.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    # Get more strings from
+    # https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    keywords="transform intelligenttext",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://pypi.org/project/plone.intelligenttext",
+    license="GPL version 2",
+    packages=find_packages("src"),
+    namespace_packages=["plone"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "setuptools",
+    ],
+)
