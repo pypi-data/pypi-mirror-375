7362925e35d7b9790def5a5e0f7a8e4d679ae44a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 09:06:27 +0000

    Add package rlfe/0.1.0/rlfe-0.1.0-py3-none-any.whl

---
+++ packages/rlfe/rlfe-0.1.0-py3-none-any.whl/rlf/__init__.py
@@ -0,0 +1,13 @@
+"""
+RL Forever (rlf) - A minimal reinforcement learning package.
+
+Simple and forever!
+"""
+
+__version__ = "0.1.0"
+__author__ = "Mohammad"
+__email__ = "mohammad@example.com"
+
+from .core import hello_rl_forever, VERSION_INFO
+
+__all__ = ["hello_rl_forever", "VERSION_INFO", "__version__"]
