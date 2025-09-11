cdd0369745dd653633bb6f62a46ffb51d765dd09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 06:06:09 +0000

    Add package plone.memoize/3.0.4/plone_memoize-3.0.4-py3-none-any.whl

---
+++ packages/plone.memoize/plone_memoize-3.0.4-py3-none-any.whl/plone/memoize/compress.py
@@ -0,0 +1,11 @@
+"""XHTML Compressor"""
+
+from plone.memoize.interfaces import IXHTMLCompressor
+from zope.component import queryUtility
+
+
+def xhtml_compress(string):
+    util = queryUtility(IXHTMLCompressor)
+    if util is not None:
+        return util.compress(string)
+    return string
