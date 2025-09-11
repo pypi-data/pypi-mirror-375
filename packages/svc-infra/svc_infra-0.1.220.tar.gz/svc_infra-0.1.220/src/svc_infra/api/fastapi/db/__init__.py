2bbfcf90f3e319544f830121a462a67d3abffaec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 09:07:08 +0000

    Add package svc-infra/0.1.220/svc_infra-0.1.220.tar.gz

---
+++ packages/svc-infra/svc_infra-0.1.220.tar.gz/svc_infra-0.1.220/src/svc_infra/api/fastapi/db/__init__.py
@@ -0,0 +1,13 @@
+from .nosql import add_mongo_database, add_mongo_health, add_mongo_resources
+from .sql import add_sql_db, add_sql_health, add_sql_resources
+
+__all__ = [
+    # SQL
+    "add_sql_health",
+    "add_sql_db",
+    "add_sql_resources",
+    # NoSQL
+    "add_mongo_resources",
+    "add_mongo_database",
+    "add_mongo_health",
+]
---
+++ packages/svc-infra/svc_infra-0.1.220.tar.gz/svc_infra-0.1.220/src/svc_infra/db/nosql/__init__.py
@@ -0,0 +1,7 @@
+from .mongo.resource import MongoResource
+from .repository import NoSqlRepository
+
+__all__ = [
+    "MongoResource",
+    "NoSqlRepository",
+]
