38f9f83dd6c8e7ca66106551bfd4d673318a949a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 03:18:22 +0000

    Add package instavm/0.3.1/instavm-0.3.1-py3-none-any.whl

---
+++ packages/instavm/instavm-0.3.1-py3-none-any.whl/instavm/__init__.py
@@ -0,0 +1,16 @@
+from .sandbox_client import InstaVM
+from .exceptions import (
+    InstaVMError,
+    AuthenticationError, 
+    SessionError,
+    ExecutionError,
+    NetworkError,
+    RateLimitError,
+    BrowserError,
+    BrowserSessionError,
+    BrowserInteractionError,
+    BrowserTimeoutError,
+    BrowserNavigationError,
+    ElementNotFoundError,
+    QuotaExceededError
+)
