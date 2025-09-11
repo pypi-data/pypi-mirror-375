fd109e9ab6969cc27ed33d756a0ec839b489e0e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 15:06:44 +0000

    Add package ipgs/0.1.1/ipgs-0.1.1.tar.gz

---
+++ packages/ipgs/ipgs-0.1.1.tar.gz/ipgs-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ipgs',
+    version='0.1.1',
+    description='Interactive Progress Bar for Jupyter Notebooks with Visual Flair',
+    author='Abolfazl',
+    author_email='mohampouraz@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Jupyter',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
