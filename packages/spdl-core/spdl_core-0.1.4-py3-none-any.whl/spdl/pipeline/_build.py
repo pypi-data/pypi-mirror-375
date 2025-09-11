451cdcc85e5307973bc6121c5aefac5d48dd9037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 08:06:43 +0000

    Add package spdl-core/0.1.4/spdl_core-0.1.4-py3-none-any.whl

---
+++ packages/spdl-core/spdl_core-0.1.4-py3-none-any.whl/spdl/pipeline/_build.py
@@ -0,0 +1,327 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+__all__ = [
+    "build_pipeline",
+    "PipelineFailure",
+]
+
+import asyncio
+import logging
+from collections.abc import Callable, Coroutine
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
+from typing import TypeVar
+
+from ._components._pipe import _FailCounter, _ordered_pipe, _pipe
+from ._components._sink import _sink
+from ._components._source import _source
+from ._hook import TaskHook, TaskStatsHook as DefaultHook
+from ._pipeline import Pipeline
+from ._queue import AsyncQueue, StatsQueue as DefaultQueue
+from ._utils import create_task
+from .defs._defs import _PipeType, PipeConfig, PipelineConfig
+
+# pyre-strict
+
+T = TypeVar("T")
+U = TypeVar("U")
+
+_LG: logging.Logger = logging.getLogger(__name__)
+
+
+# The following is how we intend pipeline to behave. If the implementation
+# is inconsistent with the following, it is a bug.
+#
+# 1. Successful execution.
+#
+#    Assumption: The consumer keeps consuming the data from the output queue.
+#
+#    Each stage completes without failure.
+#    The source will pass EOF, and each stage receiving EOF will shut down,
+#    then pass EOF.
+#    EOF is not propagated to the output queue.
+#
+# 2. Failures at some stage. One or more stages fail.
+#
+#    Assumption: The consumer keeps consuming the data from the output queue.
+#
+#    Resolution:
+#      We want to keep the tasks downstream to failed ones alive, so that they
+#      can finish the ongoing works.
+#
+#    Actions:
+#      - The stage must pass EOF to the next queue, so that downstream stages
+#        can exit cleanly.
+#      - Passing EOF to the next queue can be blocked if the queue is full.
+#      - For a graceful exit, the assumption must be met.
+#      - The stages upstream to the failure must be cancelled.
+#
+# 3. Cancelled: The pipeline execution is cancelled.
+#
+#    Assumption: The consumer stopped consuming the data from the output queue.
+#
+#    Actions:
+#      - All the stages must be cancelled.
+#        (If tasks are blocked on async await, cancelling should do).
+#
+# Following the above intention, each stage must pass EOF to the output queue
+# unless the task was cancelled. See `_queue_stage_hook`.
+#
+
+
+# Use different class variables per pipeline
+def _get_fail_counter() -> type[_FailCounter]:
+    class _FC(_FailCounter):
+        num_failures: int = 0
+
+    return _FC
+
+
+# Used to append stage name with pipeline
+_PIPELINE_ID: int = -1
+
+
+def _get_task_name(i: int, cfg: PipeConfig[..., ...]) -> str:
+    name = f"{_PIPELINE_ID}:{i}:{cfg.name}"
+    if cfg._type == _PipeType.Pipe and cfg._args.concurrency > 1:
+        name = f"{name}[{cfg._args.concurrency}]"
+    return name
+
+
+def _build_pipeline_coro(
+    pipeline_cfg: PipelineConfig[T, U],
+    max_failures: int,
+    queue_class: type[AsyncQueue[...]],
+    task_hook_factory: Callable[[str], list[TaskHook]],
+    stage_id: int,
+) -> tuple[Coroutine[None, None, None], AsyncQueue[U]]:
+    # Note:
+    # Make sure that coroutines are ordered from source to sink.
+    # `_run_pipeline_coroutines` expects and rely on this ordering.
+    coros = []
+    queues = []
+
+    global _PIPELINE_ID
+    _PIPELINE_ID += 1
+
+    # source
+    name = f"{stage_id}:src"
+    stage_id += 1
+    queues.append(queue_class(f"{_PIPELINE_ID}:{name}_queue"))
+    coros.append((f"Pipeline::{name}", _source(pipeline_cfg.src.source, queues[0])))
+
+    _FailCounter = _get_fail_counter()
+
+    # pipes
+    for cfg in pipeline_cfg.pipes:
+        name = _get_task_name(stage_id, cfg)
+        stage_id += 1
+        queue_name = f"{name}_queue"
+        # Use buffer_size=2 so that it is possible that queue always
+        # has an item as long as upstream is fast enough.
+        # This make it possible for data readiness (occupancy rate)
+        # to reach 100%, instead of 99.999999%
+        queues.append(queue_class(queue_name, buffer_size=2))
+        in_queue, out_queue = queues[-2:]
+
+        match cfg._type:
+            case _PipeType.Pipe | _PipeType.Aggregate | _PipeType.Disaggregate:
+                coro = _pipe(
+                    name,
+                    in_queue,
+                    out_queue,
+                    cfg._args,
+                    _FailCounter(),
+                    task_hook_factory(name),
+                    max_failures,
+                )
+            case _PipeType.OrderedPipe:
+                coro = _ordered_pipe(
+                    name,
+                    in_queue,
+                    out_queue,
+                    cfg._args,
+                    _FailCounter(),
+                    task_hook_factory(name),
+                    max_failures,
+                )
+            case _:  # pragma: no cover
+                raise ValueError(f"Unexpected process type: {cfg._type}")
+
+        coros.append((f"Pipeline::{name}", coro))
+
+    # sink
+    name = f"{stage_id}:sink_queue"
+    output_queue = queue_class(
+        f"{_PIPELINE_ID}:{name}", buffer_size=pipeline_cfg.sink.buffer_size
+    )
+    coros.append(
+        (
+            f"Pipeline::{name}",
+            _sink(queues[-1], output_queue),
+        )
+    )
+
+    return _run_pipeline_coroutines(coros), output_queue  # pyre-ignore
+
+
+################################################################################
+# Coroutine execution logics
+################################################################################
+
+
+# TODO [Python 3.11]: Migrate to ExceptionGroup
+class PipelineFailure(RuntimeError):
+    """PipelineFailure()
+    Thrown by :py:class:`spdl.pipeline.Pipeline` when pipeline encounters an error.
+    """
+
+    def __init__(self, errs: dict[str, Exception]) -> None:
+        msg = []
+        for k, v in errs.items():
+            e = str(v)
+            msg.append(f"{k}:{e if e else type(v).__name__}")
+        msg.sort()
+
+        super().__init__(", ".join(msg))
+
+        # This is for unittesting.
+        self._errs = errs
+
+
+async def _run_pipeline_coroutines(
+    coros: list[tuple[str, Coroutine[None, None, None]]],
+) -> None:
+    """Run the pipeline coroutines and handle errors.
+
+    Args:
+        coros: The coroutines each corresponds to a stage in pipeline.
+            IMPORTANT: The coroutinues must be in the order of src to sink.
+    """
+    tasks = [create_task(coro, name=name) for name, coro in coros]
+    pending = set(tasks)
+
+    while pending:
+        # Note:
+        # `asyncio.wait` does not automatically propagate the cancellation to its children.
+        # For graceful shutdown, we manually cancel the child tasks.
+        #
+        # Also, it seems asyncio loop throws Cancellation on most outer task.
+        # I am not sure where this behavior is documented, but here is an example script to
+        # demonstrate the behavior.
+        # https://gist.github.com/mthrok/3a1c11c2d8012e29f4835679ac0baaee
+        try:
+            _, pending = await asyncio.wait(
+                pending, return_when=asyncio.FIRST_EXCEPTION
+            )
+        except asyncio.CancelledError:
+            for task in pending:
+                task.cancel()
+            await asyncio.wait(pending)
+            raise
+
+        if not pending:
+            break
+
+        # Check if any of the task caused an error.
+        # If an error occurred, we cancel the stages upstream to the failed one,
+        # then continue waiting the downstream ones.
+        for i in range(len(tasks) - 1, -1, -1):
+            task = tasks[i]
+            if task.done() and not task.cancelled() and task.exception() is not None:
+                if upstream := tasks[:i]:
+                    for task in upstream:
+                        task.cancel()
+                    await asyncio.wait(upstream)
+                break
+
+    errs = {}
+    for task in tasks:
+        if not task.cancelled() and (err := task.exception()) is not None:
+            errs[task.get_name()] = err
+
+    if errs:
+        raise PipelineFailure(errs)
+
+
+def build_pipeline(
+    pipeline_cfg: PipelineConfig[T, U],
+    /,
+    *,
+    num_threads: int,
+    max_failures: int = -1,
+    report_stats_interval: float = -1,
+    queue_class: type[AsyncQueue[...]] | None = None,
+    task_hook_factory: Callable[[str], list[TaskHook]] | None = None,
+    stage_id: int = 0,
+) -> Pipeline[U]:
+    """Build a pipeline from the config.
+
+    Args:
+        pipeline_cfg: The definition of the pipeline to build.
+
+        num_threads: The number of threads in the thread pool commonly used among
+            Pipeline stages.
+
+            .. note::
+
+               If a stage in the pipeline has dedicated executor, that stage will
+               use it.
+
+        max_failures: The maximum number of failures each pipe stage can have before
+            the pipeline is halted. Setting ``-1`` (default) disables it.
+
+        report_stats_interval: When provided, report the pipeline performance stats
+            every given interval. Unit: [sec]
+
+            This is only effective if there is no custom hook or custom AsyncQueue
+            provided for stages. The argument is passed to
+            :py:class:`TaskStatsHook` and :py:class:`StatsQueue`.
+
+            If a custom stage hook is provided and stats report is needed,
+            you can instantiate :py:class:`TaskStatsHook` and include
+            it in the hooks provided to :py:meth:`PipelineBuilder.pipe`.
+
+            Similarly if you are providing a custom :py:class:`AsyncQueue` class,
+            you need to implement the same logic by your self.
+
+        queue_class: If provided, override the queue class used to connect stages.
+            Must be a class (not an instance) inherits :py:class:`AsyncQueue`.
+
+        task_hook_factory: If provided, used to create task hook objects, given a
+            name of the stage. If ``None``, a default hook,
+            :py:class:`TaskStatsHook` is used.
+            To disable hooks, provide a function that returns an empty list.
+
+        stage_id: The index of the initial stage  used for logging.
+    """
+    desc = repr(pipeline_cfg)
+
+    _LG.debug("%s", desc)
+
+    def _hook_factory(name: str) -> list[TaskHook]:
+        return [DefaultHook(name=name, interval=report_stats_interval)]
+
+    _queue_class = (
+        partial(DefaultQueue, interval=report_stats_interval)
+        if queue_class is None
+        else queue_class
+    )
+
+    coro, queues = _build_pipeline_coro(
+        pipeline_cfg,
+        max_failures,
+        _queue_class,
+        _hook_factory if task_hook_factory is None else task_hook_factory,
+        stage_id,
+    )
+
+    executor = ThreadPoolExecutor(
+        max_workers=num_threads,
+        thread_name_prefix="spdl_worker_thread_",
+    )
+    return Pipeline(coro, queues, executor, desc=desc)
---
+++ packages/spdl-core/spdl_core-0.1.4-py3-none-any.whl/spdl/pipeline/defs/_defs.py
@@ -0,0 +1,390 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import inspect
+from collections.abc import AsyncIterable, Callable, Iterable, Sequence
+from concurrent.futures import Executor
+from dataclasses import dataclass
+from enum import IntEnum
+from functools import partial
+from typing import Any, Generic, Protocol, runtime_checkable, TypeAlias, TypeVar
+
+from .._convert import Callables
+
+# pyre-strict
+
+T = TypeVar("T")
+U = TypeVar("U")
+
+__all__ = [
+    "_PipeArgs",
+    "_PipeType",
+    "_TPipeInputs",
+    "Aggregate",
+    "Disaggregate",
+    "Pipe",
+    "PipeConfig",
+    "PipelineConfig",
+    "SinkConfig",
+    "SourceConfig",
+]
+
+
+################################################################################
+# Source
+################################################################################
+@dataclass
+class SourceConfig(Generic[T]):
+    """A source configuration.
+
+    A source in Pipeline yields a series of input data, that is going to be
+    processed by downstream pipes.
+
+    .. seealso::
+
+       - :py:config:`PipelineConfig`
+       - :py:func:`spdl.pipeline.build_pipeline`
+    """
+
+    source: Iterable | AsyncIterable
+    """Generates the series of source data."""
+
+    def __post_init__(self) -> None:
+        if not (hasattr(self.source, "__aiter__") or hasattr(self.source, "__iter__")):
+            raise ValueError("Source must be either generator or async generator.")
+
+    def __repr__(self) -> str:
+        # Overwrite source repr because it might print a huge string.
+        source = f"<{self.source.__class__.__name__} object at 0x{id(self.source):0x}>"
+        return f"SourceConfig({source=})"
+
+
+################################################################################
+# Pipe
+################################################################################
+class _PipeType(IntEnum):
+    Pipe = 1
+    OrderedPipe = 2
+    Aggregate = 3
+    Disaggregate = 4
+
+
+@dataclass
+class _PipeArgs(Generic[T, U]):
+    op: Callables[T, U]
+    executor: Executor | None = None
+    concurrency: int = 1
+    op_requires_eof: bool = False
+    # Used to pass EOF to op.
+    # Usually pipe does not pas EOF to op. This is because op is expected to be
+    #  stateless, and requiring users to handle EOF is cumbersome, and there is
+    # no real benefit.
+    # However, some ops are exception. The aggregation (with drop_last=False)
+    # requires to benotified when the pipeline reached the EOF, so that it can
+    # flush the buffered items.
+
+    def __post_init__(self) -> None:
+        if self.concurrency < 1:
+            raise ValueError(
+                f"`concurrency` value must be >= 1. Found: {self.concurrency}"
+            )
+
+
+@dataclass
+class PipeConfig(Generic[T, U]):
+    """PipeConfig()
+
+    A pipe configuration.
+
+    The pipe is an operation applied to an incoming data.
+
+    Use factory functions :py:func:`Pipe`, :py:func:`Aggregate`
+    or :py:func:`Disaggregate` to create a config.
+
+    .. seealso::
+
+       - :py:config:`PipelineConfig`
+       - :py:func:`spdl.pipeline.build_pipeline`
+    """
+
+    name: str
+    """Name of the pipe."""
+
+    _type: _PipeType
+
+    _args: _PipeArgs[T, U]
+
+    def __post_init__(self) -> None:
+        op = self._args.op
+        if inspect.iscoroutinefunction(op) or inspect.isasyncgenfunction(op):
+            if self._args.executor is not None:
+                raise ValueError("`executor` cannot be specified when op is async.")
+        if inspect.isasyncgenfunction(op):
+            if self._type == _PipeType.OrderedPipe:
+                raise ValueError(
+                    "pipe does not support async generator function "
+                    "when `output_order` is 'input'."
+                )
+
+    def __repr__(self) -> str:
+        match self._type:
+            case _PipeType.Pipe | _PipeType.OrderedPipe:
+                args = [
+                    f"concurrency={self._args.concurrency}",
+                ]
+                if self._args.executor is not None:
+                    args.append(f"executor={self._args.executor!r}")
+                if self._type == _PipeType.OrderedPipe:
+                    args.append("output_order='input'")
+                return f"{self.name}({', '.join(args)})"
+            case _PipeType.Aggregate | _PipeType.Disaggregate:
+                return self.name
+            case _:
+                return str(self._type)
+
+
+################################################################################
+# Sink
+################################################################################
+@dataclass
+class SinkConfig(Generic[T]):
+    """A sink configuration.
+
+    The sink is where the final result of pipeline is buffered.
+
+    .. seealso::
+
+       - :py:config:`PipelineConfig`
+       - :py:func:`spdl.pipeline.build_pipeline`
+    """
+
+    buffer_size: int
+
+    def __post_init__(self) -> None:
+        if not isinstance(self.buffer_size, int):
+            raise ValueError(
+                f"`buffer_size` must be int. Found: {type(self.buffer_size)}."
+            )
+        if self.buffer_size < 1:
+            raise ValueError(
+                f"`buffer_size` must be greater than 0. Found: {self.buffer_size}"
+            )
+
+
+##############################################################################
+# Top-level Config
+##############################################################################
+@dataclass
+class PipelineConfig(Generic[T, U]):
+    """A pipeline configuration.
+
+    A pipeline consists of source, a series of pipes and sink.
+
+    You can use :py:func:`spdl.pipeline.build_pipeline` to build a
+    :py:class:`spdl.pipeline.Pipeline` object.
+    """
+
+    src: SourceConfig[T]
+    """Source configuration."""
+
+    pipes: Sequence[PipeConfig[Any, Any]]
+    """Pipe configurations."""
+
+    sink: SinkConfig[U]
+    """Sink configuration."""
+
+    def __repr__(self) -> str:
+        args = ["PipelineConfig"]
+        args.append(f" - Source: {self.src!r}")
+        if self.pipes:
+            args.append(" - Pipes:")
+        for p in self.pipes:
+            args.append(f"   - {p!r}")
+        args.append(f" - Sink: {self.sink!r}")
+        return "\n".join(args)
+
+
+##############################################################################
+# Specialization for ease of use for users.
+##############################################################################
+def _get_op_name(op: Callable) -> str:
+    if isinstance(op, partial):
+        return _get_op_name(op.func)
+    return getattr(op, "__name__", op.__class__.__name__)
+
+
+@runtime_checkable
+class SupportsGetItem(Protocol[T, U]):
+    """Protocol for classes with ``__getitem__`` method, such as ``dict`` and ``list``."""
+
+    def __getitem__(self, key: T) -> U: ...
+
+
+_TPipeInputs: TypeAlias = Callables[T, U] | SupportsGetItem[T, U]
+
+
+def Pipe(
+    op: _TPipeInputs[T, U],
+    /,
+    *,
+    concurrency: int = 1,
+    executor: Executor | None = None,
+    name: str | None = None,
+    output_order: str = "completion",
+) -> PipeConfig[T, U]:
+    """Create a :py:class:`PipeConfig`.
+
+    A pipe applys a function or mapping to the inocming item.
+
+    Args:
+        op: A function, callable or container with ``__getitem__`` method
+            (such as dict, list and tuple).
+            If it's function or callable, it is inovked with the input from the input queue.
+            If it's container type, the input is passed to ``__getitem__`` method.
+
+            The function or callable must take exactly one argument, which is the output
+            from the upstream. If passing around multiple objects, take
+            them as a tuple or use :py:class:`~dataclasses.dataclass` and
+            define a custom protocol.
+
+            If the result of applying ``op`` to an input item is ``None``,
+            the pipeline skips absorb the result and it won't be propagated to
+            the downstream stages.
+
+            Optionally, the op can be a generator function, async function or
+            async generator function.
+
+            If ``op`` is (async) generator, the items yielded are put in the output
+            queue separately.
+
+            .. warning::
+
+                If ``op`` is synchronous geneartor, and ``executor`` is an instance of
+                :py:class:`concurrent.futures.ProcessPoolExecutor`, the output items
+                are not put in the output queue until the generator is exhausted.
+
+                Async generator, or synchronous generator without ``ProcessPoolExecutor``
+                does not have this issue, and the yielded items are put in the output
+                queue immediately.
+
+            .. tip::
+
+                When passing an async op, make sure that the op does not call sync
+                function inside.
+                If calling a sync function, use :py:meth:`asyncio.loop.run_in_executor`
+                or :py:func:`asyncio.to_thread` to delegate the execution to the thread pool.
+
+        concurrency: The maximum number of async tasks executed concurrently.
+        executor: A custom executor object to be used to convert the synchronous operation
+            into asynchronous one. If ``None``, the default executor is used.
+
+            It is invalid to provide this argument when the given op is already async.
+        name: The name (prefix) to give to the task.
+        output_order: If ``"completion"`` (default), the items are put to output queue
+            in the order their process is completed.
+            If ``"input"``, then the items are put to output queue in the order given
+            in the input queue.
+
+    Returns:
+        The config object.
+
+    .. seealso::
+
+       - :py:config:`PipelineConfig`
+       - :py:func:`spdl.pipeline.build_pipeline`
+    """
+
+    if output_order not in ["completion", "input"]:
+        raise ValueError(
+            '`output_order` must be either "completion" or "input". '
+            f"Found: {output_order}"
+        )
+
+    _type = _PipeType.Pipe if output_order == "completion" else _PipeType.OrderedPipe
+
+    if isinstance(op, SupportsGetItem):
+        # Note, if op is list/dict/tuple with a lot of elements, then
+        # debug print on `_ProcessConfig` might produce extremely long string.
+        # So it is important to extract the __getitem__ before it is passed to
+        # `_ProcessConfig`.
+        op = op.__getitem__
+
+        # We could do the same for callable (__call__)
+        # but usually callable class name contains readable information, so
+        # we don't do that here. (it happens in to_async helper function)
+
+    return PipeConfig(
+        name=name or _get_op_name(op),
+        _type=_type,
+        _args=_PipeArgs(
+            op=op,
+            executor=executor,
+            concurrency=concurrency,
+        ),
+    )
+
+
+def Aggregate(num_items: int, /, *, drop_last: bool = False) -> PipeConfig[Any, Any]:
+    """Create a :py:class:`PipeConfig` object for aggregation.
+
+    The aggregation buffers the incoming items and emits once enough items are buffered.
+
+    Args:
+        num_items: The number of items to buffer.
+        drop_last: Drop the last aggregation if it has less than ``num_aggregate`` items.
+
+    Returns:
+        The config object.
+
+    .. seealso::
+
+       - :py:config:`PipelineConfig`
+       - :py:func:`spdl.pipeline.build_pipeline`
+    """
+
+    # To avoid circular deps
+    from .._components._pipe import _Aggregate
+
+    name = (
+        f"aggregate({num_items}, {drop_last=})"
+        if drop_last
+        else f"aggregate({num_items})"
+    )
+    return PipeConfig(
+        name=name,
+        _type=_PipeType.Aggregate,
+        _args=_PipeArgs(
+            op=_Aggregate(num_items, drop_last),
+            op_requires_eof=True,
+        ),
+    )
+
+
+def Disaggregate() -> PipeConfig[Any, Any]:
+    """Create a :py:class:`PipeConfig` object for disaggregation.
+
+    The disaggregate slices the incoming list of items and yield them
+    one by one.
+
+    Returns:
+        The config object.
+
+    .. seealso::
+
+       - :py:config:`PipelineConfig`
+       - :py:func:`spdl.pipeline.build_pipeline`
+    """
+
+    # To avoid circular deps
+    from .._components._pipe import _disaggregate
+
+    return PipeConfig(
+        name="disaggregate",
+        _type=_PipeType.Disaggregate,
+        _args=_PipeArgs(
+            op=_disaggregate,  # pyre-ignore: [6]
+        ),
+    )
