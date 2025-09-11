f3cb114f7852a713380cb875719983dafc68653c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 04:06:22 +0000

    Add package coin-api/2.0.0/coin_api-2.0.0-py3-none-any.whl

---
+++ packages/coin-api/coin_api-2.0.0-py3-none-any.whl/coin_api/__init__.py
@@ -0,0 +1,14 @@
+"""
+Coin API Package
+
+A Python package for getting cryptocurrency prices from CoinMarketCap.
+"""
+
+from .COIN_API import coin, main
+
+
+__version__ = "1.0.0"
+__author__ = "faizan code"
+__email__ = "thetriquetrathetriquetradeveloper@gmail.com"
+
+__all__ = ["coin", "main"]
