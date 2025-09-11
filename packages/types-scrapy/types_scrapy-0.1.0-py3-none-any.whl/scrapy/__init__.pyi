f9ac340c2ba7720773e0c733f0b20ef2c5b3df16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 08:06:54 +0000

    Add package types-scrapy/0.1.0/types_scrapy-0.1.0-py3-none-any.whl

---
+++ packages/types-scrapy/types_scrapy-0.1.0-py3-none-any.whl/scrapy/__init__.pyi
@@ -0,0 +1,10 @@
+from _typeshed import Incomplete
+from scrapy.http import FormRequest as FormRequest, Request as Request
+from scrapy.item import Field as Field, Item as Item
+from scrapy.selector import Selector as Selector
+from scrapy.spiders import Spider as Spider
+
+__all__ = ['Field', 'FormRequest', 'Item', 'Request', 'Selector', 'Spider', '__version__', 'version_info']
+
+__version__: Incomplete
+version_info: Incomplete
