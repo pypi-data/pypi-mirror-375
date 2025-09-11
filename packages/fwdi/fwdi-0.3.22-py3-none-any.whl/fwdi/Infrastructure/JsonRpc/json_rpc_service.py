c6b57d04645372decaec13e0693670d9fb78763d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 14:06:16 +0000

    Add package fwdi/0.3.22/fwdi-0.3.22-py3-none-any.whl

---
+++ packages/fwdi/fwdi-0.3.22-py3-none-any.whl/fwdi/Infrastructure/JsonRpc/json_rpc_service.py
@@ -0,0 +1,27 @@
+from types import FunctionType
+from typing import Union
+from collections.abc import Coroutine
+
+from fastapi_jsonrpc import Entrypoint, API
+from fastapi import FastAPI
+
+class JRPCService():
+    def __init__(self, path:str='/api/v1/jsonrpc'):
+        self.__api = Entrypoint(path=path)
+        self.__app = API()
+    
+    def add_method(self, 
+                   func: Union[FunctionType, Coroutine],
+                   name:str,
+                   tags:list[str]|None=None):
+        if tags:
+            try:
+                self.__api.add_method_route(func=func, name=name, **{"tags": tags})
+            except Exception as ex:
+                print(f"ERROR:{ex}")
+        else:
+            self.__api.add_method_route(func=func, name=name)
+    
+    def mount_to_asgi(self, fast_api:FastAPI, path:str='rpc'):
+        self.__app.bind_entrypoint(self.__api)
+        fast_api.mount(path, self.__app)
