e85ed02f1e9f528e662488956842a60228be0109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 06:05:33 +0000

    Add package plone.subrequest/2.0.6/plone_subrequest-2.0.6.tar.gz

---
+++ packages/plone.subrequest/plone_subrequest-2.0.6.tar.gz/plone_subrequest-2.0.6/README.rst
@@ -0,0 +1,18 @@
+Overview
+========
+
+plone.subrequest provides a mechanism for issuing subrequests under Zope.
+
+Installation
+============
+
+Plone 4+
+--------
+
+This package is included by core Plone, so no special installation is required.
+
+
+Zope
+----
+
+Load this package's ZCML in the usual manner.
---
+++ packages/plone.subrequest/plone_subrequest-2.0.6.tar.gz/plone_subrequest-2.0.6/setup.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "2.0.6"
+
+long_description = f"""
+{Path("README.rst").read_text()}
+\n\n
+{(Path("src") / "plone" / "subrequest" / "usage.rst").read_text()}
+\n\n
+{Path("CHANGES.rst").read_text()}
+"""
+
+setup(
+    name="plone.subrequest",
+    version=version,
+    description="Subrequests for Zope2",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope",
+        "Framework :: Zope :: 5",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="plone",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://github.com/plone/plone.subrequest",
+    license="GPL version 2",
+    packages=find_packages("src"),
+    namespace_packages=["plone"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    platforms="Any",
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "setuptools",
+        "plone.protect",
+        "Zope",
+    ],
+    extras_require={
+        "test": [
+            "five.localsitemanager",
+            "manuel",
+            "plone.testing[zope]",
+            "zope.configuration",
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
