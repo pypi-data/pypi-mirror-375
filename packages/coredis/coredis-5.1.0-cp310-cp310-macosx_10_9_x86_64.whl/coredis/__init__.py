bb3129861a351ebd94e1f3e23e24592155e81340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 07:06:38 +0000

    Add package coredis/5.1.0/coredis-5.1.0-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/coredis/coredis-5.1.0-cp310-cp310-macosx_10_9_x86_64.whl/coredis/__init__.py
@@ -0,0 +1,46 @@
+"""
+coredis
+-------
+
+coredis is an async redis client with support for redis server,
+cluster & sentinel.
+"""
+
+from __future__ import annotations
+
+from typing import cast
+
+from coredis.client import Redis, RedisCluster
+from coredis.config import Config
+from coredis.connection import (
+    BaseConnection,
+    ClusterConnection,
+    Connection,
+    UnixDomainSocketConnection,
+)
+from coredis.pool import (
+    BlockingClusterConnectionPool,
+    BlockingConnectionPool,
+    ClusterConnectionPool,
+    ConnectionPool,
+)
+from coredis.tokens import PureToken
+
+from . import _version
+
+__all__ = [
+    "Config",
+    "Redis",
+    "RedisCluster",
+    "BaseConnection",
+    "Connection",
+    "UnixDomainSocketConnection",
+    "ClusterConnection",
+    "BlockingConnectionPool",
+    "ConnectionPool",
+    "BlockingClusterConnectionPool",
+    "ClusterConnectionPool",
+    "PureToken",
+]
+
+__version__ = cast(str, _version.get_versions()["version"])  # type: ignore
