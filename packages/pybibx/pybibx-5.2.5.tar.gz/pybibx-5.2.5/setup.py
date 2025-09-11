c0564a24dabd9261bcfde360a7851c4a61b7f3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 09:07:39 +0000

    Add package pybibx/5.2.5/pybibx-5.2.5.tar.gz

---
+++ packages/pybibx/pybibx-5.2.5.tar.gz/pybibx-5.2.5/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+    name='pybibx',
+    version='5.2.5',
+    license='GNU',
+    author='Valdecy Pereira',
+    author_email='valdecy.pereira@gmail.com',
+    url='https://github.com/Valdecy/pybibx',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'bertopic',
+        'bert-extractive-summarizer',
+        'chardet',
+        'google-generativeai',
+        'gensim',
+        'keybert',
+        'llmx',
+        'matplotlib',
+        'networkx',
+        'numba',
+        'numpy',
+        'pandas',
+        'Pillow',
+        'plotly',
+        'scipy',
+        'scikit-learn',
+        'sentencepiece',
+        'sentence-transformers',
+        'torch', 
+        'torchvision',
+        'torchaudio',
+        'transformers',
+        'umap-learn',
+        'openai',
+        'wordcloud'
+    ],
+    zip_safe=True,
+    description='A Bibliometric and Scientometric Library Powered with Artificial Intelligence Tools',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
