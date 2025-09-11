6d45a2c7f41ab3d7e1e4d9a11d2f012c0d31a4d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 09:07:28 +0000

    Add package spotifi/0.1.1/spotifi-0.1.1.tar.gz

---
+++ packages/spotifi/spotifi-0.1.1.tar.gz/spotifi-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spotifi',
+    version='0.1.1',
+    packages=find_packages(),
+    author='fswair',
+    description='A python package that is spotifi',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
