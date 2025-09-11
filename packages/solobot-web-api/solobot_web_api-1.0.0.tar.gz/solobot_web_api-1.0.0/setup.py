0697198e95c6c82914e08f69140497b7a407aa0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 14:06:12 +0000

    Add package solobot-web-api/1.0.0/solobot_web_api-1.0.0.tar.gz

---
+++ packages/solobot-web-api/solobot_web_api-1.0.0.tar.gz/solobot_web_api-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+HERE = Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="solobot-web-api",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "aiohttp>=3.8.0",
+    ],
+    python_requires=">=3.9",
+    description="Async Python SoloBot API Wrapper",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hteppl/solobot-web-api",
+    author="hteppl",
+    author_email="hteppl.dev@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+    ],
+    include_package_data=True,
+)
