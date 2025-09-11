780419bfe85ff5d378fb35929398d216c038debe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 06:05:52 +0000

    Add package plone.resource/3.0.3/plone_resource-3.0.3.tar.gz

---
+++ packages/plone.resource/plone_resource-3.0.3.tar.gz/plone_resource-3.0.3/setup.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "3.0.3"
+
+test_requires = [
+    "plone.app.testing",
+    "plone.testing",
+]
+
+long_description = (
+    f"{Path('README.rst').read_text()}\n{Path('CHANGES.rst').read_text()}"
+)
+
+setup(
+    name="plone.resource",
+    version=version,
+    description="Static files for Plone",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="plone resource",
+    author="David Glick, Plone Foundation",
+    author_email="davidglick@groundwire.org",
+    url="https://pypi.org/project/plone.resource",
+    license="GPL version 2 or later",
+    packages=find_packages("src"),
+    namespace_packages=["plone"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "plone.caching",
+        "python-dateutil",
+        "setuptools",
+        "z3c.caching",
+        "Zope",
+        "Products.BTreeFolder2",
+        "Products.CMFCore",
+        "Products.GenericSetup",
+    ],
+    extras_require={
+        "test": test_requires,
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
