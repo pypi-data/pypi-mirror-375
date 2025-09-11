0b57aa4a2246ca810ea11b7394d5c42121dbc03d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 06:06:10 +0000

    Add package plone.event/2.0.3/plone_event-2.0.3.tar.gz

---
+++ packages/plone.event/plone_event-2.0.3.tar.gz/plone_event-2.0.3/setup.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "2.0.3"
+
+long_description = (
+    f"{Path('README.rst').read_text()}\n{Path('CHANGES.rst').read_text()}"
+)
+
+setup(
+    name="plone.event",
+    version=version,
+    description="Event and calendaring related tools not bound to Plone",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    # Get more strings from
+    # https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="Plone calendar calendaring event recurring",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://github.com/plone/plone.event",
+    license="GPL",
+    packages=find_packages("src"),
+    namespace_packages=["plone"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "setuptools",
+        "python-dateutil",  # >4.0.2
+        "pytz",
+        "zope.component",
+        "zope.interface",
+    ],
+    extras_require={
+        "test": [
+            "DateTime",
+            "zope.configuration",
+        ],
+    },
+)
