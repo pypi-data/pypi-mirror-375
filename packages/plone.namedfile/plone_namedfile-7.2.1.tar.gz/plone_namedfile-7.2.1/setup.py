cd8260f2c1d74c054c17f870b7194e5d566ec035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 06:06:09 +0000

    Add package plone.namedfile/7.2.1/plone_namedfile-7.2.1.tar.gz

---
+++ packages/plone.namedfile/plone_namedfile-7.2.1.tar.gz/plone_namedfile-7.2.1/setup.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "7.2.1"
+
+description = "File types and fields for images, files and blob files with filenames"
+long_description = (
+    f"{Path('README.rst').read_text()}\n"
+    f"{Path('CHANGES.rst').read_text()}\n"
+    f"{(Path('src') / 'plone' / 'namedfile' / 'usage.rst').read_text()}"
+)
+
+setup(
+    name="plone.namedfile",
+    version=version,
+    description=description,
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: 6.1",
+        "Framework :: Plone :: Core",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+    ],
+    keywords="plone named file image blob",
+    author="Laurence Rowe, Martin Aspeli",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://pypi.org/project/plone.namedfile",
+    license="BSD",
+    packages=find_packages("src"),
+    namespace_packages=["plone"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.9",
+    install_requires=[
+        "BeautifulSoup4",
+        "persistent",
+        "piexif",
+        "Pillow",
+        "plone.app.uuid>=2.2.0",
+        "plone.base",
+        "plone.dexterity",
+        "plone.memoize",
+        "plone.protect",
+        "plone.rfc822>=2.0.0",
+        "plone.scale[storage]>=4.2.0",
+        "plone.schemaeditor",
+        "plone.supermodel",
+        "Products.CMFCore",
+        "setuptools",
+        "z3c.caching",
+        "zope.cachedescriptors",
+        "zope.copy",
+        "Zope",
+    ],
+    extras_require={
+        "test": [
+            "lxml",
+            "plone.app.testing",
+            "plone.testing",
+        ],
+    },
+)
