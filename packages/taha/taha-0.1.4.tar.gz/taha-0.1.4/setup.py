70ef5a88ff81253e501c93989a70595f079d35e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 15:05:56 +0000

    Add package taha/0.1.4/taha-0.1.4.tar.gz

---
+++ packages/taha/taha-0.1.4.tar.gz/taha-0.1.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='taha',
+    version='0.1.4',
+    py_modules=['taha'],
+    author='T_programmer',
+    description='It is a library for you',
+    python_requires='>=3.6',
+)
