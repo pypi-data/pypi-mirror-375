8b130924cbf7af482c076e8ff9ee5ceaba4b1943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 06:06:09 +0000

    Add package plone.memoize/3.0.4/plone_memoize-3.0.4.tar.gz

---
+++ packages/plone.memoize/plone_memoize-3.0.4.tar.gz/plone_memoize-3.0.4/setup.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "3.0.4"
+
+long_description = (
+    f"{Path('README.rst').read_text()}\n"
+    f"{(Path('src') / 'plone' / 'memoize' / 'README.rst').read_text()}\n"
+    f"{Path('CHANGES.rst').read_text()}"
+)
+
+setup(
+    name="plone.memoize",
+    version=version,
+    description="Decorators for caching the values of functions and methods",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    # Get more strings from
+    # https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    keywords="plone memoize decorator cache",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://pypi.org/project/plone.memoize",
+    license="BSD",
+    packages=find_packages("src"),
+    namespace_packages=["plone"],
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    extras_require=dict(
+        test=[
+            "zope.configuration",
+            "zope.publisher",
+            "zope.testing",
+        ],
+    ),
+    install_requires=[
+        "setuptools",
+        "zope.annotation",
+        "zope.component",
+        "zope.globalrequest",
+        "zope.interface",
+        "zope.ramcache",
+    ],
+)
