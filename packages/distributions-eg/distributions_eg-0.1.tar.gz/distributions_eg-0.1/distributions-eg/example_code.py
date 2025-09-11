ea1b746566f360702d429cb59d9c32c0a95283eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 08:06:43 +0000

    Add package distributions-eg/0.1/distributions_eg-0.1.tar.gz

---
+++ packages/distributions-eg/distributions_eg-0.1.tar.gz/distributions_eg-0.1/distributions-eg/example_code.py
@@ -0,0 +1,4 @@
+from distributions import Gaussian
+
+gaussian_one = Gaussian(10, 5)
+print(gaussian_one.mean)
---
+++ packages/distributions-eg/distributions_eg-0.1.tar.gz/distributions_eg-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='distributions-eg',
+    version='0.1',
+    packages=['distributions-eg'],
+    install_requires=[],
+    author='Ali Tharwat',
+    zip_safe=False
+)
