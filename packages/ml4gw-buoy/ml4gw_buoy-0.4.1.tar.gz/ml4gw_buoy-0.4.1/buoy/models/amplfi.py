3ef52ce943bb8c7850c29f8ed2fdf43206349de7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 08:06:49 +0000

    Add package ml4gw-buoy/0.4.1/ml4gw_buoy-0.4.1.tar.gz

---
+++ packages/ml4gw-buoy/ml4gw_buoy-0.4.1.tar.gz/ml4gw_buoy-0.4.1/buoy/models/amplfi.py
@@ -0,0 +1,186 @@
+import logging
+from dataclasses import dataclass
+from typing import List, Optional, TYPE_CHECKING
+
+import torch
+from jsonargparse import ArgumentParser
+from ml4gw.transforms import ChannelWiseScaler, SpectralDensity, Whiten
+
+from buoy.utils.data import get_local_or_hf, slice_amplfi_data
+from buoy.utils.pe import postprocess_samples, run_amplfi
+
+if TYPE_CHECKING:
+    from amplfi.train.architectures.flows import FlowArchitecture
+    from amplfi.train.prior import AmplfiPrior
+
+REPO_ID = "ML4GW/amplfi"
+
+
+@dataclass
+class AmplfiConfig:
+    architecture: "FlowArchitecture"
+    parameter_sampler: "AmplfiPrior"
+    sample_rate: float
+    kernel_length: float
+    inference_params: List[str]
+    event_position: float
+    psd_length: float
+    fduration: float
+    fftlength: float
+    highpass: float
+    lowpass: Optional[float] = None
+
+
+class Amplfi(AmplfiConfig):
+    def __init__(
+        self,
+        model_weights: Optional[str] = "amplfi-hlv.ckpt",
+        config: Optional[str] = "amplfi-hlv-config.yaml",
+        device: Optional[str] = None,
+    ):
+        if device is None:
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.device = device
+        logging.debug(f"Using device: {self.device}")
+
+        model_weights = get_local_or_hf(
+            filename=model_weights,
+            repo_id=REPO_ID,
+            descriptor="AMPLFI model weights",
+        )
+        config = get_local_or_hf(
+            filename=config,
+            repo_id=REPO_ID,
+            descriptor="AMPLFI model config",
+        )
+
+        parser = ArgumentParser()
+        parser.add_class_arguments(
+            AmplfiConfig, fail_untyped=False, sub_configs=True
+        )
+        parser.link_arguments(
+            "inference_params",
+            "architecture.init_args.num_params",
+            compute_fn=lambda x: len(x),
+            apply_on="parse",
+        )
+        args = parser.parse_path(config)
+        args = parser.instantiate_classes(args)
+
+        super().__init__(**vars(args))
+
+        model, scaler = self.load_model(
+            args.architecture,
+            model_weights,
+            len(args.inference_params),
+        )
+        self.model = model.to(self.device)
+        self.scaler = scaler.to(self.device)
+
+        self.configure_preprocessing()
+
+    def load_model(
+        self,
+        model: "FlowArchitecture",
+        model_weights: str,
+        num_params: int,
+    ):
+        checkpoint = torch.load(
+            model_weights, map_location="cpu", weights_only=False
+        )
+        arch_weights = {
+            k[6:]: v
+            for k, v in checkpoint["state_dict"].items()
+            if k.startswith("model.")
+        }
+        model.load_state_dict(arch_weights)
+        model.eval()
+        scaler_weights = {
+            k[len("scaler.") :]: v
+            for k, v in checkpoint["state_dict"].items()
+            if k.startswith("scaler.")
+        }
+        scaler = ChannelWiseScaler(num_params)
+        scaler.load_state_dict(scaler_weights)
+        return model, scaler
+
+    def update_config(self, **kwargs):
+        """
+        Update the AMPLFI configuration with new parameters.
+
+        Warning: some changes may not be sensible given how
+        the model was trained (e.g., kernel_length, sample_rate).
+        Changing these parameters may lead to unexpected results.
+        """
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+            else:
+                raise ValueError(f"Invalid configuration parameter: {key}")
+
+        # Reconfigure preprocessing after updating parameters
+        self.configure_preprocessing()
+
+    def configure_preprocessing(self):
+        self.spectral_density = SpectralDensity(
+            sample_rate=self.sample_rate,
+            fftlength=self.fftlength,
+            average="median",
+        ).to(self.device)
+        self.whitener = Whiten(
+            fduration=self.fduration,
+            sample_rate=self.sample_rate,
+            highpass=self.highpass,
+            lowpass=self.lowpass,
+        ).to(self.device)
+
+    @property
+    def minimum_data_size(self) -> int:
+        """Minimum data size required for the model to run."""
+        return int(
+            (self.kernel_length + self.psd_length + self.fduration)
+            * self.sample_rate
+        )
+
+    def __call__(
+        self,
+        data: torch.Tensor,
+        t0: float,
+        tc: float,
+        samples_per_event: int,
+    ):
+        if data.shape[-1] < self.minimum_data_size:
+            raise ValueError(
+                f"Data size {data.shape[-1]} is less than the minimum "
+                f"size of {self.minimum_data_size}"
+            )
+
+        psd_data, window = slice_amplfi_data(
+            data=data,
+            sample_rate=self.sample_rate,
+            t0=t0,
+            tc=tc,
+            kernel_length=self.kernel_length,
+            event_position=self.event_position,
+            psd_length=self.psd_length,
+            fduration=self.fduration,
+        )
+
+        samples = run_amplfi(
+            amplfi_strain=window,
+            amplfi_psd_strain=psd_data,
+            samples_per_event=samples_per_event,
+            spectral_density=self.spectral_density,
+            amplfi_whitener=self.whitener,
+            amplfi=self.model,
+            std_scaler=self.scaler,
+            device=self.device,
+        )
+        samples = samples.cpu()
+        result = postprocess_samples(
+            samples=samples,
+            event_time=tc,
+            inference_params=self.inference_params,
+            parameter_sampler=self.parameter_sampler,
+        )
+        return result
---
+++ packages/ml4gw-buoy/ml4gw_buoy-0.4.1.tar.gz/ml4gw_buoy-0.4.1/buoy/utils/data.py
@@ -0,0 +1,191 @@
+import logging
+import warnings
+from pathlib import Path
+
+import gwosc
+import h5py
+import numpy as np
+import torch
+from gwpy.timeseries import TimeSeries, TimeSeriesDict
+from huggingface_hub import hf_hub_download
+from huggingface_hub.errors import EntryNotFoundError
+from ligo.gracedb.rest import GraceDb
+
+STRAIN_CHANNELS = {
+    "H1": "H1:GDS-CALIB_STRAIN_CLEAN",
+    "L1": "L1:GDS-CALIB_STRAIN_CLEAN",
+    "V1": "V1:Hrec_hoft_16384Hz",
+}
+
+
+def get_local_or_hf(
+    filename: Path,
+    repo_id: str,
+    descriptor: str,
+):
+    """
+    Determine whether a given file exists locally or in a HuggingFace
+    repository. If the file exists locally, return the filename.
+    If it does not exist locally, attempt to download it from the
+    HuggingFace repository. If the file is not found in either
+    location, raise a ValueError.
+
+    Args:
+        filename: The name of the file to load.
+        repo_id: The HuggingFace repository ID.
+        descriptor: A description of the file for logging.
+
+    Returns:
+        The path to the file.
+    """
+    if Path(filename).exists():
+        logging.info(f"Loading {descriptor} from {filename}")
+        return filename
+    else:
+        try:
+            logging.info(
+                f"Downloading {descriptor} from HuggingFace "
+                "or loading from cache"
+            )
+            return hf_hub_download(repo_id=repo_id, filename=str(filename))
+        except EntryNotFoundError as e:
+            raise ValueError(
+                f"{descriptor} {filename} not found locally or in "
+                f"HuggingFace repository {repo_id}. Please check the name."
+            ) from e
+
+
+def slice_amplfi_data(
+    data: torch.Tensor,
+    sample_rate: float,
+    t0: float,
+    tc: float,
+    kernel_length: float,
+    event_position: float,
+    psd_length: float,
+    fduration: float,
+):
+    """
+    Slice the data to get the PSD window and kernel for amplfi
+    """
+    window_start = tc - t0 - event_position - fduration / 2
+    window_start = int(sample_rate * window_start)
+    window_length = int((kernel_length + fduration) * sample_rate)
+    window_end = window_start + window_length
+
+    if window_start < 0:
+        raise ValueError(
+            "The start of the AMPLFI window before the start of the data. "
+            "This may be due to the event time being too close to "
+            "the start of the data."
+        )
+    if window_end > data.shape[-1]:
+        raise ValueError(
+            "The end of the AMPLFI window is after the end of the data. "
+            "This may be due to the event time being too close to "
+            "the end of the data."
+        )
+
+    psd_start = window_start - int(psd_length * sample_rate)
+
+    if psd_start < 0:
+        raise ValueError(
+            "The start of the PSD window before the start of the data. "
+            "This may be due to the event time being too close to "
+            "the start of the data."
+        )
+
+    psd_data = data[0, :, psd_start:window_start]
+    window = data[0, :, window_start:window_end]
+
+    return psd_data, window
+
+
+def get_data(
+    event: str,
+    sample_rate: float,
+    psd_length: float,
+    datadir: Path,
+):
+    if event.startswith("GW"):
+        event_time = gwosc.datasets.event_gps(event)
+        ifos = sorted(gwosc.datasets.event_detectors(event))
+    else:
+        client = GraceDb()
+        if event.startswith("G"):
+            response = client.event(event).json()
+            event_time = response["gpstime"]
+            ifos = response["instruments"].split(",")
+        elif event.startswith("S"):
+            response = client.superevent(event).json()
+            event_time = response["preferred_event_data"]["gpstime"]
+            ifos = response["preferred_event_data"]["instruments"].split(",")
+        else:
+            raise ValueError(
+                f"Event {event} is not a valid event name. "
+                "Should be a valid GPS time, a known gravitational wave "
+                "event name (e.g. GW123456), or a GraceDB event or superevent "
+                "(e.g. G123456 or S123456)."
+            )
+
+    # Make sure things start at an integer time for consistency.
+    # Take data from psd_length * (-1.5, 0.5) around the event
+    # time to make sure there's enough for analysis. This isn't
+    # totally robust, but should be good for most use cases.
+    offset = event_time % 1
+    start = event_time - 1.5 * psd_length - offset
+    end = event_time + 0.5 * psd_length - offset
+
+    datafile = datadir / f"{event}.hdf5"
+    if not datafile.exists():
+        logging.info(
+            "Fetching data from between GPS times "
+            f"{start} and {end} for {ifos}"
+        )
+
+        ts_dict = TimeSeriesDict()
+        for ifo in ifos:
+            if start < 1269363618:
+                ts_dict[ifo] = TimeSeries.fetch_open_data(ifo, start, end)
+            else:
+                ts_dict[ifo] = TimeSeries.get(STRAIN_CHANNELS[ifo], start, end)
+
+            span = ts_dict[ifo].span
+            if span.end - span.start < 128:
+                ts_dict.pop(ifo)
+                warnings.warn(
+                    f"Detector {ifo} did not have sufficient data surrounding "
+                    "the event time, removing it from the dataset",
+                    stacklevel=2,
+                )
+
+        ifos = list(ts_dict.keys())
+        logging.info(f"Fetched data for detectors {ifos}")
+        if ifos not in [["H1", "L1"], ["H1", "L1", "V1"]]:
+            raise ValueError(
+                f"Event {event} does not have the required detectors. "
+                f"Expected ['H1', 'L1'] or ['H1', 'L1', 'V1'], got {ifos}"
+            )
+        ts_dict = ts_dict.resample(sample_rate)
+
+        logging.info(f"Saving data to file {datafile}")
+
+        with h5py.File(datafile, "w") as f:
+            f.attrs["tc"] = event_time
+            f.attrs["t0"] = start
+            for ifo in ifos:
+                f.create_dataset(ifo, data=ts_dict[ifo].value)
+
+        t0 = start
+        data = np.stack([ts_dict[ifo].value for ifo in ifos])[None]
+
+    else:
+        logging.info(f"Loading data from file for event {event}")
+        with h5py.File(datafile, "r") as f:
+            ifos = list(f.keys())
+            data = np.stack([f[ifo][:] for ifo in ifos])[None]
+            event_time = f.attrs["tc"]
+            t0 = f.attrs["t0"]
+        logging.info(f"Loaded data for detectors {ifos}")
+
+    return data, ifos, t0, event_time
