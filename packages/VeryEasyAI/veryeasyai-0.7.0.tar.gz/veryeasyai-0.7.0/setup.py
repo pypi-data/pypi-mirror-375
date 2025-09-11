b9034a5485fb554b39010f79e4ae0f2850726aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 04:07:06 +0000

    Add package VeryEasyAI/0.7.0/veryeasyai-0.7.0.tar.gz

---
+++ packages/VeryEasyAI/veryeasyai-0.7.0.tar.gz/veryeasyai-0.7.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="VeryEasyAI",
+    version="0.7.0",
+    description="Uma biblioteca de IA super simples e fácil de usar",
+    author="Enzo DEV",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.8",
+)
