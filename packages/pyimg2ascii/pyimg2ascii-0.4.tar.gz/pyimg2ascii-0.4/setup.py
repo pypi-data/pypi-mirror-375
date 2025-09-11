1e35b1a1885990916486889f4fcacb7fae09770c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-11 03:18:02 +0000

    Add package pyimg2ascii/0.4/pyimg2ascii-0.4.tar.gz

---
+++ packages/pyimg2ascii/pyimg2ascii-0.4.tar.gz/pyimg2ascii-0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyimg2ascii",
+    version="0.4",
+    packages=find_packages(),
+    install_requires=["pillow"],
+    description="Convert images to ASCII art",
+    author="Devulapalli.Abhiram",
+    long_description="README.md",
+    long_description_content_type="text/markdown",
+    
+    author_email="abhiramdevulapalli8@gmail.com",
+    url="https://github.com/abhirammdh/pyimg2ascii",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7",
+)
